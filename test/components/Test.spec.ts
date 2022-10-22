075d365997478aca29622208e44eec51010e9461
o <poteat@users.noreply.github.com>
2022-10-22 13:04:40 -0700

    Write HKT-level parameter type checking

---
+++ test/components/Test.spec.ts
@@ -0,0 +1,49 @@
+import Test from "./Test";
+
+type Test_Spec = [
+  /**
+   * Expect should pass compilation with a true type.
+   */
+  Test.Expect<true>,
+
+  /**
+   * ExpectNot should pass compilation with a false type.
+   */
+  Test.ExpectNot<false>,
+
+  /**
+   * Expect should emit an error on false type.
+   */
+  // @ts-expect-error
+  Test.Expect<false>,
+
+  /**
+   * ExpectNot should emit an error on true type.
+   */
+  // @ts-expect-error
+  Test.ExpectNot<true>,
+
+  /**
+   * Expect should emit an error on non-boolean types.
+   */
+  // @ts-expect-error
+  Test.Expect<number>,
+
+  /**
+   * ExpectNot should emit an error on non-boolean types.
+   */
+  // @ts-expect-error
+  Test.ExpectNot<number>,
+
+  /**
+   * Expect should emit an error on the never type.
+   */
+  // @ts-expect-error
+  Test.Expect<never>,
+
+  /**
+   * Expect should emit an error on the never type.
+   */
+  // @ts-expect-error
+  Test.ExpectNot<never>
+];
