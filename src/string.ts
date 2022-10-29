d09fa4e58d611ef0787d971e54902d32d1f6ef71
o <poteat@users.noreply.github.com>
2022-10-28 18:21:08 -1000

    Add string replace utility

--- src/string.ts
+++ src/string.ts
@@ -136,4 +136,32 @@ export abstract class Init extends Kind {
   abstract f: (x: Cast<this[Kind._], string>) => _$init<typeof x>;
 }
 
+type _$replace2<
+  S extends string,
+  From extends string,
+  To extends string,
+  O extends string = ""
+> = S extends `${infer Head}${From}${infer Tail}`
+  ? _$replace2<Tail, From, To, `${O}${Head}${To}`>
+  : `${O}${S}`;
+
+export type _$replace<
+  S extends string,
+  From extends string,
+  To extends string
+> = _$isTemplate<From> extends true
+  ? string
+  : string extends From
+  ? string
+  : From extends ""
+  ? `${To}${_$replace2<S, From, To>}`
+  : _$replace2<S, From, To>;
+
+export abstract class Replace<
+  From extends string,
+  To extends string
+> extends Kind {
+  abstract f: (x: Cast<this[Kind._], string>) => _$replace<typeof x, From, To>;
+}
+
 export * as String from "./string";
