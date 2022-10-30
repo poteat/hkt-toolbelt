91c3fd8c209cc58d9b301836a836c449387577fb
o <poteat@users.noreply.github.com>
2022-10-29 21:12:46 -1000

    Fix weak type check for apply-self combinator

--- src/combinator.ts
+++ src/combinator.ts
@@ -4,9 +4,13 @@ export abstract class Self extends Kind {
   abstract f: (x: this[Kind._]) => Self;
 }
 
+declare abstract class RecursiveKind extends Kind {
+  abstract f: (x: Cast<this[Kind._], RecursiveKind>) => unknown;
+}
+
 export abstract class ApplySelf extends Kind {
   abstract f: (
-    x: Cast<this[Kind._], Kind>
+    x: Cast<this[Kind._], RecursiveKind>
   ) => $<typeof x, Cast<typeof x, Kind.InputOf<typeof x>>>;
 }
 
