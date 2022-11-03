9cc83bcae8840815f18a41d915b214ab1431a05b
o <poteat@users.noreply.github.com>
2022-11-02 20:46:02 -0700

    Export Object, Type, and Union modules

--- src/index.ts
+++ src/index.ts
@@ -5,8 +5,11 @@ export { Conditional } from "./conditional";
 export { Function } from "./function";
 export * from "./kind";
 export { List } from "./list";
+export { Object } from "./object";
 export { String } from "./string";
 export { Test } from "./test";
+export { Type } from "./type";
+export { Union } from "./union";
 
 export { $, $$ } from "./$";
 import { $ } from "./$";
