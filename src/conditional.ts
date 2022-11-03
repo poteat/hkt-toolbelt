856613306eafee7a352bcf7649b951233bbaa9a9
o <poteat@users.noreply.github.com>
2022-11-02 20:39:40 -0700

    Add 'if' hk-type utility

--- src/conditional.ts
+++ src/conditional.ts
@@ -1,4 +1,4 @@
-import { Kind } from ".";
+import $, { Cast, Function, Kind } from ".";
 
 export type _$equals<T, U> = [T, U] extends [U, T] ? true : false;
 
@@ -20,6 +20,25 @@ export abstract class Extends<Super> extends Kind {
  */
 export abstract class SubtypeOf<Super> extends Kind {
   abstract f: (x: this[Kind._]) => _$extends<Super, typeof x>;
+}
+
+export type _$if<
+  Predicate extends Kind<(x: never) => boolean>,
+  Then extends Kind,
+  Else extends Kind,
+  X extends Kind.InputOf<Predicate>
+> = $<Predicate, X> extends true
+  ? $<Then, Cast<X, Kind.InputOf<Then>>>
+  : $<Else, Cast<X, Kind.InputOf<Else>>>;
+
+export abstract class If<
+  Predicate extends Kind<(x: never) => boolean>,
+  Then extends Kind,
+  Else extends Kind = Function.Constant<never>
+> extends Kind {
+  abstract f: (
+    x: Cast<this[Kind._], Kind.InputOf<Predicate>>
+  ) => _$if<Predicate, Then, Else, typeof x>;
 }
 
 export * as Conditional from "./conditional";
--- test/conditional.spec.ts
+++ test/conditional.spec.ts
@@ -1,4 +1,4 @@
-import $, { Conditional, Test } from "hkt-toolbelt";
+import $, { Conditional, Function, List, String, Test } from "hkt-toolbelt";
 
 type Equals_Spec = [
   /**
@@ -14,7 +14,17 @@ type Equals_Spec = [
   /**
    * A type does not equal a supertype.
    */
-  Test.ExpectNot<$<Conditional.Equals<true>, boolean>>
+  Test.ExpectNot<$<Conditional.Equals<true>, boolean>>,
+
+  /**
+   * True does not equal never.
+   */
+  Test.ExpectNot<$<Conditional.Equals<true>, never>>,
+
+  /**
+   * Never does not equal true.
+   */
+  Test.ExpectNot<$<Conditional.Equals<never>, true>>
 ];
 
 type Extends_Spec = [
@@ -37,4 +47,52 @@ type Extends_Spec = [
    * A type does not extend a different type.
    */
   Test.ExpectNot<$<Conditional.Extends<string>, number>>
+];
+
+/**
+ * Tests associated with `Conditional.If`, which encodes kind-level if-then-else
+ * statements.
+ */
+type If_Spec = [
+  /**
+   * If-then-else statement with a true predicate.
+   */
+  Test.Expect<
+    $<Conditional.If<Function.Constant<true>, Function.Constant<"foo">>, 0>,
+    "foo"
+  >,
+
+  /**
+   * If-then-else statement with a false predicate.
+   */
+  Test.Expect<
+    $<Conditional.If<Function.Constant<false>, Function.Constant<"foo">>, 0>,
+    never
+  >,
+
+  /**
+   * If-then-else statement with a true predicate and a false alternative.
+   */
+  Test.Expect<
+    $<
+      Conditional.If<
+        Function.Constant<true>,
+        Function.Identity,
+        Function.Constant<"bar">
+      >,
+      "foo"
+    >,
+    "foo"
+  >,
+
+  /**
+   * Can be wrapped around other kinds, such as string kinds.
+   */
+  Test.Expect<
+    $<
+      List.Map<Conditional.If<String.IsString, String.StartsWith<"foo">>>,
+      ["foo", "bar", 42, "foobar"]
+    >,
+    [true, false, never, true]
+  >
 ];
