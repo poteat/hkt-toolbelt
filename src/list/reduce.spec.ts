c39cff9bbbe534b71926bb434df770347dadfc32
o <poteat@users.noreply.github.com>
2022-11-20 15:08:03 -0800

    Add further reduce stress tests

--- src/list/reduce.spec.ts
+++ src/list/reduce.spec.ts
@@ -15,5 +15,32 @@ type Reduce_Spec = [
       [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20]
     >,
     "210"
-  >
+  >,
+
+  /**
+   * Can handle a list of 100 elements.
+   */
+  Test.Expect<$<List.Reduce<NaturalNumber.Add, 0>, Hundred>, "550">,
+
+  /**
+   * Can handle a list of 500 elements.
+   */
+  Test.Expect<$<List.Reduce<NaturalNumber.Add, 0>, FiveHundred>, "2750">
 ];
+
+type Ten = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+type Hundred = [
+  ...Ten,
+  ...Ten,
+  ...Ten,
+  ...Ten,
+  ...Ten,
+  ...Ten,
+  ...Ten,
+  ...Ten,
+  ...Ten,
+  ...Ten
+];
+
+type FiveHundred = [...Hundred, ...Hundred, ...Hundred, ...Hundred, ...Hundred];
