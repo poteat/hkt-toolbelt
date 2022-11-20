e675c6a64b4479725e1943bead5dac20bbdc263a
o <poteat@users.noreply.github.com>
2022-11-20 14:26:23 -0800

    Publish 0.13.0

--- src/test/expect-not.ts
+++ src/test/expect-not.ts
@@ -1,10 +1,10 @@
-import { Conditional, Test } from "..";
+import { Conditional, Test, Type } from "..";
 
 export type ExpectNot<
   X extends Conditional._$equals<X, V> extends true ? V : V & Test._,
   V = false
-> = Test._$isNever<V> extends true
+> = Type._$isNever<V> extends true
   ? X
-  : Test._$isNever<X> extends true
+  : Type._$isNever<X> extends true
   ? ExpectNot<X, V>
   : X;
