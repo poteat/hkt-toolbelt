68f142e627f1d27ed6fd92a437eaafc7ea8b04d1
o <poteat@users.noreply.github.com>
2022-11-22 13:07:07 -0800

    Implement natural number subtraction

---
+++ src/natural-number/subtract.spec.ts
@@ -0,0 +1,80 @@
+import { $, Test, NaturalNumber, List } from "..";
+
+type Subtract_Spec = [
+  /**
+   * Can subtract two numbers.
+   */
+  Test.Expect<$<$<NaturalNumber.Subtract, 456>, 123>, "333">,
+
+  /**
+   * Can subtract two 3-digit numbers that result in a two-digit number.
+   */
+  Test.Expect<$<$<NaturalNumber.Subtract, 168>, 123>, "45">,
+
+  /**
+   * One minus one is zero.
+   */
+  Test.Expect<$<$<NaturalNumber.Subtract, 1>, 1>, "0">,
+
+  /**
+   * Zero minus anything is zero.
+   */
+  Test.Expect<$<$<NaturalNumber.Subtract, 0>, 123>, "0">,
+
+  /**
+   * Can subtract two empty lists.
+   */
+  Test.Expect<$<$<NaturalNumber.Subtract, 0>, 0>, "0">,
+
+  /**
+   * Can subtract a smaller number from a larger one.
+   */
+  Test.Expect<$<$<NaturalNumber.Subtract, 1000>, 1>, "999">,
+
+  /**
+   * Can subtract a number from itself.
+   */
+  Test.Expect<$<$<NaturalNumber.Subtract, 12>, 12>, "0">,
+
+  /**
+   * Can subtract zero.
+   */
+  Test.Expect<$<$<NaturalNumber.Subtract, 12>, 0>, "12">,
+
+  /**
+   * Can subtract bigint literals.
+   */
+  Test.Expect<
+    $<
+      $<NaturalNumber.Subtract, 123456789012345678901234567890n>,
+      123456789012345678901234567890n
+    >,
+    "0"
+  >,
+
+  /**
+   * Can subtract natural numbers represented as strings.
+   */
+  Test.Expect<$<$<NaturalNumber.Subtract, "456">, "123">, "333">,
+
+  /**
+   * Subtracting more than the first number results in zero.
+   */
+  Test.Expect<$<$<NaturalNumber.Subtract, 123>, 456>, "0">,
+
+  /**
+   * Can subtract larger numbers.
+   */
+  Test.Expect<
+    $<$<NaturalNumber.Subtract, 123456789012345678901234567890n>, 1>,
+    "123456789012345678901234567889"
+  >,
+
+  /**
+   * Can use reduce to subtract over a list.
+   */
+  Test.Expect<
+    $<List.Reduce<NaturalNumber.Subtract, 10000>, [1, 2, 3, 4, 5]>,
+    "9985"
+  >
+];
---
+++ src/natural-number/subtract.ts
@@ -0,0 +1,24 @@
+import { Number, NaturalNumber, DigitList, Type, Kind } from "..";
+
+export type _$subtract<
+  A extends Number.Number,
+  B extends Number.Number,
+  A_LIST extends DigitList.DigitList = NaturalNumber._$toList<A>,
+  B_LIST extends DigitList.DigitList = NaturalNumber._$toList<B>,
+  SUB_LIST extends DigitList.DigitList = DigitList._$subtract<A_LIST, B_LIST>,
+  RESULT = DigitList._$toString<SUB_LIST>
+> = RESULT;
+
+declare abstract class Subtract_T<X extends Number.Number> extends Kind.Kind {
+  abstract f: (
+    x: Type._$cast<this[Kind._], Number.Number>
+  ) => Number._$isNatural<typeof x> extends true
+    ? _$subtract<X, typeof x>
+    : never;
+}
+
+export declare abstract class Subtract extends Kind.Kind {
+  abstract f: (
+    x: Type._$cast<this[Kind._], Number.Number>
+  ) => Number._$isNatural<typeof x> extends true ? Subtract_T<typeof x> : never;
+}
