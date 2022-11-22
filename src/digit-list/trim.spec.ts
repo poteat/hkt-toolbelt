fafeefe970c6ff1067b0e27216facbe6d1fe79ee
o <poteat@users.noreply.github.com>
2022-11-21 18:08:15 -0800

    Add trim and decrement for digit lists.

---
+++ src/digit-list/trim.spec.ts
@@ -0,0 +1,33 @@
+import { $, Test, Kind, DigitList } from "..";
+
+type Trim_Spec = [
+  /**
+   * Trimming zero should result in zero.
+   */
+  Test.Expect<$<DigitList.Trim, ["0"]>, ["0"]>,
+
+  /**
+   * Trimming an empty zero should result in zero.
+   */
+  Test.Expect<$<DigitList.Trim, []>, ["0"]>,
+
+  /**
+   * Trimming a single digit should result in the same digit.
+   */
+  Test.Expect<$<DigitList.Trim, ["1"]>, ["1"]>,
+
+  /**
+   * Trimming a digit with leading zeros should result in the same digit.
+   */
+  Test.Expect<$<DigitList.Trim, ["0", "1"]>, ["1"]>,
+
+  /**
+   * Should be able to handle multiple leading zeros.
+   */
+  Test.Expect<$<DigitList.Trim, ["0", "0", "1"]>, ["1"]>,
+
+  /**
+   * Should be able to handle all zeroes.
+   */
+  Test.Expect<$<DigitList.Trim, ["0", "0", "0"]>, ["0"]>
+];
