0ad04b356ce7f472029d47d9d8f40c3ba607bfdb
o <poteat@users.noreply.github.com>
2022-11-27 11:47:22 -0800

    Add string length utility

---
+++ src/string/length.ts
@@ -0,0 +1,11 @@
+import { Type, Kind, String } from "..";
+
+export type _$length<S extends string> = String._$isTemplate<S> extends true
+  ? number
+  : string extends S
+  ? number
+  : String._$toList<S>["length"];
+
+export declare abstract class Length extends Kind.Kind {
+  abstract f: (x: Type._$cast<this[Kind._], string>) => _$length<typeof x>;
+}
