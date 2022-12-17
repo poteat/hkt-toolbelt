cf313a1940b2b85a450a6b5663bc751942bab494
o <poteat@users.noreply.github.com>
2022-12-16 18:24:43 -0800

    Add letter parser

---
+++ src/parser/letter.spec.ts
@@ -0,0 +1,8 @@
+import { $, Test, Parser } from "..";
+
+type Letter_Spec = [
+  /**
+   * A letter parser can be applied to a string input.
+   */
+  Test.Expect<$<$<Parser.Run, Parser.Letter>, "hello world">, "h">
+];
---
+++ src/parser/letter.ts
@@ -0,0 +1,77 @@
+import { Parser, Kind, String, NaturalNumber, Type } from "..";
+
+type __letter =
+  | "a"
+  | "b"
+  | "c"
+  | "d"
+  | "e"
+  | "f"
+  | "g"
+  | "h"
+  | "i"
+  | "j"
+  | "k"
+  | "l"
+  | "m"
+  | "n"
+  | "o"
+  | "p"
+  | "q"
+  | "r"
+  | "s"
+  | "t"
+  | "u"
+  | "v"
+  | "w"
+  | "x"
+  | "y"
+  | "z"
+  | "A"
+  | "B"
+  | "C"
+  | "D"
+  | "E"
+  | "F"
+  | "G"
+  | "H"
+  | "I"
+  | "J"
+  | "K"
+  | "L"
+  | "M"
+  | "N"
+  | "O"
+  | "P"
+  | "Q"
+  | "R"
+  | "S"
+  | "T"
+  | "U"
+  | "V"
+  | "W"
+  | "X"
+  | "Y"
+  | "Z";
+
+export type _$letter<
+  STATE extends Parser._$state,
+  SLICED_INPUT extends string = String._$slice<STATE["input"], STATE["index"]>,
+  MATCH_RESULT extends string = SLICED_INPUT extends `${infer L}${string}`
+    ? L extends __letter
+      ? L
+      : never
+    : never
+> = MATCH_RESULT extends never
+  ? never
+  : {
+      input: STATE["input"];
+      index: NaturalNumber._$increment<STATE["index"]>;
+      result: MATCH_RESULT;
+    };
+
+export declare abstract class Letter extends Parser.Parser {
+  abstract f: (
+    x: Type._$cast<this[Kind._], Parser._$state>
+  ) => _$letter<typeof x>;
+}
