f89a5802c7bd633433cdf608aaaa751a814b65b1
o <poteat@users.noreply.github.com>
2022-12-16 18:06:32 -0800

    Implement object emplacement

--- src/object/index.ts
+++ src/object/index.ts
@@ -2,6 +2,7 @@ export * from "./at-path";
 export * from "./at";
 export * from "./deep-input-of";
 export * from "./deep-map-values";
+export * from "./emplace";
 export * from "./keys";
 export * from "./map-keys";
 export * from "./map-values";
