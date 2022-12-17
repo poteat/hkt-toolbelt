a58a74fac721c51c020dbe2048a0ec74938722fe
o <poteat@users.noreply.github.com>
2022-12-17 14:13:11 -0800

    Implement TCO and 01 performance tricks for FromList

--- src/string/from-list.spec.ts
+++ src/string/from-list.spec.ts
@@ -14,5 +14,58 @@ type FromList_Spec = [
   /**
    * "foo", "bar" => foobar
    */
-  Test.Expect<$<String.FromList, ["foo", "bar"]>, "foobar">
+  Test.Expect<$<String.FromList, ["foo", "bar"]>, "foobar">,
+
+  /**
+   * Can join 10 elements.
+   */
+  Test.Expect<$<String.FromList, Ten>, TenString>,
+
+  /**
+   * Can join 100 elements.
+   */
+  Test.Expect<$<String.FromList, Hundred>, HundredString>,
+
+  /**
+   * Can join 1000 elements.
+   */
+  Test.Expect<$<String.FromList, Thousand>, ThousandString>
 ];
+
+type Ten = ["a", "b", "c", "d", "e", "f", "g", "h", "i", " "];
+
+type Hundred = [
+  ...Ten,
+  ...Ten,
+  ...Ten,
+  ...Ten,
+  ...Ten,
+  ...Ten,
+  ...Ten,
+  ...Ten,
+  ...Ten,
+  ...Ten
+];
+
+type Thousand = [
+  ...Hundred,
+  ...Hundred,
+  ...Hundred,
+  ...Hundred,
+  ...Hundred,
+  ...Hundred,
+  ...Hundred,
+  ...Hundred,
+  ...Hundred,
+  ...Hundred
+];
+
+type TenString = "abcdefghi ";
+
+type HundredString =
+  `${TenString}${TenString}${TenString}${TenString}${TenString}${TenString}${TenString}${TenString}${TenString}${TenString}`;
+
+type ThousandString =
+  `${HundredString}${HundredString}${HundredString}${HundredString}${HundredString}${HundredString}${HundredString}${HundredString}${HundredString}${HundredString}`;
+
+type X = $<String.FromList, Hundred>;
--- src/string/from-list.ts
+++ src/string/from-list.ts
@@ -1,11 +1,10 @@
 import { Type, Kind } from "..";
 
-export type _$fromList<T extends string[]> = T extends [
-  infer Head extends string,
-  ...infer Tail extends string[]
-]
-  ? `${Head}${_$fromList<Tail>}`
-  : "";
+export type _$fromList<T, O extends string = ""> = 0 extends 1
+  ? never
+  : T extends [infer Head, ...infer Tail]
+  ? _$fromList<Tail, `${O}${Type._$cast<Head, string>}`>
+  : O;
 
 export abstract class FromList extends Kind.Kind {
   abstract f: (x: Type._$cast<this[Kind._], string[]>) => _$fromList<typeof x>;
