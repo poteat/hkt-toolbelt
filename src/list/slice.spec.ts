5519fa0bcfe9c93d09a851bce768d75456be19a7
o <poteat@users.noreply.github.com>
2022-12-17 13:58:05 -0800

    Increase performance of List.Slice

--- src/list/slice.spec.ts
+++ src/list/slice.spec.ts
@@ -49,5 +49,98 @@ type Slice_Spec = [
   /**
    * Negative slices are not allowed.
    */
-  Test.Expect<$<$<List.Slice, -1>, ["a", "b", "c"]>, never>
+  Test.Expect<$<$<List.Slice, -1>, ["a", "b", "c"]>, never>,
+
+  /**
+   * Can execute zero slice against 10 elements.
+   */
+  Test.Expect<$<$<List.Slice, 0>, Ten>, Ten>,
+
+  /**
+   * Can execute zero slice against 100 elements.
+   */
+  Test.Expect<$<$<List.Slice, 0>, Hundred>, Hundred>,
+
+  /**
+   * Can execute zero slice against 1000 elements.
+   */
+  Test.Expect<$<$<List.Slice, 0>, Thousand>, Thousand>,
+
+  /**
+   * Can execute zero slice against 2000 elements.
+   */
+  Test.Expect<
+    $<$<List.Slice, 0>, [...Thousand, ...Thousand]>,
+    [...Thousand, ...Thousand]
+  >,
+
+  /**
+   * Can execute zero slice against 3000 elements.
+   */
+  Test.Expect<
+    $<$<List.Slice, 0>, [...Thousand, ...Thousand, ...Thousand]>,
+    [...Thousand, ...Thousand, ...Thousand]
+  >,
+
+  /**
+   * Can execute zero slice against 4000 elements.
+   */
+  Test.Expect<
+    $<$<List.Slice, 0>, [...Thousand, ...Thousand, ...Thousand, ...Thousand]>,
+    [...Thousand, ...Thousand, ...Thousand, ...Thousand]
+  >,
+
+  /**
+   * Can execute zero slice against 5000 elements.
+   */
+  Test.Expect<
+    $<
+      $<List.Slice, 0>,
+      [...Thousand, ...Thousand, ...Thousand, ...Thousand, ...Thousand]
+    >,
+    [...Thousand, ...Thousand, ...Thousand, ...Thousand, ...Thousand]
+  >,
+
+  /**
+   * Can execute 90 slice against 100 elements.
+   */
+  Test.Expect<$<$<List.Slice, 90>, Hundred>, Ten>,
+
+  /**
+   * Can execute 990 slice against 1000 elements.
+   */
+  Test.Expect<$<$<List.Slice, 990>, Thousand>, Ten>,
+
+  /**
+   * Can execute a 1990 slice against 2000 elements.
+   */
+  Test.Expect<$<$<List.Slice, 1990>, [...Thousand, ...Thousand]>, Ten>
+];
+
+type Ten = ["a", "b", "c", "d", "e", "f", "g", "h", "i", " "];
+
+type Hundred = [
+  ...Ten,
+  ...Ten,
+  ...Ten,
+  ...Ten,
+  ...Ten,
+  ...Ten,
+  ...Ten,
+  ...Ten,
+  ...Ten,
+  ...Ten
+];
+
+type Thousand = [
+  ...Hundred,
+  ...Hundred,
+  ...Hundred,
+  ...Hundred,
+  ...Hundred,
+  ...Hundred,
+  ...Hundred,
+  ...Hundred,
+  ...Hundred,
+  ...Hundred
 ];
