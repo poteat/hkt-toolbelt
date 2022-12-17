2d935281fa03d8dd4f46f37204200de7fd93ef9b
o <poteat@users.noreply.github.com>
2022-12-17 13:57:37 -0800

    Increase performance of String.ToList

---
+++ src/string/to-list.spec.ts
@@ -0,0 +1,112 @@
+import { $, Test, String } from "..";
+
+type ToList_Spec = [
+  /**
+   * "abc" => ["a", "b", "c"]
+   */
+  Test.Expect<$<String.ToList, "abc">, ["a", "b", "c"]>,
+
+  /**
+   * "" => []
+   */
+  Test.Expect<$<String.ToList, "">, []>,
+
+  /**
+   * "a" => ["a"]
+   */
+  Test.Expect<$<String.ToList, "a">, ["a"]>,
+
+  /**
+   * "ab" => ["a", "b"]
+   */
+  Test.Expect<$<String.ToList, "ab">, ["a", "b"]>,
+
+  /**
+   * Can split 10 elements.
+   */
+  Test.Expect<$<String.ToList, TenString>, Ten>,
+
+  /**
+   * Can split 100 elements.
+   */
+  Test.Expect<$<String.ToList, HundredString>, Hundred>,
+
+  /**
+   * Can split 1000 elements.
+   */
+  Test.Expect<$<String.ToList, ThousandString>, Thousand>,
+
+  /**
+   * Can split 2000 elements.
+   */
+  Test.Expect<
+    $<String.ToList, `${ThousandString}${ThousandString}`>,
+    [...Thousand, ...Thousand]
+  >,
+
+  /**
+   * Can split 3000 elements.
+   */
+  Test.Expect<
+    $<String.ToList, `${ThousandString}${ThousandString}${ThousandString}`>,
+    [...Thousand, ...Thousand, ...Thousand]
+  >,
+
+  /**
+   * Can split 4000 elements.
+   */
+  Test.Expect<
+    $<
+      String.ToList,
+      `${ThousandString}${ThousandString}${ThousandString}${ThousandString}`
+    >,
+    [...Thousand, ...Thousand, ...Thousand, ...Thousand]
+  >,
+
+  /**
+   * Can split 5000 elements.
+   */
+  Test.Expect<
+    $<
+      String.ToList,
+      `${ThousandString}${ThousandString}${ThousandString}${ThousandString}${ThousandString}`
+    >,
+    [...Thousand, ...Thousand, ...Thousand, ...Thousand, ...Thousand]
+  >
+];
+
+type Ten = ["a", "b", "c", "d", "e", "f", "g", "h", "i", " "];
+
+type Hundred = [
+  ...Ten,
+  ...Ten,
+  ...Ten,
+  ...Ten,
+  ...Ten,
+  ...Ten,
+  ...Ten,
+  ...Ten,
+  ...Ten,
+  ...Ten
+];
+
+type Thousand = [
+  ...Hundred,
+  ...Hundred,
+  ...Hundred,
+  ...Hundred,
+  ...Hundred,
+  ...Hundred,
+  ...Hundred,
+  ...Hundred,
+  ...Hundred,
+  ...Hundred
+];
+
+type TenString = "abcdefghi ";
+
+type HundredString =
+  `${TenString}${TenString}${TenString}${TenString}${TenString}${TenString}${TenString}${TenString}${TenString}${TenString}`;
+
+type ThousandString =
+  `${HundredString}${HundredString}${HundredString}${HundredString}${HundredString}${HundredString}${HundredString}${HundredString}${HundredString}${HundredString}`;
--- src/string/to-list.ts
+++ src/string/to-list.ts
@@ -1,9 +1,8 @@
 import { Type, Kind } from "..";
 
-export type _$toList<
-  S extends string,
-  O extends string[] = []
-> = string extends S
+export type _$toList<S extends string, O extends string[] = []> = 0 extends 1
+  ? never
+  : string extends S
   ? [string]
   : S extends `${infer Head}${infer Tail}`
   ? _$toList<Tail, [...O, Head]>
