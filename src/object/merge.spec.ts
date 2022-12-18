8252397a12135e24cc476a61a0084949e897fd49
o <poteat@users.noreply.github.com>
2022-12-17 16:09:25 -0800

    Add kind-level object merging

---
+++ src/object/merge.spec.ts
@@ -0,0 +1,21 @@
+import { $, Test, Object } from "..";
+
+type Merge_Spec = [
+  /**
+   * Can merge two objects.
+   */
+  Test.Expect<
+    $<$<Object.Merge, { a: 1; b: 2 }>, { c: 3; d: 4 }>,
+    { a: 1; b: 2; c: 3; d: 4 }
+  >,
+
+  /**
+   * Can merge with an empty object.
+   */
+  Test.Expect<$<$<Object.Merge, {}>, { a: 1; b: 2 }>, { a: 1; b: 2 }>,
+
+  /**
+   * The second object takes precedence.
+   */
+  Test.Expect<$<$<Object.Merge, { a: 1 }>, { a: 2 }>, { a: 2 }>
+];
