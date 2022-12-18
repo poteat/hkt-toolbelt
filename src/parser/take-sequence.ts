1cc814f6323740e6abbc303b7124c2264417f399
o <poteat@users.noreply.github.com>
2022-12-17 20:04:33 -0800

    Implement TakeSequence for parser

---
+++ src/parser/take-sequence.ts
@@ -0,0 +1,47 @@
+import { $, Kind, Type, Parser, List } from "..";
+
+export type _$takeSequence<
+  STATE extends Parser._$state,
+  PX extends ([Parser.Parser] | Parser.Parser)[],
+  RESULT = never,
+  ACTIVE_PX extends [Parser.Parser] | Parser.Parser = List._$first<PX>,
+  NEXT_PX extends ([Parser.Parser] | Parser.Parser)[] = Type._$cast<
+    List._$shift<PX>,
+    ([Parser.Parser] | Parser.Parser)[]
+  >,
+  NEXT_STATE extends Parser._$state = ACTIVE_PX extends [Parser.Parser]
+    ? $<ACTIVE_PX[0], Type._$cast<STATE, Kind._$inputOf<ACTIVE_PX[0]>>>
+    : ACTIVE_PX extends Parser.Parser
+    ? $<ACTIVE_PX, Type._$cast<STATE, Kind._$inputOf<ACTIVE_PX>>>
+    : never,
+  NEXT_RESULT extends unknown = ACTIVE_PX extends [Parser.Parser]
+    ? NEXT_STATE["result"]
+    : RESULT,
+  IS_DONE extends boolean = NEXT_PX extends []
+    ? true
+    : NEXT_STATE extends never
+    ? false
+    : false
+> = IS_DONE extends true
+  ? NEXT_STATE extends never
+    ? never
+    : {
+        input: STATE["input"];
+        index: NEXT_STATE["index"];
+        result: NEXT_RESULT;
+      }
+  : _$takeSequence<NEXT_STATE, NEXT_PX, NEXT_RESULT>;
+
+declare abstract class TakeSequence_T<
+  PX extends ([Parser.Parser] | Parser.Parser)[]
+> extends Kind.Kind {
+  abstract f: (
+    x: Type._$cast<this[Kind._], Parser._$state>
+  ) => _$takeSequence<typeof x, PX>;
+}
+
+export declare abstract class TakeSequence extends Kind.Kind {
+  abstract f: (
+    x: Type._$cast<this[Kind._], ([Parser.Parser] | Parser.Parser)[]>
+  ) => TakeSequence_T<typeof x>;
+}
