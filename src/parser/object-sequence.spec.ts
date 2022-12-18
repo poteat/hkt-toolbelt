d8b67a91bd7ebb7792531c3dc2e94662cc8ea2dd
o <poteat@users.noreply.github.com>
2022-12-17 16:19:30 -0800

    Add object-seq test case

--- src/parser/object-sequence.spec.ts
+++ src/parser/object-sequence.spec.ts
@@ -1,4 +1,4 @@
-import { $, Test, Parser } from "..";
+import { $, Test, Parser, Object } from "..";
 
 type ObjectSequence_Spec = [
   /**
@@ -20,5 +20,55 @@ type ObjectSequence_Spec = [
       "hello world"
     >,
     { foo: "hello"; bar: "world" }
+  >
+];
+
+type WeatherString = $<Parser.String, "Weather">;
+
+type TimeString = $<
+  $<Parser.Map, Object.At<"time">>,
+  $<
+    Parser.ObjectSequence,
+    [
+      $<Parser.String, "(">,
+      [
+        "time",
+        $<
+          Parser.Choice,
+          [$<Parser.String, "today">, $<Parser.String, "yesterday">]
+        >
+      ],
+      $<Parser.String, ")">
+    ]
+  >
+>;
+
+type WeatherType = $<
+  Parser.Choice,
+  [$<Parser.String, "Sunny">, $<Parser.String, "Cloudy">]
+>;
+
+type FullParser = $<
+  Parser.Run,
+  $<
+    Parser.ObjectSequence,
+    [
+      WeatherString,
+      $<Parser.String, " ">,
+      ["time", TimeString],
+      $<Parser.String, ": ">,
+      ["weather", WeatherType]
+    ]
+  >
+>;
+
+type FullParser_Spec = [
+  /**
+   * If we have a parser "Weather (today): Sunny", we can match "Weather (today)
+   * : Sunny".
+   */
+  Test.Expect<
+    $<FullParser, "Weather (today): Sunny">,
+    { time: "today"; weather: "Sunny" }
   >
 ];
