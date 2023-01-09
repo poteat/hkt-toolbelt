0c6ee24e380b89a4b7dfb23b571afa36c0b780e9
o <poteat@users.noreply.github.com>
2023-01-08 18:29:33 -0800

    Add many1 parser combinator

---
+++ src/parser/many1.stress.spec.ts
@@ -0,0 +1,14 @@
+import { $, Test, Parser, Stress } from "..";
+
+type Many1_StressSpec = [
+  /**
+   * Can match 100 characters.
+   */
+//   Test.Expect<
+//     $<
+//       $<Parser.Run, $<Parser.Many1, $<Parser.String, "x">>>,
+//       Stress.HundredString
+//     >,
+//     Stress.HundredTuple
+//   >
+];
---
+++ src/parser/string.stress.spec.ts
@@ -0,0 +1,33 @@
+import { $, Test, Parser, Stress } from ".."
+
+type String_StressSpec = [
+  /**
+   * Can match a string literal of 1000 length.
+   */
+  Test.Expect<
+    $<
+      $<Parser.String, Stress.ThousandString>,
+      { input: Stress.ThousandString; index: 0; result: never }
+    >,
+    {
+      input: Stress.ThousandString
+      index: 1_000
+      result: Stress.ThousandString
+    }
+  >,
+
+  /**
+   * Can perform a single match at the end of a 1000 length string.
+   */
+  Test.Expect<
+    $<
+      $<Parser.String, "x">,
+      { input: `${Stress.ThousandString}`; index: 999; result: never }
+    >,
+    {
+      input: `${Stress.ThousandString}`
+      index: 1_000
+      result: "x"
+    }
+  >
+]
