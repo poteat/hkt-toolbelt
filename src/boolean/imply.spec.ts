ad96c8614e31bf93fc2c10403e06154512646194
Jongsun Suh <34228073+MajorLift@users.noreply.github.com>
2023-01-11 08:20:48 -0800

    Implement boolean imply, nimply operators and test cases

---
+++ src/boolean/imply.spec.ts
@@ -0,0 +1,29 @@
+import { $, Boolean, Test } from "hkt-toolbelt";
+
+type Imply_Spec = [
+  /**
+   * True → True = True
+   */
+  Test.Expect<$<Boolean.Imply<true>, true>>,
+
+  /**
+   * True → False = False
+   */
+  Test.ExpectNot<$<Boolean.Imply<true>, false>>,
+
+  /**
+   * False → True = True
+   */
+  Test.Expect<$<Boolean.Imply<false>, true>>,
+
+  /**
+   * False → False = True
+   */
+  Test.Expect<$<Boolean.Imply<false>, false>>,
+
+  /**
+   * Running 'Imply' on a non-boolean type should emit an error.
+   */
+  // @ts-expect-error
+  Test.Expect<$<Boolean.Imply<true>, number>>
+];
