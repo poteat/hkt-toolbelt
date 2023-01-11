da71bd841dc257f909a1954db8154f623f227dcb
Jongsun Suh <34228073+MajorLift@users.noreply.github.com>
2023-01-11 08:06:21 -0800

    Implement boolean nand operator and test cases

---
+++ src/boolean/nand.spec.ts
@@ -0,0 +1,29 @@
+import { $, Boolean, Test } from "hkt-toolbelt";
+
+type Nand_Spec = [
+  /**
+   * True && True = False
+   */
+  Test.ExpectNot<$<Boolean.Nand<true>, true>>,
+
+  /**
+   * True && False = True
+   */
+  Test.Expect<$<Boolean.Nand<true>, false>>,
+
+  /**
+   * False && True = True
+   */
+  Test.Expect<$<Boolean.Nand<false>, true>>,
+
+  /**
+   * False && False = True
+   */
+  Test.Expect<$<Boolean.Nand<false>, false>>,
+
+  /**
+   * Running 'Nand' on a non-boolean type should emit an error.
+   */
+  // @ts-expect-error
+  Test.Expect<$<Boolean.Nand<true>, number>>
+];
