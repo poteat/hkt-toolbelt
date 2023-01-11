96cae3eb99bcbf73ee69e61cfa1f30c34f2423c5
Jongsun Suh <34228073+MajorLift@users.noreply.github.com>
2023-01-11 08:03:20 -0800

    Implement boolean nor operator and test cases

---
+++ src/boolean/nor.spec.ts
@@ -0,0 +1,23 @@
+import { $, Boolean, Test } from "hkt-toolbelt";
+
+type Nor_Spec = [
+  /**
+   * True || True = False
+   */
+  Test.ExpectNot<$<Boolean.Nor<true>, true>>,
+
+  /**
+   * True || False = False
+   */
+  Test.ExpectNot<$<Boolean.Nor<true>, false>>,
+
+  /**
+   * False || True = False
+   */
+  Test.ExpectNot<$<Boolean.Nor<false>, true>>,
+
+  /**
+   * False || False = True
+   */
+  Test.Expect<$<Boolean.Nor<false>, false>>
+];
