52bd209b787bd5263fb2a7d7c2365cdc79d8398a
Jongsun Suh <34228073+MajorLift@users.noreply.github.com>
2023-01-10 19:42:29 -0800

    Modify digit comparator for improved efficiency
    - Use LUT covering all cases for O(1) performance.
    - Avoid invoking multiple search, recursions for every digit compare operation.

--- src/digit/compare.ts
+++ src/digit/compare.ts
@@ -1,25 +1,21 @@
-import { $, Digit, DigitList, Kind, Type } from '../'
+import { $, Digit, Kind, Type } from '../'
 
-type _$compare_LUT = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
-
-type _$compare2<
-  A extends Digit.Digit,
-  B extends Digit.Digit,
-  PREV extends DigitList.DigitList = _$compare_LUT,
-  CURR extends Digit.Digit = DigitList._$first<PREV>,
-  IS_A extends boolean = A extends CURR ? true : false,
-  IS_B extends boolean = B extends CURR ? true : false,
-  NEXT extends DigitList.DigitList = DigitList._$shift<PREV>,
-  RESULT = IS_A extends true
-    ? IS_B extends true ? 0 : -1
-    : IS_B extends true ? 1 : _$compare2<A, B, NEXT>
-> = RESULT;
-
+type _$compare_LUT = [
+  [0, -1, -1, -1, -1, -1, -1, -1, -1, -1], 
+  [1,  0, -1, -1, -1, -1, -1, -1, -1, -1], 
+  [1,  1,  0, -1, -1, -1, -1, -1, -1, -1], 
+  [1,  1,  1,  0, -1, -1, -1, -1, -1, -1], 
+  [1,  1,  1,  1,  0, -1, -1, -1, -1, -1], 
+  [1,  1,  1,  1,  1,  0, -1, -1, -1, -1], 
+  [1,  1,  1,  1,  1,  1,  0, -1, -1, -1], 
+  [1,  1,  1,  1,  1,  1,  1,  0, -1, -1], 
+  [1,  1,  1,  1,  1,  1,  1,  1,  0, -1], 
+  [1,  1,  1,  1,  1,  1,  1,  1,  1,  0],
+];
 export type _$compare<
   A extends Digit.Digit,
   B extends Digit.Digit,
-  RESULT = _$compare2<A, B>
-> = RESULT
+> = _$compare_LUT[A][B]
 
 declare abstract class Compare_T<A extends Digit.Digit> extends Kind.Kind {
   abstract f: (x: Type._$cast<this[Kind._], Digit.Digit>) => _$compare<A, typeof x>;
