e85613cc44cb364e82ee4e03ab27d3b095fd4bfd
o <poteat@users.noreply.github.com>
2023-02-07 01:30:08 -0800

    Refactor type cast higher-kinded type

--- src/type/cast.ts
+++ src/type/cast.ts
@@ -2,6 +2,10 @@ import { Kind } from "..";
 
 export type _$cast<T, U> = T extends U ? T : U;
 
-export interface Cast<T> extends Kind.Kind {
+interface Cast_T<T> extends Kind.Kind {
   f(x: this[Kind._]): _$cast<typeof x, T>;
+}
+
+export interface Cast extends Kind.Kind {
+  f(x: this[Kind._]): Cast_T<typeof x>;
 }
