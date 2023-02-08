50bba9f2ae7619ff5bcfa99c168907935ed9b1ba
o <poteat@users.noreply.github.com>
2023-02-08 00:40:30 -0800

    Make arithmetic properly reductive

--- src/combinator/fix-sequence.ts
+++ src/combinator/fix-sequence.ts
@@ -2,17 +2,15 @@ import { $, Type, Kind } from "..";
 
 export type _$fixSequence<
   KIND extends Kind.Kind,
-  INITIAL extends Kind._$inputOf<KIND>,
-  VALUE extends Kind._$inputOf<KIND> = INITIAL,
-  STATE extends unknown[] = [INITIAL],
+  VALUE extends Kind._$inputOf<KIND>,
+  STATE extends unknown[] = [VALUE],
   NEXT_VALUE = $<KIND, VALUE>,
   NEXT_STATE extends unknown[] = [...STATE, NEXT_VALUE],
   DONE extends boolean = NEXT_VALUE extends VALUE ? true : false
 > = DONE extends true
   ? STATE
   : _$fixSequence<
-      KIND,
-      INITIAL,
+      DONE extends false ? KIND : never,
       Type._$cast<NEXT_VALUE, Kind._$inputOf<KIND>>,
       NEXT_STATE
     >;
