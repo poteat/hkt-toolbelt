75a55c2217a8732d9b15d2c7643e525210921823
o <poteat@users.noreply.github.com>
2023-02-08 00:40:30 -0800

    Make arithmetic properly reductive

--- src/digit-list/multiply.ts
+++ src/digit-list/multiply.ts
@@ -5,18 +5,19 @@ export type _$multiply2<
   B extends DigitList.DigitList,
   SCALE extends DigitList.DigitList = [],
   SUM extends DigitList.DigitList = [],
-  LAST_B extends Digit.Digit = DigitList._$last<B>,
-  POP_B extends DigitList.DigitList = DigitList._$pop<B>,
-  MUL extends DigitList.DigitList = DigitList._$multiplyDigit<A, LAST_B>,
+  LAST_A extends Digit.Digit = DigitList._$last<A>,
+  POP_A extends DigitList.DigitList = DigitList._$pop<A>,
+  MUL extends DigitList.DigitList = DigitList._$multiplyDigit<B, LAST_A>,
   MUL_SCALE extends DigitList.DigitList = [...MUL, ...SCALE],
   ADD extends DigitList.DigitList = DigitList._$add<SUM, MUL_SCALE>,
-  NEXT_SCALE extends DigitList.DigitList = [Digit.Zero, ...SCALE]
-> = B extends [] ? SUM : _$multiply2<A, POP_B, NEXT_SCALE, ADD>;
+  NEXT_SCALE extends DigitList.DigitList = [Digit.Zero, ...SCALE],
+  DONE extends boolean = A extends [] ? true : false
+> = DONE extends true ? SUM : _$multiply2<POP_A, B, NEXT_SCALE, ADD>;
 
 export type _$multiply<
   A extends DigitList.DigitList,
   B extends DigitList.DigitList
-> = Type._$cast<_$multiply2<A, B>, DigitList.DigitList>;
+> = DigitList._$trim<Type._$cast<_$multiply2<A, B>, DigitList.DigitList>>;
 
 export interface Multiply_T<T extends DigitList.DigitList> extends Kind.Kind {
   f(x: Type._$cast<this[Kind._], DigitList.DigitList>): _$multiply<typeof x, T>;
