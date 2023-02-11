150e5776f43855639d37706d006fd649887ebf56
Jongsun Suh <34228073+MajorLift@users.noreply.github.com>
2023-02-11 10:08:55 -0800

    Implement list flatten, flatten-n
    - `Array.prototype.flat()`.
    - FlattenN: Flattens list by N depth levels.
    - Flatten: Flattens list completely into a single-depth list.

---
+++ src/list/flatten.spec.ts
@@ -0,0 +1,39 @@
+import { $, Test, List } from ".."
+
+type Flatten_Spec = [
+  /**
+   * Flattening empty array results in empty array.
+   */
+  Test.Expect<$<List.Flatten, []>, []>,
+
+  /**
+   * Flattening array with no nested elements is identity.
+   */
+  Test.Expect<$<List.Flatten, ["a", "b", "c", "d"]>, ["a", "b", "c", "d"]>,
+
+  /**
+   * Flattens array with single-level nested elements.
+   */
+  Test.Expect<$<List.Flatten, ["a", ["b", "c", "d"]]>, ["a", "b", "c", "d"]>,
+
+  /**
+   * Flattens array with multiple higher-level nested elements.
+   */
+  Test.Expect<$<List.Flatten, ["a", ["b"], ["c", [["d"]]]]>, ["a", "b", "c", "d"]>,
+
+  /**
+   * Flattens nested array with elements of multiple types.
+   */
+  Test.Expect<$<List.Flatten, [[{ foo: 'bar'; 2: 10 }, 'foobar']]>, [{ foo: 'bar'; 2: 10 }, 'foobar']>,
+
+  /**
+   * Flattens array nested at 20 levels.
+   */
+  Test.Expect<$<List.Flatten, [[[[[[[[[[[[[[[[[[[["a"]]]]]]]]]]]]]]]]]]]]>, ["a"]>,
+
+  /**
+   * Emits an error if being applied to a non-tuple.
+   */
+  // @ts-expect-error
+  $<List.Flatten, number>,
+];
