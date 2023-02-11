5a648734f95da7efee88e4f8e5581e04057bff80
Jongsun Suh <34228073+MajorLift@users.noreply.github.com>
2023-02-11 10:01:11 -0800

    Implement list pop-n, shift-n methods
    - including stress tests based on `slice.stress.spec.ts`

---
+++ src/list/pop-n.spec.ts
@@ -0,0 +1,56 @@
+import { $, List, Test } from "hkt-toolbelt";
+
+type PopN_Spec = [
+  /**
+   * Can pop one or more elements from the tail of an array.
+   */
+  Test.Expect<$<$<List.PopN, 1>, ["a", "b", "c"]>, ["a", "b"]>,
+  Test.Expect<$<$<List.PopN, 2>, ["a", "b", "c"]>, ["a"]>,
+
+  /**
+   * Shift of zero is identity.
+   */
+  Test.Expect<$<$<List.PopN, 0>, ["a", "b", "c"]>, ["a", "b", "c"]>,
+
+  /**
+   * Can handle being applied to an empty array.
+   */
+  Test.Expect<$<$<List.PopN, 0>, []>, []>,
+  Test.Expect<$<$<List.PopN, 1>, []>, []>,
+
+  /**
+   * Can handle being applied to a single-element array.
+   */
+  Test.Expect<$<$<List.PopN, 1>, ["a"]>, []>,
+
+  /**
+   * Can execute high-N and overflow slices that result in empty array.
+   */
+  Test.Expect<$<$<List.PopN, 3>, ["a", "b", "c"]>, []>,
+  Test.Expect<$<$<List.PopN, 4>, ["a", "b", "c"]>, []>,
+  Test.Expect<$<$<List.PopN, 100>, ["a", "b", "c"]>, []>,
+
+  /**
+   * Can handle nested arrays.
+   */
+  Test.Expect<$<$<List.PopN, 1>, [1, [2, [3]]]>, [1]>,
+  Test.Expect<$<$<List.PopN, 1>, [[1, 2], [3]]>, [[1, 2]]>,
+  Test.Expect<$<$<List.PopN, 2>, [[1, 2], [3]]>, []>,
+
+  /**
+   * Can handle different element types.
+   */
+  Test.Expect<$<$<List.PopN, 3>, ["foo" | "bar", null, ["a"]]>, []>,
+
+  /**
+   * Non-natural number N are not allowed.
+   */
+  Test.Expect<$<$<List.PopN, 1.5>, [1, 2, 3]>, never>,
+  Test.Expect<$<$<List.PopN, -1>, [1, 2, 3]>, never>,
+
+  /**
+   * Emits an error if being applied to a non-tuple.
+   */
+  // @ts-expect-error
+  $<List.PopN<1>, number>,
+];
