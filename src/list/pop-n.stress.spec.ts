4a1bb91fcf2a39425a4933326d025c4dfb58721b
Jongsun Suh <34228073+MajorLift@users.noreply.github.com>
2023-02-11 10:01:11 -0800

    Implement list pop-n, shift-n methods
    - including stress tests based on `slice.stress.spec.ts`

---
+++ src/list/pop-n.stress.spec.ts
@@ -0,0 +1,103 @@
+import { $, Test, List, Stress } from ".."
+
+type PopN_Spec = [
+  /**
+   * Can execute zero pop against 10 elements.
+   */
+  Test.Expect<$<$<List.PopN, 0>, Stress.TenTuple>, Stress.TenTuple>,
+
+  /**
+   * Can execute zero pop against 100 elements.
+   */
+  Test.Expect<$<$<List.PopN, 0>, Stress.HundredTuple>, Stress.HundredTuple>,
+
+  /**
+   * Can execute zero pop against 1000 elements.
+   */
+  Test.Expect<$<$<List.PopN, 0>, Stress.ThousandTuple>, Stress.ThousandTuple>,
+
+  /**
+   * Can execute zero pop against 2000 elements.
+   */
+  Test.Expect<
+    $<$<List.PopN, 0>, [...Stress.ThousandTuple, ...Stress.ThousandTuple]>,
+    [...Stress.ThousandTuple, ...Stress.ThousandTuple]
+  >,
+
+  /**
+   * Can execute zero pop against 3000 elements.
+   */
+  Test.Expect<
+    $<
+      $<List.PopN, 0>,
+      [
+        ...Stress.ThousandTuple,
+        ...Stress.ThousandTuple,
+        ...Stress.ThousandTuple
+      ]
+    >,
+    [...Stress.ThousandTuple, ...Stress.ThousandTuple, ...Stress.ThousandTuple]
+  >,
+
+  /**
+   * Can execute zero pop against 4000 elements.
+   */
+  Test.Expect<
+    $<
+      $<List.PopN, 0>,
+      [
+        ...Stress.ThousandTuple,
+        ...Stress.ThousandTuple,
+        ...Stress.ThousandTuple,
+        ...Stress.ThousandTuple
+      ]
+    >,
+    [
+      ...Stress.ThousandTuple,
+      ...Stress.ThousandTuple,
+      ...Stress.ThousandTuple,
+      ...Stress.ThousandTuple
+    ]
+  >,
+
+  /**
+   * Can execute zero pop against 5000 elements.
+   */
+  Test.Expect<
+    $<
+      $<List.PopN, 0>,
+      [
+        ...Stress.ThousandTuple,
+        ...Stress.ThousandTuple,
+        ...Stress.ThousandTuple,
+        ...Stress.ThousandTuple,
+        ...Stress.ThousandTuple
+      ]
+    >,
+    [
+      ...Stress.ThousandTuple,
+      ...Stress.ThousandTuple,
+      ...Stress.ThousandTuple,
+      ...Stress.ThousandTuple,
+      ...Stress.ThousandTuple
+    ]
+  >,
+
+  /**
+   * Can execute 90 pop against 100 elements.
+   */
+  Test.Expect<$<$<List.PopN, 90>, Stress.HundredTuple>, Stress.TenTuple>,
+
+  /**
+   * Can execute 990 pop against 1000 elements.
+   */
+  Test.Expect<$<$<List.PopN, 990>, Stress.ThousandTuple>, Stress.TenTuple>,
+
+  /**
+   * Can execute a 1990 pop against 2000 elements.
+   */
+  Test.Expect<
+    $<$<List.PopN, 1990>, [...Stress.ThousandTuple, ...Stress.ThousandTuple]>,
+    Stress.TenTuple
+  >
+]
---
+++ src/list/shift-n.stress.spec.ts
@@ -0,0 +1,103 @@
+import { $, Test, List, Stress } from ".."
+
+type ShiftN_Spec = [
+  /**
+   * Can execute zero shift against 10 elements.
+   */
+  Test.Expect<$<$<List.ShiftN, 0>, Stress.TenTuple>, Stress.TenTuple>,
+
+  /**
+   * Can execute zero shift against 100 elements.
+   */
+  Test.Expect<$<$<List.ShiftN, 0>, Stress.HundredTuple>, Stress.HundredTuple>,
+
+  /**
+   * Can execute zero shift against 1000 elements.
+   */
+  Test.Expect<$<$<List.ShiftN, 0>, Stress.ThousandTuple>, Stress.ThousandTuple>,
+
+  /**
+   * Can execute zero shift against 2000 elements.
+   */
+  Test.Expect<
+    $<$<List.ShiftN, 0>, [...Stress.ThousandTuple, ...Stress.ThousandTuple]>,
+    [...Stress.ThousandTuple, ...Stress.ThousandTuple]
+  >,
+
+  /**
+   * Can execute zero shift against 3000 elements.
+   */
+  Test.Expect<
+    $<
+      $<List.ShiftN, 0>,
+      [
+        ...Stress.ThousandTuple,
+        ...Stress.ThousandTuple,
+        ...Stress.ThousandTuple
+      ]
+    >,
+    [...Stress.ThousandTuple, ...Stress.ThousandTuple, ...Stress.ThousandTuple]
+  >,
+
+  /**
+   * Can execute zero shift against 4000 elements.
+   */
+  Test.Expect<
+    $<
+      $<List.ShiftN, 0>,
+      [
+        ...Stress.ThousandTuple,
+        ...Stress.ThousandTuple,
+        ...Stress.ThousandTuple,
+        ...Stress.ThousandTuple
+      ]
+    >,
+    [
+      ...Stress.ThousandTuple,
+      ...Stress.ThousandTuple,
+      ...Stress.ThousandTuple,
+      ...Stress.ThousandTuple
+    ]
+  >,
+
+  /**
+   * Can execute zero shift against 5000 elements.
+   */
+  Test.Expect<
+    $<
+      $<List.ShiftN, 0>,
+      [
+        ...Stress.ThousandTuple,
+        ...Stress.ThousandTuple,
+        ...Stress.ThousandTuple,
+        ...Stress.ThousandTuple,
+        ...Stress.ThousandTuple
+      ]
+    >,
+    [
+      ...Stress.ThousandTuple,
+      ...Stress.ThousandTuple,
+      ...Stress.ThousandTuple,
+      ...Stress.ThousandTuple,
+      ...Stress.ThousandTuple
+    ]
+  >,
+
+  /**
+   * Can execute 90 shift against 100 elements.
+   */
+  Test.Expect<$<$<List.ShiftN, 90>, Stress.HundredTuple>, Stress.TenTuple>,
+
+  /**
+   * Can execute 990 shift against 1000 elements.
+   */
+  Test.Expect<$<$<List.ShiftN, 990>, Stress.ThousandTuple>, Stress.TenTuple>,
+
+  /**
+   * Can execute a 1990 shift against 2000 elements.
+   */
+  Test.Expect<
+    $<$<List.ShiftN, 1990>, [...Stress.ThousandTuple, ...Stress.ThousandTuple]>,
+    Stress.TenTuple
+  >
+]
