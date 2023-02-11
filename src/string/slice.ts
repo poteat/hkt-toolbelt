1a28244102d62a92005878e6ee3690366f9bcaae
Jongsun Suh <34228073+MajorLift@users.noreply.github.com>
2023-02-11 10:16:21 -0800

    Feat!: Refactor `String.Slice` to use new `List.ShiftN`method

--- src/string/slice.ts
+++ src/string/slice.ts
@@ -3,7 +3,7 @@ import { Kind, Type, String, Number, List } from "..";
 export type _$slice<
   S extends string,
   N extends Number.Number
-> = String._$fromList<List._$slice<String._$toList<S>, N>>;
+> = String._$fromList<List._$shiftN<String._$toList<S>, N>>;
 
 interface Slice_T<N extends Number.Number> extends Kind.Kind {
   f(x: Type._$cast<this[Kind._], string>): _$slice<typeof x, N>;
