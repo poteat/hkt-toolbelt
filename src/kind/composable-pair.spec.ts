738be9e6a82af57a483e9be65eb9f2b4f680a758
o <poteat@users.noreply.github.com>
2023-03-01 19:21:12 -0800

    Add test case for composable pair type

---
+++ src/kind/composable-pair.spec.ts
@@ -0,0 +1,21 @@
+import { $, String, Test, Kind, List } from "..";
+
+type ComposablePair_Spec = [
+  /**
+   * Simple list operations should be composable.
+   */
+  Test.Expect<
+    Kind._$composablePair<[$<List.Push, "foo">, List.Unshift<"bar">]>
+  >,
+
+  /**
+   * A list push (on strings) and a string.join should be composable.
+   */
+  Test.Expect<Kind._$composablePair<[$<String.Join, "">, $<List.Push, "foo">]>>,
+
+  /**
+   * The empty tuple should emit an error.
+   */
+  // @ts-expect-error
+  Kind._$composablePair<[]>
+];
