0c25ae24c411f9c8a5f415bf2970e62b41640a60
o <poteat@users.noreply.github.com>
2023-03-01 19:48:01 -0800

    Improve docs on Nand utility

--- src/boolean/nand.ts
+++ src/boolean/nand.ts
@@ -1,5 +1,30 @@
 import { Type, Kind } from "..";
 
+/**
+ * `_nand` is a type-level function that takes in two boolean types, `T` and
+ * `U`, and returns the boolean result of applying the 'nand' logical operation
+ * on `T` and `U`. If both `T` and `U` are true, then `_nand` returns false,
+ * otherwise it returns true.
+ *
+ * ## Parameters
+ *
+ * @param T A boolean type.
+ * @param U A boolean type.
+ *
+ * ## Example
+ *
+ * @example
+ *
+ * For example, we can use `_nand` to determine whether two boolean types are
+ * not both true. In this example, `true` and `false` are passed as type
+ * arguments to the type-level function:
+ *
+ * ```ts
+ * import { _nand } from "hkt-toolbelt";
+ *
+ * type Result = _nand<true, false>; // true
+ * ```
+ */
 export type _$nand<T extends boolean, U extends boolean> = [T, U] extends [
   true,
   true
@@ -11,6 +36,29 @@ interface Nand_T<T extends boolean> extends Kind.Kind {
   f(x: Type._$cast<this[Kind._], boolean>): _$nand<T, typeof x>;
 }
 
+/**
+ * `Nand` is a type-level function that takes in two boolean types, `T` and
+ * `U`, and returns the boolean result of applying the 'nand' logical operation
+ * on `T` and `U`.
+ *
+ * @param T A boolean type.
+ * @param U A boolean type.
+ *
+ * @example
+ *
+ * For example, we can use `Nand` to determine whether two boolean types are
+ * not both true. In this example, `true` and `false` are passed as type
+ * arguments to the type-level function:
+ *
+ * We apply `Nand` to `true` and `false` respectively using the `$` type-level
+ * applicator:
+ *
+ * ```ts
+ * import { $, Nand } from "hkt-toolbelt";
+ *
+ * type Result = $<$<Nand, true>, false>; // true
+ * ```
+ */
 export interface Nand extends Kind.Kind {
   f(x: Type._$cast<this[Kind._], boolean>): Nand_T<typeof x>;
 }
