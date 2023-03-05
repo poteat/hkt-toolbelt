9baa92b7d157a50202ea8d83fe8a8ff122642aae
o <poteat@users.noreply.github.com>
2023-03-05 00:22:47 -0800

    Add doc on recursive kind subtype

--- src/combinator/recursive-kind.ts
+++ src/combinator/recursive-kind.ts
@@ -1,5 +1,13 @@
 import { Type, Kind } from "..";
 
+/**
+ * `RecursiveKind` is a higher-order type-level function that serves as a
+ * subtype. It is used to express the fact that some other higher-order kind
+ * takes in itself as a type argument.
+ *
+ * This type-level function doesn't do much on its own, but it is useful in
+ * combination with other type-level functions.
+ */
 export interface RecursiveKind extends Kind.Kind {
   f(x: Type._$cast<this[Kind._], RecursiveKind>): unknown;
 }
