6383e7dc4deb08b50df4abfbfcd0b8d99030cf4c
o <poteat@users.noreply.github.com>
2023-03-08 14:32:26 -0800

    Add "IsLessThan" natural number comparator

---
+++ src/natural-number/is-less-than.spec.ts
@@ -0,0 +1,24 @@
+import { $, NaturalNumber, Test } from "hkt-toolbelt";
+
+type IsLessThan_Spec = [
+  /**
+   * 2 is less than 3.
+   */
+  Test.Expect<$<$<NaturalNumber.IsLessThan, 3>, 2>>,
+
+  /**
+   * 3 is not less than 3.
+   */
+  Test.ExpectNot<$<$<NaturalNumber.IsLessThan, 3>, 3>>,
+
+  /**
+   * 4 is not less than 3.
+   */
+  Test.ExpectNot<$<$<NaturalNumber.IsLessThan, 3>, 4>>,
+
+  /**
+   * Running 'IsLessThan' on a non-number type should emit an error.
+   */
+  // @ts-expect-error
+  Test.Expect<$<$<NaturalNumber.IsLessThan, boolean>, 2>>
+];
