c7db5eff4b06c40a1e913885fcc872187473d972
o <poteat@users.noreply.github.com>
2023-03-08 14:36:24 -0800

    Add $N utility

---
+++ src/$/$N.spec.ts
@@ -0,0 +1,17 @@
+import { $, $N, Function, NaturalNumber, Conditional, Test } from "hkt-toolbelt"
+
+type IsLessThan5 = $N<
+  Conditional.If,
+  [
+    $<NaturalNumber.IsLessThan, 5>,
+    $<Function.Constant, "yes">,
+    $<Function.Constant, "no">
+  ]
+>
+
+type $N_Spec = [
+  /**
+   * 4 less than 5 => yes
+   */
+  Test.Expect<$<IsLessThan5, 4>, "yes">
+]
--- src/$/index.ts
+++ src/$/index.ts
@@ -1,2 +1,3 @@
-export * from "./$";
-export * from "./$$";
+export * from "./$"
+export * from "./$$"
+export * from "./$N"
