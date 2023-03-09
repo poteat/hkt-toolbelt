4c3feb1f79d691825248d7cdad9833a9405dba42
o <poteat@users.noreply.github.com>
2023-03-08 21:36:19 -0800

    Add jsdoc for not-equals type

--- src/conditional/not-equals.ts
+++ src/conditional/not-equals.ts
@@ -1,11 +1,32 @@
-import { Kind } from "..";
+import { Kind } from ".."
 
-export type _$notEquals<T, U> = [T, U] extends [U, T] ? false : true;
+/**
+ * `_$notEquals` is a type-level function that returns `true` if `T` and `U` are
+ * not equal. Otherwise, it returns `false`.
+ *
+ * @param T The first type to compare.
+ * @param U The second type to compare.
+ */
+export type _$notEquals<T, U> = [T, U] extends [U, T] ? false : true
 
 interface NotEquals_T<T> extends Kind.Kind {
-  f(x: this[Kind._]): _$notEquals<T, typeof x>;
+  f(x: this[Kind._]): _$notEquals<T, typeof x>
 }
 
+/**
+ * `NotEquals` is a type-level function that returns `true` if `T` and `U` are
+ * not equal. Otherwise, it returns `false`.
+ *
+ * @param T The first type to compare.
+ * @param U The second type to compare.
+ *
+ * @example
+ * ```ts
+ * import { $, Conditional } from 'hkt-toolbelt'
+ *
+ * type Result = $<Conditional.NotEquals, "foo", "bar"> // true
+ * ```
+ */
 export interface NotEquals extends Kind.Kind {
-  f(x: this[Kind._]): NotEquals_T<typeof x>;
+  f(x: this[Kind._]): NotEquals_T<typeof x>
 }
