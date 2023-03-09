5bf9cdf61b40609b954e92d5a72d0ac5e8bd2a40
o <poteat@users.noreply.github.com>
2023-03-08 22:02:56 -0800

    Remove need for breaking change

--- src/natural-number/modulo.spec.ts
+++ src/natural-number/modulo.spec.ts
@@ -9,17 +9,17 @@ type Modulo_Spec = [
   /**
    * 10 % 2 = 0. The remainder is 0.
    */
-  Test.Expect<$<$<NaturalNumber.Modulo, 2>, 10>, 0>,
+  Test.Expect<$<$<NaturalNumber.Modulo, 10>, 2>, 0>,
 
   /**
    * 123 % 17 = 4.
    */
-  Test.Expect<$<$<NaturalNumber.Modulo, 17>, 123>, 4>,
+  Test.Expect<$<$<NaturalNumber.Modulo, 123>, 17>, 4>,
 
   /**
    * 123 % 1 = 0. The remainder is 0.
    */
-  Test.Expect<$<$<NaturalNumber.Modulo, 1>, 123>, 0>,
+  Test.Expect<$<$<NaturalNumber.Modulo, 123>, 1>, 0>,
 
   /**
    * 123 % 123 = 0. The remainder is 0.
@@ -29,20 +29,20 @@ type Modulo_Spec = [
   /**
    * 100 % 25 = 0.
    */
-  Test.Expect<$<$<NaturalNumber.Modulo, 25>, 100>, 0>,
+  Test.Expect<$<$<NaturalNumber.Modulo, 100>, 25>, 0>,
 
   /**
    * 100 % 50 = 0.
    */
-  Test.Expect<$<$<NaturalNumber.Modulo, 50>, 100>, 0>,
+  Test.Expect<$<$<NaturalNumber.Modulo, 100>, 50>, 0>,
 
   /**
    * 100 % 75 = 25.
    */
-  Test.Expect<$<$<NaturalNumber.Modulo, 75>, 100>, 25>,
+  Test.Expect<$<$<NaturalNumber.Modulo, 100>, 75>, 25>,
 
   /**
    * 100 % 99 = 1.
    */
-  Test.Expect<$<$<NaturalNumber.Modulo, 99>, 100>, 1>
+  Test.Expect<$<$<NaturalNumber.Modulo, 100>, 99>, 1>
 ]
