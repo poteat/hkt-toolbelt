de627a1113ccf37cb04ec9fd84c447c4a346bb43
Michael Poteat <poteat@users.noreply.github.com>
2023-05-10 13:00:35 -0700

    Upgrade AI connectors

--- prompts/scripts/resolve-template.sh
+++ prompts/scripts/resolve-template.sh
@@ -3,17 +3,24 @@
 prompt_template="$1"
 content_file="$2"
 
+# Spec file is optional and of the form <content_file>.spec.ts
+new_extension=".spec.ts"
+spec_file="${content_file%.*}$new_extension"
+
 # Read contents of prompt_template
 prompt_template=$(<"$prompt_template")
 
 # Read contents of content_file
 content=$(<"$content_file")
 
+# Read contents of spec_file
+spec=$(<"$spec_file")
+
 # Replace <FILE_CONTENTS> with contents of content_file
 result="${prompt_template//'<FILE_CONTENTS>'/$'\n'$content}"
 
+# Replace <UNIT_TEST_CONTENTS> with contents of spec_file.
+result="${result//'<UNIT_TEST_CONTENTS>'/$'\n'$spec}"
+
 # Print result to stdout
 echo "$result"
-
-# Copy result to clipboard
-echo "$result" | pbcopy
