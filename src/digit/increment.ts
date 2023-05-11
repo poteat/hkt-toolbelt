e6cfe348069d797f2457888652b6952ba70cbb5e
poteat <poteat@users.noreply.github.com>
2023-05-10 18:47:52 -0700

    Finish documenting Digit namespace

--- src/digit/increment.ts
+++ src/digit/increment.ts
@@ -2,8 +2,48 @@ import { Type, Digit, Kind } from '..'
 
 type _$increment_LUT = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '0']
 
+/**
+ * `_$increment` is a type-level function that takes in a digit type `A` and
+ * returns the next digit in the sequence. If the input digit is "9", the
+ * function returns "0".
+ *
+ * ## Parameters
+ *
+ * @param A A digit type.
+ *
+ * ## Example
+ *
+ * @example
+ *
+ * For example, we can use `_$increment` to increment a digit type:
+ *
+ * ```ts
+ * import { Digit } from "hkt-toolbelt";
+ *
+ * type Result = Digit._$increment<"3">; // "4"
+ * ```
+ */
 export type _$increment<A extends Digit.Digit> = _$increment_LUT[A]
 
+/**
+ * `Increment` is a type-level function that takes in a digit type `A` and
+ * returns the next digit in the sequence. If the input digit is "9", the
+ * function returns "0".
+ *
+ * @param A A digit type.
+ *
+ * @example
+ *
+ * For example, we can use `Increment` to increment a digit type:
+ *
+ * We apply `Increment` to a digit type using the `$` type-level applicator:
+ *
+ * ```ts
+ * import { $, Digit } from "hkt-toolbelt";
+ *
+ * type Result = $<Digit.Increment, "3">; // "4"
+ * ```
+ */
 export interface Increment extends Kind.Kind {
   f(x: Type._$cast<this[Kind._], Digit.Digit>): _$increment<typeof x>
 }
--- src/digit/zero.ts
+++ src/digit/zero.ts
@@ -1,1 +1,21 @@
-export type Zero = "0";
+/**
+ * `Zero` is a type representing the digit zero ("0"). It is a subtype of the
+ * `Digit` type, and can be used in various arithmetic operations and
+ * comparisons provided by the `DigitList` and `NaturalNumber` namespaces.
+ *
+ * ## Example
+ *
+ * @example
+ *
+ * For example, we can use `Zero` in arithmetic operations like addition:
+ *
+ * ```ts
+ * import { $, DigitList, Digit } from "hkt-toolbelt";
+ *
+ * type Result = $<$<DigitList.Add, [Digit.Zero]>, ["1"]>; // ["1"]
+ * ```
+ *
+ * In this example, we add `Zero` to the digit "1", and the result is a digit
+ * list containing the digit "1".
+ */
+export type Zero = '0'
