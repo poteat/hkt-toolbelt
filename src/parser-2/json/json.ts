114f7f4a66bb13811a575c0176dd94dc5ca5ec51
poteat <poteat@users.noreply.github.com>
2023-05-12 12:57:49 -0700

    Add further point-free semantics

--- src/parser-2/json/json.ts
+++ src/parser-2/json/json.ts
@@ -11,16 +11,19 @@ type _$json = $<
   ]
 >
 
+type _$nestJSON = $<
+  Kind.Pipe,
+  [
+    Kind.Apply,
+    List.Map,
+    $<Kind.Apply, [Parser2.JSON.Array, Parser2.JSON.Object]>
+  ]
+>
+
 type JSON3 = $<_$json, []>
-type Array3 = $<Parser2.JSON.Array, JSON3>
-type Object3 = $<Parser2.JSON.Object, JSON3>
 
-type JSON2 = $<_$json, [Array3, Object3]>
-type Array2 = $<Parser2.JSON.Array, JSON2>
-type Object2 = $<Parser2.JSON.Object, JSON2>
+type JSON2 = $<_$json, $<_$nestJSON, JSON3>>
 
-type JSON1 = $<_$json, [Array2, Object2]>
-type Array1 = $<Parser2.JSON.Array, JSON1>
-type Object1 = $<Parser2.JSON.Object, JSON1>
+type JSON1 = $<_$json, $<_$nestJSON, JSON2>>
 
-export type JSON = $<_$json, [Array1, Object1]>
+export type JSON = $<_$json, $<_$nestJSON, JSON1>>
