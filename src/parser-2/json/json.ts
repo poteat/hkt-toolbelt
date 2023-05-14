6a37c3906ca2647fd748f4797713901888636fae
Michael Poteat <poteat@users.noreply.github.com>
2023-05-13 18:44:57 -0700

    Clean up JSON implementation

--- src/parser-2/json/json.ts
+++ src/parser-2/json/json.ts
@@ -1,8 +1,11 @@
-import { $, Parser2, List, Kind } from '../..'
+import { $, Parser2, List, Kind, Combinator } from '../..'
 
 type _$json = $<
   Kind.Pipe,
   [
+    Kind.Apply,
+    List.Map,
+    $<Kind.Apply, [Parser2.JSON.Array, Parser2.JSON.Object]>,
     $<
       List.ConcatBy,
       [Parser2.JSON.Boolean, Parser2.JSON.Null, Parser2.JSON.String]
@@ -11,44 +14,18 @@ type _$json = $<
   ]
 >
 
-type _$nestJSON = $<
-  Kind.Pipe,
-  [
-    Kind.Apply,
-    List.Map,
-    $<Kind.Apply, [Parser2.JSON.Array, Parser2.JSON.Object]>
-  ]
->
-
 type JSON7 = $<_$json, []>
 
-type JSON6 = $<_$json, $<_$nestJSON, JSON7>>
+type JSON6 = $<_$json, JSON7>
 
-type JSON5 = $<_$json, $<_$nestJSON, JSON6>>
+type JSON5 = $<_$json, JSON6>
 
-type JSON4 = $<_$json, $<_$nestJSON, JSON5>>
+type JSON4 = $<_$json, JSON5>
 
-type JSON3 = $<_$json, $<_$nestJSON, JSON4>>
+type JSON3 = $<_$json, JSON4>
 
-type JSON2 = $<_$json, $<_$nestJSON, JSON3>>
+type JSON2 = $<_$json, JSON3>
 
-type JSON1 = $<_$json, $<_$nestJSON, JSON2>>
+type JSON1 = $<_$json, JSON2>
 
-export type JSON = $<_$json, $<_$nestJSON, JSON1>>
-
-/**
- * The above is a 'hack' to get around the fact that I haven't been able to
- * figure out co-recursive higher-order types yet. The below is the 'correct'
- * version, but it doesn't work.
- */
-
-// export type MyJSON = $<
-//   Parser2.Choice,
-//   [
-//     Parser2.JSON.Boolean,
-//     Parser2.JSON.Null,
-//     Parser2.JSON.String,
-//     $<Parser2.JSON.Array, MyJSON>,
-//     $<Parser2.JSON.Object, MyJSON>
-//   ]
-// >
+export type JSON = $<_$json, JSON1>
