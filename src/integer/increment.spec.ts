83da14392ecd476ef1042355640224085f6e3d3b
Jongsun Suh <34228073+MajorLift@users.noreply.github.com>
2023-05-18 18:19:16 -0700

    Add `Increment`, `Decrement` for `Integer`.

---
+++ src/integer/increment.spec.ts
@@ -0,0 +1,29 @@
+import { $, Test, Integer } from ".."
+
+type Increment_Spec = [
+  /**
+   * Can increment a natural number of zero.
+   */
+  Test.Expect<$<Integer.Increment, 0>, 1>,
+
+  /**
+   * Can increment a natural number of one.
+   */
+  Test.Expect<$<Integer.Increment, 1>, 2>,
+
+  /**
+   * Can increment a negative number of 10.
+   */
+  Test.Expect<$<Integer.Increment, -10>, -9>,
+
+  /**
+   * We emit 'never' if the input is not an integer.
+   */
+  Test.Expect<$<Integer.Decrement, 1.5>, never>,
+
+  /**
+   * Can increment 9999 and -9999.
+   */
+  Test.Expect<$<Integer.Increment, 9999>, 10000>,
+  Test.Expect<$<Integer.Increment, -9999>, -9998>,
+]
