05aed3374afdd4dcda28d3c22a441b0a99b4bd20
Alex Onaci <alexandru.onaci@outlook.com>
2023-07-17 23:35:43 +0300

    - Added docs for digit list and divide by subsctraction

--- src/digit-list/digit-list.ts
+++ src/digit-list/digit-list.ts
@@ -1,3 +1,20 @@
 import { Digit } from "..";
 
+/**
+ * `DigitList` is a is a type alias representing a list of decimal
+ * digits from "0" to "9" (inclusive).
+ *
+ * ## Example
+ *
+ * @example
+ *
+* For example, we can use `DigitList` to represent a list of decimal digits from "0" to "9":
+*
+* ```ts
+* import { DigitList } from "hkt-toolbelt";
+*
+* type MyDigitList = DigitList; // ("0" | "1" | "2" | "3" | "4" | "5" | "6" | "7" | "8" | "9")[]
+* ```
+*
+*/
 export type DigitList = readonly Digit.Digit[];
--- src/digit-list/divide-by-subtraction.ts
+++ src/digit-list/divide-by-subtraction.ts
@@ -1,4 +1,4 @@
-import { Type, Kind, Digit, DigitList } from "..";
+import { $, Type, Kind, Digit, DigitList } from "..";
 
 type _$divideBySubtraction2<
   A extends DigitList.DigitList,
@@ -28,6 +28,36 @@ type _$divideBySubtraction2<
   ? RESULT
   : _$divideBySubtraction2<A, B, OPERATION, NEXT_QUOTIENT, NEXT_REMAINDER>;
 
+/**
+ * `_$divideBySubtraction` is a type-level function that performs the division or modulo operation by subtraction.
+ * It takes in two digit lists `A` and `B` representing the dividend and divisor respectively, and an operation type
+ * which can be either "DIVIDE" or "MODULO". It checks if the divisor is zero or one, and if so, returns the appropriate
+ * result. Otherwise, it calls the `_$divideBySubtraction2` function to perform the operation.
+ *
+ * @param A A digit list representing a number to divide.
+ * @param B A digit list representing a number to divide by.
+ * @param OPERATION A string type representing the operation to be performed. Can be either "DIVIDE" or "MODULO".
+ *
+ * @example
+ *
+ * For example, we can use `_$divideBySubtraction` to divide a digit list representing the number 10 by 2:
+ *
+ * ```ts
+ * import { DigitList } from "hkt-toolbelt";
+ *
+ * type Result = DigitList._$divideBySubtraction<["1", "0"], ["2"]>; // ["5"]
+ * ```
+ *
+ * @example
+ *
+ * We can also use `_$divideBySubtraction` to find the remainder when a digit list representing the number 123 is divided by 17:
+ *
+ * ```ts
+ * import { DigitList } from "hkt-toolbelt";
+ *
+ * type Result = DigitList._$divideBySubtraction<["1", "2", "3"], ["1", "7"], "MODULO">; // ["4"]
+ * ```
+ */
 export type _$divideBySubtraction<
   A extends DigitList.DigitList,
   B extends DigitList.DigitList,
@@ -47,6 +77,27 @@ interface DivideBySubtraction_T<A extends DigitList.DigitList>
   ): _$divideBySubtraction<A, typeof x>;
 }
 
+
+/**
+ * `DivideBySubtraction` is a type-level function that performs the division by subtraction.
+ * It takes in a digit list `A` representing the dividend and returns a function that takes in a DigitList representing
+ * the divisor.
+ *
+ * @param A A digit list representing a number to divide.
+ * @param B A digit list representing a number to divide by.
+
+ *
+ * @example
+ *
+ * For example, we can use `DivideBySubtraction` to divide a digit list representing the number 10 by 2:
+ *
+ * ```ts
+ * import { $, DigitList } from "hkt-toolbelt";
+ *
+ * type Result = $<$<DigitList.DivideBySubtraction, ["1", "0"]>, ["2"]>; // ["5"] quotient is 5, and the remainder is 0.
+ * ```
+ *
+ */
 export interface DivideBySubtraction extends Kind.Kind {
   f(
     x: Type._$cast<this[Kind._], DigitList.DigitList>
