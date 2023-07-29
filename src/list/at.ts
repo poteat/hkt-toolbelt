760d5c924e6b0bc251ff7cf5d1d12568aa9dba38
Jongsun Suh <34228073+MajorLift@users.noreply.github.com>
2023-07-29 12:21:23 -0700

    Add list at utility - Supports negative indices - Same functionality as ES2022's `Array.prototype.at`

---
+++ src/list/at.ts
@@ -0,0 +1,40 @@
+import {
+  $,
+  Digit,
+  DigitList,
+  Kind,
+  Type,
+  Number,
+  List,
+  NaturalNumber,
+  Boolean,
+} from "../"
+
+export type _$at<
+  T extends List.List,
+  POS extends Number.Number,
+  T_LENGTH extends DigitList.DigitList = NaturalNumber._$toList<T["length"]>,
+  POS_ABS extends DigitList.DigitList = NaturalNumber._$toList<
+    Number._$absolute<POS>
+  >,
+  POS_NORM extends DigitList.DigitList = Number._$isNatural<POS> extends true
+    ? DigitList._$compare<POS_ABS, T_LENGTH> extends -1
+      ? POS_ABS
+      : never
+    : DigitList._$compare<T_LENGTH, POS_ABS> extends -1
+    ? never
+    : DigitList._$subtract<T_LENGTH, POS_ABS>,
+  INDEX extends number = DigitList._$toNumber<POS_NORM>
+> = POS_NORM extends never
+  ? never
+  : T[INDEX]
+
+interface At_T<X extends Number.Number> extends Kind.Kind {
+  f(
+    x: Type._$cast<this[Kind._], unknown[]>
+  ): Number._$isInteger<X> extends true ? _$at<typeof x, X> : never
+}
+
+export interface At extends Kind.Kind {
+  f(x: Type._$cast<this[Kind._], Number.Number>): At_T<typeof x>
+}
