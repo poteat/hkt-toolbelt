f0e5b8e65db2d62bff329ee1ca3bae0e8e2cbdfb
Jongsun Suh <34228073+MajorLift@users.noreply.github.com>
2023-07-31 00:02:05 -0700

    Add number max and min utility
    - binary operations usable with reduce.

--- src/number/index.ts
+++ src/number/index.ts
@@ -4,6 +4,8 @@ export * from "./from-string";
 export * from "./is-fractional";
 export * from "./is-integer";
 export * from "./is-natural";
+export * from "./max";
+export * from "./min";
 export * from "./number";
 export * from "./sign";
 export * from "./to-string";
