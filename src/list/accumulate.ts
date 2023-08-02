b908c950c2ed6e0697dd2dec46a6eee885a2a5a3
Jongsun Suh <34228073+MajorLift@users.noreply.github.com>
2023-08-02 11:13:32 -0700

    Fix performance limitation of list `accumulate`
    - No longer relies on `List._$reduce`
    - Generic constraints with `Kind._$inputOf<F>`
    - Handles input length in the thousands.

--- src/list/accumulate.ts
+++ src/list/accumulate.ts
@@ -1,23 +1,28 @@
-import { $, Kind, Type, Function, List } from ".."
+import { $, $N, Kind, Type, List } from ".."
 
 export type _$accumulate<
   F extends Kind.Kind<(x: never) => Kind.Kind>,
   X extends List.List,
-  O,
-  CURR = $<List.First, X>,
-  REST extends List.List = $<List.Shift, X>,
-  ACC extends unknown = List._$reduce<F, [CURR], O>,
-  RESULT = X extends [] ? [] : List._$unshift<ACC, _$accumulate<F, REST, ACC>>
+  O extends Kind._$inputOf<F>,
+  M extends Kind._$inputOf<F>[] = [],
+  CURR = List._$first<X>,
+  REST extends List.List = List._$shift<X>,
+  ACC = $N<F, [O, CURR]>,
+  RESULT extends Kind._$inputOf<F>[] = X extends [] 
+    ? M
+    : ACC extends Kind._$inputOf<F> 
+      ? _$accumulate<F, REST, ACC, List._$push<ACC, M>> 
+      : never
 > = 0 extends 1 ? never : RESULT
 
-interface Accumulate_T2<F extends Kind.Kind<(x: never) => Kind.Kind>, X>
+interface Accumulate_T2<F extends Kind.Kind<(x: never) => Kind.Kind>, O extends Kind._$inputOf<F>>
   extends Kind.Kind {
-  f(x: Type._$cast<this[Kind._], unknown[]>): _$accumulate<F, typeof x, X>
+  f(x: Type._$cast<this[Kind._], List.List>): _$accumulate<F, typeof x, O>
 }
 
 interface Accumulate_T<F extends Kind.Kind<(x: never) => Kind.Kind>>
   extends Kind.Kind {
-  f(x: Type._$cast<this[Kind._], unknown>): Accumulate_T2<F, typeof x>
+  f(x: Type._$cast<this[Kind._], Kind._$inputOf<F>>): Accumulate_T2<F, typeof x>
 }
 
 export interface Accumulate extends Kind.Kind {
