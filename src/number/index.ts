1e906653f8e38f6493d9f9f1f2c61a267ff3572a
Jongsun Suh <34228073+MajorLift@users.noreply.github.com>
2023-08-01 23:22:29 -0700

    Add number negate utility
    - Example of `Combinator.RecursiveKind`

--- src/number/index.ts
+++ src/number/index.ts
@@ -4,6 +4,7 @@ export * from "./from-string";
 export * from "./is-fractional";
 export * from "./is-integer";
 export * from "./is-natural";
+export * from "./negate";
 export * from "./number";
 export * from "./sign";
 export * from "./to-string";
