06e15428f630ed2975136a11a65dea7bc67da937
Jongsun Suh <34228073+MajorLift@users.noreply.github.com>
2023-08-02 21:08:57 -0700

    Add kind unapply utility

---
+++ src/kind/unapply.spec.ts
@@ -0,0 +1,30 @@
+import {
+  $,
+  Kind,
+  Function,
+  Conditional,
+  NaturalNumber,
+  List,
+  Test,
+} from ".."
+
+type Add2 = $<NaturalNumber.Add, 2>
+type ReduceAdd = $<List.Reduce, NaturalNumber.Add>
+type ReduceAdd0 = $<ReduceAdd, 0>
+
+type Unapply_Spec = [
+  Test.Expect<
+    $<$<Kind.Unapply, NaturalNumber.Add>, Add2>, 
+    2
+  >,
+
+  Test.Expect<
+    $<$<Kind.Unapply, List.Reduce>, ReduceAdd>, 
+    NaturalNumber.Add
+  >,
+
+  Test.Expect<
+    $<$<Kind.Unapply, ReduceAdd>, ReduceAdd0>, 
+    0
+  >,
+]
