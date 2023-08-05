c3a6bf26dcb43c6efa2bcabb6a7c3feac4171295
Jongsun Suh <34228073+MajorLift@users.noreply.github.com>
2023-08-04 17:14:03 -0700

    Fixed error by returning true instead of never for empty array input
    - error TS2578: Unused '@ts-expect-error' directive.
    `src/test/expect.spec.ts`

--- src/conditional/extends-all.ts
+++ src/conditional/extends-all.ts
@@ -2,7 +2,7 @@ import { $, Type, Kind, List, Conditional, Boolean } from ".."
 
 /**
  * `_$extendsAll` is a type-level function that takes in an array of types `T` and a type `U`,
- * and returns `true` if and only if all elements of `T` extend `U`.
+ * and returns `true` if and only if all elements of `T` extend `U`, or `T` is empty.
  * Otherwise it returns `false`.
  *
  * ## Parameters
@@ -62,7 +62,7 @@ export type _$extendsAll<
   ? Conditional._$extends<U, CURR> extends false
     ? false
     : _$extendsAll<REST, U>
-  : never
+  : true
 
 interface ExtendsAll_T<U extends unknown> extends Kind.Kind {
   f(x: Type._$cast<this[Kind._], List.List>): _$extendsAll<typeof x, U>
@@ -72,6 +72,8 @@ interface ExtendsAll_T<U extends unknown> extends Kind.Kind {
  * `ExtendsAll` is a type-level function that takes in a type `U` and an array of types, `T`,
  * and returns a type-level function that returns `true` if all elements of `T` extend `U`,
  * and `false` if otherwise.
+ * 
+ * If T is empty, `true` is returned.
  *
  * @param U A type.
  * @param T An array of types.
