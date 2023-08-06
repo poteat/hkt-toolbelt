1b1bb8ac29ec94eef5e20012abd011ee8ad537cd
Jongsun Suh <34228073+MajorLift@users.noreply.github.com>
2023-08-06 09:50:29 -0700

    Add jsdoc for list map, filter, reduce

--- src/list/filter.ts
+++ src/list/filter.ts
@@ -1,5 +1,27 @@
 import { $, Type, Kind } from "..";
 
+/**
+ * `_$filter` is a type-level function that takes in two inputs:
+ * a partially-applied type-level predicate that expects one more argument and returns a boolean type, 
+ * and a target list of types upon which to perform the filtering operation.
+ * It returns a filtered list of types.
+ * 
+ * The type-level function input must be a unary, curried `Kind` type as defined in this library.
+ * @see {@link https://github.com/poteat/hkt-toolbelt/blob/main/docs/guides/custom-kinds.md} for details on how to create a custom kind.
+ * 
+ * @param F A type-level function that returns a boolean type indicating whether a type should be included in the result.
+ * @param X A list of types. The target of the filtering operation.
+ * 
+ * @example
+ * 
+ * For example, we can use `_$filter` to filter out negative elements from a tuple of numeric types:
+ * 
+ * ```ts
+ * import { List } from "hkt-toolbelt";
+ * 
+ * type FilteredNumbers = List._$filter<Conditional.IsPositive, [1, -2, 3, -4]>;  // [1, 3]
+ * ```
+ */
 export type _$filter<
   F extends Kind.Kind,
   X extends unknown[],
@@ -15,6 +37,70 @@ interface Filter_T<F extends Kind.Kind<(x: never) => boolean>>
   f(x: Type._$cast<this[Kind._], Kind._$inputOf<F>[]>): _$filter<F, typeof x>;
 }
 
+
+/**
+ * `Filter` is a type-level function that takes in two inputs:
+ * a partially-applied type-level predicate that expects one more argument and returns a boolean type, 
+ * and a target list of types upon which to perform the filtering operation.
+ * It returns a filtered list of types.
+ * 
+ * The type-level function input must be a unary, curried `Kind` type as defined in this library.
+ * @see {@link https://github.com/poteat/hkt-toolbelt/blob/main/docs/guides/custom-kinds.md} for details on how to create a custom kind.
+ * 
+ * @param F A type-level function that returns a boolean type indicating whether a type should be included in the result.
+ * @param X A list of types. The target of the filtering operation.
+ * 
+ * @example
+ * 
+ * For example, we can define a filter for positive numbers and then apply it to a list:
+ * 
+ * ```ts
+ * import { $, List, Conditional } from "hkt-toolbelt";
+ * 
+ * type FilteredNumbers = $<$<List.Filter, Conditional.IsPositive>, [1, -2, 3, -4]>;  // [1, 3]
+ * ```
+ * 
+ * @example
+ * 
+ * We can also use the `$N` applicator to invoke `Filter` with a list containing the required arguments
+ * This improves readability by allowing us to avoid nesting `$` calls. 
+ * 
+ * ```ts
+ * import { $N, List } from "hkt-toolbelt";
+ * 
+ * type FilterZeros = $N<List.Filter, [
+ *   $<Conditional.NotEquals, 0>,
+ *   [1, 0, 2, 0, 3]
+ * ]>;  // [1, 2, 3]
+ * ```
+ * 
+ * @example
+ * 
+ * By partially applying only the first argument to `Filter`, 
+ * we can define a type-level function that can apply the same operation to multiple list inputs.
+ * 
+ * ```ts
+ * import { $, List, Conditional } from hkt-toolbelt;
+ * 
+ * type FilterZeros = $<List.Filter, $<Conditional.NotEquals, 0>>;
+ * type AllZero = $<FilterZeros, [0, 0, 0, 0, 0]>;  // []
+ * type OneZero = $<FilterZeros, [0, 1, 2, 3, 4, 5]>;  // [1, 2, 3, 4, 5]
+ * ```
+ * 
+ * @example
+ * 
+ * Another use case for a partially-applied `Filter` function is to implement
+ * sophisticated higher-order functionality by passing it into other type-level functions. 
+ * 
+ * ```ts
+ * import { $, $$, List, Conditional, String } from "hkt-toolbelt";
+ * 
+ * type HelloWorld = $$<[
+ *   $<List.Filter, $<Conditional.Extends, string>>
+ *   $<String.Join, ", ">
+ * ], [42.42, null, "hello", undefined, "world"]>  // "hello, world"
+ * ```
+ */
 export interface Filter extends Kind.Kind {
   f(
     x: Type._$cast<this[Kind._], Kind.Kind<(x: never) => boolean>>
--- src/list/reduce.ts
+++ src/list/reduce.ts
@@ -1,5 +1,30 @@
 import { $, Kind, Type, Function } from "..";
 
+/**
+ * `_$reduce` is a type-level function that takes in three inputs: 
+ * a) a partially-applied type-level function for a pairwise operation that is expecting two more arguments, 
+ * b) a list of types, c) a type specifying the initial argument that will be passed into the partially-applied function,
+ * performs a reduce operation (also known as "fold") over the list input, 
+ * and returns the resulting type. 
+ * 
+ * The type-level function input must be a unary, curried `Kind` type as defined in this library, while being of arity 2 when uncurried.
+ * @see {@link https://github.com/poteat/hkt-toolbelt/blob/main/docs/guides/custom-kinds.md} for details on how to create a custom kind.
+ * 
+ * @param F A type-level function for a pairwise operation. 
+ * @param X A list of types. The target of the reduce operation.
+ * @param O A type specifying the initial argument that will be taken by `F`. 
+ *          To use first element of `X` as the initial argument, simply pass in `Function.Identity`.
+ * 
+ * @example
+ * 
+ * For example, we can use `_$reduce` to derive the sum of all elements in a list of numeric types. 
+ * 
+ * ```ts
+ * import { List } from "hkt-toolbelt";
+ * 
+ * type Sum1to10 = List._$reduce<NaturalNumber.Add, [1, 2, 3, 4, 5, 6, 7, 8, 9, 10], 0>;
+ * ```
+ */
 export type _$reduce<
   F extends Kind.Kind<(x: never) => Kind.Kind>,
   X extends unknown[],
@@ -32,6 +57,77 @@ interface Reduce_T<F extends Kind.Kind<(x: never) => Kind.Kind>>
   f(x: Type._$cast<this[Kind._], unknown>): Reduce_T2<F, typeof x>;
 }
 
+/**
+ * `Reduce` is a type-level function that takes in three inputs: 
+ * a) a partially-applied type-level function for a pairwise operation that is expecting two more arguments, 
+ * b) a type specifying the initial argument that will be passed into this partially-applied function,
+ * c) a target list of types, and performs a reduce operation (also known as "fold") over the target list,
+ * and returns the resulting type. 
+ * 
+ * The type-level function input must be a unary, curried `Kind` type as defined in this library, while being of arity 2 if uncurried.
+ * @see {@link https://github.com/poteat/hkt-toolbelt/blob/main/docs/guides/custom-kinds.md} for details on how to create a custom kind.
+ * 
+ * @param F A type-level function for a pairwise operation. 
+ * @param O A type specifying the initial argument that will be taken by `F`.
+ *          To use first element of `X` as the initial argument, simply pass in `Function.Identity`.
+ * @param X A list of types. The target of the reduce operation.
+ * 
+ * @example
+ * 
+ * For example, we can use `Reduce` to derive the sum of all elements in a list of numeric types. 
+ * 
+ * ```ts
+ * import { $, List } from "hkt-toolbelt";
+ * 
+ * type Sum1to10 = $<$<$<List.Reduce, NaturalNumber.Add>, 0>, [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]>;  // 55
+ * ```
+ * 
+ * @example
+ * 
+ * We can also use the `$N` applicator to invoke `Reduce` with a list containing the required arguments
+ * This improves readability by allowing us to avoid nesting `$` calls three level deep. 
+ * 
+ * ```ts
+ * import { $N, List } from "hkt-toolbelt";
+ * 
+ * type IsFalse = $N<List.Reduce, [
+ *   Boolean.Xor,
+ *   true,
+ *   [false, true, false, true]
+ * ]>;  // false
+ * ```
+ * 
+ * @example
+ * 
+ * By partially applying only the first two arguments to `Reduce`, 
+ * we can define a type-level function that can apply the same operation to multiple list inputs.
+ * 
+ * ```ts
+ * import { $, $N, List, Number } from hkt-toolbelt;
+ * 
+ * type GetMax = $N<List.Reduce, [Number.Max, Number.MIN_SAFE_INTEGER]>;
+ * type IsZero = $<GetMax, [-5, -4, -3, -2, -1, 0];  // 0
+ * type IsHundred = $<GetMax, [1, -1, 10, -10, 100, -100]>;  // 100
+ * ```
+ * 
+ * @example
+ * 
+ * Another use case for a partially-applied `Reduce` function is to implement
+ * sophisticated higher-order functionality by passing it into other type-level functions. 
+ * 
+ * ```ts
+ * import { $, $N, List, Conditional, Number, String } from "hkt-toolbelt";
+ * 
+ * type GetMinOrJoin = $N<Conditional.If, [
+ *   $<Conditional.Extends, number[]>,
+ *   $N<List.Reduce, [Number.Min, Number.MAX_SAFE_INTEGER]>,
+ *   $<String.Join, ", ">,
+ * ]>;
+ * 
+ * type IsNegativeHundred = $<GetMinOrJoin, [1, -1, 10, -10, 100, -100]>;  // -100
+ * type HelloWorld = $<GetMinOrJoin, ["hello", "world"]>;  // "hello, world"
+ * ```
+ */
 export interface Reduce extends Kind.Kind {
   f(
     x: Type._$cast<this[Kind._], Kind.Kind<(x: never) => Kind.Kind>>
