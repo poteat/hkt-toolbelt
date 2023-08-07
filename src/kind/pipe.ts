d0775de35b1f7b759fc70429f23a47b662dfe498
Jongsun Suh <34228073+MajorLift@users.noreply.github.com>
2023-08-06 20:17:04 -0700

    Add jsdocs for kind compose, pipe

--- src/kind/pipe.ts
+++ src/kind/pipe.ts
@@ -1,5 +1,34 @@
-import { Type, List, Kind } from '..'
+import { $$, Type, List, Kind } from '..'
 
+/**
+ * `_$pipe` is a type-level function that allows users to compose
+ * multiple type-level functions together. It takes in a list of functions and a type argument as input,
+ * composes the functions in its input list from left to right, and
+ * and applies the resulting type-level function to the second input type.
+ *
+ * @param {Kind.Kind[]} FX a tuple of type-level functions
+ * @param X a type to which a `Kind` can be applied
+ *
+ * @see {@link Kind._$compose}
+ * The functionality of `_$pipe` is identical to `Kind._$compose`
+ * except for the order of evaluation being reversed,
+ * which has the advantage of improved readability.
+ *
+ * @see {@link $$}
+ * In short, `$$` is an alias for `_$pipe`.
+ * Refer to {@link $$} for examples.
+ *
+ * ## Errors
+ *
+ * `_$pipe` ensures that the tuple of kinds is composable by enforcing that
+ * Nth type-level function's output is a subtype of the (N + 1)th input.
+ * If this is not the case, `_$pipe` will return the `never` type.
+ *
+ * @see {@link Kind.InputOf} {@link Kind.OutputOf}
+ * If you receive a `never` type, it can be helpful to use the `Kind.InputOf`
+ * and `Kind.OutputOf` type-level functions to inspect the input and output
+ * types of the type-level functions that you are piping together.
+ */
 export type _$pipe<FX extends Kind.Kind[], X> = Kind._$compose<
   List._$reverse<FX>,
   X
@@ -14,6 +43,35 @@ interface Pipe_T<FX extends Kind.Kind[]> extends Kind.Kind {
   ): _$pipe<FX, typeof x>
 }
 
+/**
+ * `Pipe` is a type-level function that allows users to compose
+ * multiple type-level functions together. It takes in a list of functions and a type argument as input,
+ * composes the functions in its input list from left to right, and
+ * and applies the resulting type-level function to the second input type.
+ *
+ * @param {Kind.Kind[]} FX a tuple of type-level functions
+ * @param X a type to which a `Kind` can be applied
+ *
+ * @see {@link Kind.Compose}
+ * The functionality of `Pipe` is identical to `Kind.Compose`
+ * except for the order of evaluation being reversed, which has the advantage of improved readability.
+ *
+ * @see {@link $$}
+ * In short, `Pipe` is the partially applicable formulation of `$$`.
+ * While `$$` immediately applies a composed function to an input,
+ * `Pipe` can also be passed into other higher-order type-level functions without being invoked.
+ *
+ * ## Errors
+ *
+ * `Pipe` ensures that the tuple of kinds is composable by enforcing that
+ * Nth type-level function's output is a subtype of the (N + 1)th input.
+ * If this is not the case, `Pipe` will return the `never` type.
+ *
+ * @see {@link Kind.InputOf} {@link Kind.OutputOf}
+ * If you receive a `never` type, it can be helpful to use the `Kind.InputOf`
+ * and `Kind.OutputOf` type-level functions to inspect the input and output
+ * types of the type-level functions that you are piping together.
+ */
 export interface Pipe extends Kind.Kind {
   f(
     x: Type._$cast<this[Kind._], Kind.Kind[]>
