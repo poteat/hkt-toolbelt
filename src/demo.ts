8010af6717affdc7667db864af14e416e7543b0e
o <poteat@users.noreply.github.com>
2023-08-07 14:40:54 -0700

    Remove example demo code

--- src/demo.ts
+++
@@ -1,6 +0,0 @@
-import { map } from './list'
-import { capitalize, length } from './string'
-import { pipe } from './kind'
-import { identity } from './function'
-
-const result = map(pipe([capitalize, length, identity]))(['foo', 'bar', 'baz'])
