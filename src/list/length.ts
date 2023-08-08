8cfa07c1df55ff69551ed370170529b407fe3eec
o <poteat@users.noreply.github.com>
2023-08-07 21:08:53 -0700

    Remove early reification tests

--- src/list/length.ts
+++ src/list/length.ts
@@ -5,5 +5,3 @@ export type _$length<T extends unknown[]> = T['length']
 export interface Length extends Kind.Kind {
   f(x: Type._$cast<this[Kind._], unknown[]>): _$length<typeof x>
 }
-
-export declare const length: Kind._$reify<Length>
