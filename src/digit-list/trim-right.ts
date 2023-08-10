c88f3f1edebcd621778e9e1927c1665d7baf4a0c
Jongsun Suh <34228073+MajorLift@users.noreply.github.com>
2023-08-09 20:14:04 -0700

    Linter fixes

--- src/digit-list/trim-right.ts
+++ src/digit-list/trim-right.ts
@@ -1,18 +1,18 @@
-import { Type, Kind, DigitList } from "..";
+import { Type, Kind, DigitList } from '..'
 
 type _$trimRight2<A extends DigitList.DigitList> = A extends [
   ...infer Rest extends DigitList.DigitList,
-   "0"
+  '0'
 ]
   ? _$trimRight2<Rest>
-  : A;
+  : A
 
 export type _$trimRight<
   A extends DigitList.DigitList,
   TRIM extends DigitList.DigitList = _$trimRight2<A>,
-  OUTPUT extends DigitList.DigitList = TRIM extends [] ? ["0"] : TRIM
-> = OUTPUT;
+  OUTPUT extends DigitList.DigitList = TRIM extends [] ? ['0'] : TRIM
+> = OUTPUT
 
 export interface TrimRight extends Kind.Kind {
-  f(x: Type._$cast<this[Kind._], DigitList.DigitList>): _$trimRight<typeof x>;
+  f(x: Type._$cast<this[Kind._], DigitList.DigitList>): _$trimRight<typeof x>
 }
