05ba9a9a60872aa93c795647d92be6c37d605c20
Jongsun Suh <34228073+MajorLift@users.noreply.github.com>
2023-08-09 20:14:04 -0700

    Linter fixes

--- src/digit-list/trim-right.spec.ts
+++ src/digit-list/trim-right.spec.ts
@@ -1,33 +1,33 @@
-import { $, Test, Kind, DigitList } from "..";
+import { $, Test, DigitList } from '..'
 
 type TrimRight_Spec = [
   /**
    * Trimming zero should result in zero.
    */
-  Test.Expect<$<DigitList.TrimRight, ["0"]>, ["0"]>,
+  Test.Expect<$<DigitList.TrimRight, ['0']>, ['0']>,
 
   /**
    * Trimming an empty zero should result in zero.
    */
-  Test.Expect<$<DigitList.TrimRight, []>, ["0"]>,
+  Test.Expect<$<DigitList.TrimRight, []>, ['0']>,
 
   /**
    * Trimming a single digit should result in the same digit.
    */
-  Test.Expect<$<DigitList.TrimRight, ["1"]>, ["1"]>,
+  Test.Expect<$<DigitList.TrimRight, ['1']>, ['1']>,
 
   /**
    * Trimming a digit with trailing zeros should result in the same digit.
    */
-  Test.Expect<$<DigitList.TrimRight, ["1", "0"]>, ["1"]>,
+  Test.Expect<$<DigitList.TrimRight, ['1', '0']>, ['1']>,
 
   /**
    * Should be able to handle multiple trailing zeros.
    */
-  Test.Expect<$<DigitList.TrimRight, ["1", "0", "0"]>, ["1"]>,
+  Test.Expect<$<DigitList.TrimRight, ['1', '0', '0']>, ['1']>,
 
   /**
    * Should be able to handle all zeroes.
    */
-  Test.Expect<$<DigitList.TrimRight, ["0", "0", "0"]>, ["0"]>
-];
+  Test.Expect<$<DigitList.TrimRight, ['0', '0', '0']>, ['0']>
+]
