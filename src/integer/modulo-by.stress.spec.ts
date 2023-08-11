fd48ab1356a95d8e0acaaf8b7b13e89bad48d05d
Jongsun Suh <34228073+MajorLift@users.noreply.github.com>
2023-08-11 11:32:11 -0700

    Add `RemainderBy`, `ModuloBy` for Integer

---
+++ src/integer/modulo-by.stress.spec.ts
@@ -0,0 +1,14 @@
+import { $, Test, Integer } from '..'
+
+type ModuloBy_Spec = [
+  /**
+   * Can perform complicated modulo.
+   */
+  Test.Expect<
+    $<
+      $<Integer.ModuloBy, -5>,
+      -123456789123456789123456789123456789123456789123456789123456789123456789123456789123456789n
+    >,
+    -1
+  >
+]
---
+++ src/integer/modulo-by.ts
@@ -0,0 +1,57 @@
+import { Kind, Type, Number, Integer } from '..'
+
+/**
+ * `_$moduloBy` is a type-level function that takes in two integer types,
+ * `A` and `B`, and returns the remainder of `B` divided by `A`.
+ *
+ * @param A The number to divide by to calculate the remainder.
+ * @param B The numerator.
+ *
+ * The parameters are reversed from `_$modulo`. This is useful for partial
+ * application, i.e. to test divisibility.
+ */
+export type _$moduloBy<
+  /**
+   * The number to divide by to calculate the remainder.
+   */
+  A extends number,
+  /**
+   * The numerator.
+   */
+  B extends number
+> = Integer._$modulo<B, A>
+
+interface ModuloBy_T<A extends number> extends Kind.Kind {
+  f(
+    x: Type._$cast<this[Kind._], number>
+  ): Number._$isInteger<typeof x> extends true ? _$moduloBy<A, typeof x> : never
+}
+
+/**
+ * `ModuloBy` is a type-level function that takes in two integer types,
+ * `A` and `B`, and returns the remainder of `B` divided by `A`.
+ *
+ * @param A The number to divide by to calculate the remainder.
+ * @param B The numerator.
+ *
+ * The parameters are reversed from `Modulo`. This is useful for partial
+ * application, i.e. to test divisibility.
+ *
+ * ## Usage Examples
+ *
+ * @example
+ * For example, we can use `ModuloBy` to determine the remainder
+ * of an integer divided by another integer. In this example, `3` and `4` are
+ * passed as type arguments to the type-level function:
+ *
+ * ```ts
+ * import { $, Integer } from "hkt-toolbelt";
+ *
+ * type ModuloByThree = $<Integer.ModuloBy, 3>;
+ *
+ * type Result = $<ModuloByThree, 4>; // 4 % 3 = 1
+ * ```
+ */
+export interface ModuloBy extends Kind.Kind {
+  f(x: Type._$cast<this[Kind._], number>): ModuloBy_T<typeof x>
+}
