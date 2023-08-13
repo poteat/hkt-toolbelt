e4ec846e23fee36d7b0dbeee86bd9c802c71942f
o <poteat@users.noreply.github.com>
2023-08-13 12:24:31 -0700

    Submit JSDoc for Kind.Apply

--- src/kind/apply.ts
+++ src/kind/apply.ts
@@ -1,17 +1,68 @@
 import { $, Type, Kind } from '..'
 
+/**
+ * `_$apply` is the internal implementation for the `Apply` utility.
+ *
+ * It takes a value X and kind K, casts X to the input type of K, and applies
+ * K to the casted X using the `$` operator.
+ *
+ * @param K - The kind to apply
+ * @param X - The value to apply K to
+ *
+ * @returns The result of applying K to the casted X
+ */
+export type _$apply<X, K extends Kind.Kind> = $<
+  K,
+  Type._$cast<X, Kind._$inputOf<K>>
+>
+
 interface Apply_T<X> extends Kind.Kind {
   f(
     x: Type._$cast<this[Kind._], Kind.Kind<(x: X) => unknown>>
-  ): $<
-    typeof x,
-    Type._$cast<
-      X,
-      Kind._$inputOf<Type._$cast<this[Kind._], Kind.Kind<(x: X) => unknown>>>
-    >
-  >
+  ): _$apply<X, typeof x>
 }
 
+/**
+ * `Apply` is a type-level function that applies a kind to a type.
+ *
+ * Notably, the argument positions are reversed - we first take in a value, and
+ * then a kind to apply to that value.
+ *
+ * @param X - The value of type X to apply the kind to
+ * @param K - The kind to apply
+ *
+ * @returns The result of applying K to x
+ *
+ * @example
+ *
+ * For a simple example, we can apply `Identity` to the type 42:
+ *
+ * ```ts
+ * import { $, Kind, Function } from 'hkt-toolbelt'
+ *
+ * type X = $<$<Kind.Apply, 42>, Function.Identity> // 42
+ * ```
+ *
+ * @example
+ *
+ * `Apply` is particularly useful for more complicated chains of type-level
+ * logic. For example, we can map over a list of kinds and apply all of them
+ * to an input type:
+ *
+ * ```ts
+ * import { $, Kind, List, String } from 'hkt-toolbelt'
+ *
+ * type X = $<
+ *   $<List.Map, $<Kind.Apply, 'qux'>>,
+ *   [$<String.Prepend, 'foo'>, $<String.Append, 'bar'>, String.ToUpper]
+ * >
+ * // ['fooqux', 'quxbar', 'QUX']
+ * ```
+ *
+ * In the above example, we map over a list of kinds, and apply each of them
+ * to the input type `'qux'`. The result is a list of types, each of which
+ * is the result of applying the corresponding kind to `'qux'`.
+ */
 export interface Apply extends Kind.Kind {
   f(x: this[Kind._]): Apply_T<typeof x>
 }
