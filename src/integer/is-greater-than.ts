4ac526d37a7b3a8754b832cd6547d6f697b53fe9
Jongsun Suh <34228073+MajorLift@users.noreply.github.com>
2023-08-13 10:19:57 -0700

    Add `IsLessThan`, `IsLessThanOrEqual`, `IsGreaterThan`, `IsGreaterThanOrEqual` for Integer

---
+++ src/integer/is-greater-than.ts
@@ -0,0 +1,76 @@
+import { Number, Integer, Kind, Type } from '..'
+
+/**
+ * `_$isGreaterThan` is a type-level function that takes in two integer
+ * types, `A` and `B`, and returns a boolean indicating whether `B` is greater
+ * than `A`.
+ *
+ * @param A The number to compare against.
+ * @param B The number to compare.
+ */
+export type _$isGreaterThan<
+  /**
+   * The number to compare against.
+   */
+  A extends Number.Number,
+  /**
+   * The number to evaluate.
+   */
+  B extends Number.Number,
+  /**
+   * `true` iff `B` is greater than `A`.
+   */
+  RESULT extends boolean = Integer._$compare<B, A> extends 1 ? true : false
+> = RESULT
+
+interface IsGreaterThan_T<A extends Number.Number> extends Kind.Kind {
+  f(x: Type._$cast<this[Kind._], Number.Number>): _$isGreaterThan<A, typeof x>
+}
+
+/**
+ * `IsGreaterThan` is a type-level function that takes in two integer
+ * types, `A` and `B`, and returns a boolean indicating whether `B` is greater
+ * than `A`.
+ *
+ * @param A The number to compare against.
+ * @param B The number to evaluate.
+ *
+ * The parameters are ordered such that `IsGreaterThan` can be partially applied
+ * in a coherent manner. That is, we can apply `IsGreaterThan` to `3`, and have a
+ * function `IsGreaterThanThree`.
+ *
+ * @example
+ * For example, we can use `IsGreaterThan` to determine whether an integer is
+ * greater than another integer. In this example, `-2` and `-3` are passed as
+ * type arguments to the type-level function:
+ *
+ * We apply `IsGreaterThan` to `-3`, and then to `-2` respectively using the `$`
+ * type-level applicator:
+ *
+ * ```ts
+ * import { $, Integer } from "hkt-toolbelt";
+ *
+ * type Result = $<$<Integer.IsGreaterThan, -3>, -2>; // true
+ * ```
+ *
+ * @example
+ * If we apply `IsGreaterThan` to `-3` and `-3`, we should expect to get `false`.
+ *
+ * ```ts
+ * import { $, Integer } from "hkt-toolbelt";
+ *
+ * type Result = $<$<Integer.IsGreaterThan, -3>, -3>; // false
+ * ```
+ *
+ * @example
+ * If we apply `IsGreaterThan` to `-3` and `-4`, we should expect to get `false`.
+ *
+ * ```ts
+ * import { $, Integer } from "hkt-toolbelt";
+ *
+ * type Result = $<$<Integer.IsGreaterThan, -3>, -4>; // false
+ * ```
+ */
+export interface IsGreaterThan extends Kind.Kind {
+  f(x: Type._$cast<this[Kind._], Number.Number>): IsGreaterThan_T<typeof x>
+}
---
+++ src/integer/is-less-than-or-equal.ts
@@ -0,0 +1,79 @@
+import { Number, Integer, Kind, Type } from '..'
+
+/**
+ * `_$isLessThanOrEqual` is a type-level function that takes in two integer
+ * types, `A` and `B`, and returns a boolean indicating whether `B` is less
+ * than or equal to `A`.
+ *
+ * @param A The number to compare against.
+ * @param B The number to compare.
+ */
+export type _$isLessThanOrEqual<
+  /**
+   * The number to compare against.
+   */
+  A extends Number.Number,
+  /**
+   * The number to evaluate.
+   */
+  B extends Number.Number,
+  /**
+   * `true` iff `B` is less than or equal to `A`.
+   */
+  RESULT extends boolean = Integer._$compare<B, A> extends -1 | 0 ? true : false
+> = RESULT
+
+interface IsLessThanOrEqual_T<A extends Number.Number> extends Kind.Kind {
+  f(
+    x: Type._$cast<this[Kind._], Number.Number>
+  ): _$isLessThanOrEqual<A, typeof x>
+}
+
+/**
+ * `IsLessThanOrEqual` is a type-level function that takes in two natural number
+ * types, `A` and `B`, and returns a boolean indicating whether `B` is less
+ * than or equal to `A`.
+ *
+ * @param A The number to compare against.
+ * @param B The number to evaluate.
+ *
+ * The parameters are ordered such that `IsLessThanOrEqual` can be partially applied
+ * in a coherent manner. That is, we can apply `IsLessThanOrEqual` to `3`, and have a
+ * function `IsLessThanOrEqualToThree`.
+ *
+ * @example
+ * For example, we can use `IsLessThanOrEqual` to determine whether an integer is
+ * less than or equal to another integer. In this example, `-3` and `-4` are passed as
+ * type arguments to the type-level function:
+ *
+ * We apply `IsLessThanOrEqual` to `-3`, and then to `-4` respectively using the `$`
+ * type-level applicator:
+ *
+ * ```ts
+ * import { $, Integer } from "hkt-toolbelt";
+ *
+ * type Result = $<$<Integer.IsLessThanOrEqual, -3>, -4>; // true
+ * ```
+ *
+ * @example
+ * If we apply `IsLessThanOrEqual` to `-3` and `-3`, we should expect to get `true`.
+ *
+ * ```ts
+ * import { $, Integer } from "hkt-toolbelt";
+ *
+ * type Result = $<$<Integer.IsLessThanOrEqual, -3>, -3>; // true
+ * ```
+ *
+ * @example
+ * If we apply `IsLessThanOrEqual` to `-3` and `4`, we should also expect to get
+ * `false`.
+ *
+ * ```ts
+ * import { $, Integer } from "hkt-toolbelt";
+ *
+ * type Result = $<$<Integer.IsLessThanOrEqual, -3>, 4>; // false
+ * ```
+ */
+export interface IsLessThanOrEqual extends Kind.Kind {
+  f(x: Type._$cast<this[Kind._], Number.Number>): IsLessThanOrEqual_T<typeof x>
+}
