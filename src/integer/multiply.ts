7bae105de3e413e577a9e114c700ab9cee3befcc
Jongsun Suh <34228073+MajorLift@users.noreply.github.com>
2023-08-13 21:19:04 -0700

    Add JSDoc for `Multiply` in NaturalNumber, Integer

--- src/integer/multiply.ts
+++ src/integer/multiply.ts
@@ -1,5 +1,31 @@
 import { Type, Number, Kind, DigitList, Boolean, NaturalNumber } from '..'
 
+/**
+ * `_$multiply` is a type-level function that takes in two integers `A` and `B`,
+ * and returns the product of the two integers.
+ *
+ * @param A An integer.
+ * @param B An integer.
+ *
+ * @example
+ * For example, we can use `_$multiply` to multiply the two integers -123 and 456:
+ *
+ * ```ts
+ * import { Integer } from "hkt-toolbelt"
+ *
+ * type Result = Integer._$multiply<-123, 456> // -56088
+ * type ResultStr = Integer._$multiply<'-123', '456'> // -56088
+ * ```
+ *
+ * @example
+ * If one of the inputs is zero, the result will be zero.
+ *
+ * ```ts
+ * import { Integer } from "hkt-toolbelt";
+ *
+ * type IsZero = Integer._$multiply<-42, 0>; // 0
+ * ```
+ */
 export type _$multiply<
   A extends Number.Number,
   B extends Number.Number,
@@ -29,6 +55,43 @@ interface Multiply_T<A extends Number.Number> extends Kind.Kind {
   ): Number._$isInteger<typeof x> extends true ? _$multiply<A, typeof x> : never
 }
 
+/**
+ * `Multiply` is a type-level function that multiplies an integer by another integer.
+ * It returns the result of the multiplication operation.
+ *
+ * @param A - An integer.
+ * @param B - An integer.
+ *
+ * If one or more of the inputs is not an integer, an error is emitted.
+ *
+ * @example
+ * For example, we can use `Multiply` to multiply an integer 456 by another integer -123:
+ *
+ * ```ts
+ * import { $, NaturalNumber } from "hkt-toolbelt";
+ *
+ * type Is504 = $<$<NaturalNumber.Multiply, -123>, 456>; // -56088
+ * type Is504Str = $<$<NaturalNumber.Multiply, '-123'>, '456'>; // -56088
+ * ```
+ *
+ * @example
+ * If one of the inputs is zero, the result will be zero.
+ *
+ * ```ts
+ * import { NaturalNumber } from "hkt-toolbelt";
+ *
+ * type IsZero = $<$<NaturalNumber.Multiply, 0>, 42>; // 0
+ * ```
+ *
+ * @example
+ * If one of the inputs is not an integer, `never` is returned.
+ *
+ * ```ts
+ * import { NaturalNumber } from "hkt-toolbelt";
+ *
+ * type IsNever = $<NaturalNumber.Multiply, -42.42>; // never
+ * ```
+ */
 export interface Multiply extends Kind.Kind {
   f(
     x: Type._$cast<this[Kind._], Number.Number>
