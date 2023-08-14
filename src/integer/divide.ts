d4f1f0e05967f5c8347258b407c67dc660cbe8e8
Jongsun Suh <34228073+MajorLift@users.noreply.github.com>
2023-08-14 11:48:09 -0700

    Add JSDoc for `Divide` in NaturalNumber, Integer

--- src/integer/divide.ts
+++ src/integer/divide.ts
@@ -1,5 +1,36 @@
 import { Type, Number, Kind, DigitList, NaturalNumber, Boolean } from '..'
 
+/**
+ * `_$divide` is a type-level function that performs the division operation.
+ * It takes in two integers `A` and `B` representing the dividend and divisor respectively,
+ * and returns the result of dividing `A` by `B`.
+ *
+ * @param {Number.Number} A - An integer to divide.
+ * @param {Number.Number} B - An integer to divide by.
+ * @returns {Number.Number} An integer type.
+ *
+ * If `A` is not a multiple of `B`, the quotient is returned and the remainder is thrown away.
+ * The quotient is truncated towards zero. That is, `-1/2` is evaluated to be 0, not -1.
+ *
+ * @example
+ * For example, we can use `_$divide` to divide 10 by 2:
+ *
+ * ```ts
+ * import { Integer } from "hkt-toolbelt";
+ *
+ * type Result = Integer._$divide<10, 2>; // 5
+ * ```
+ *
+ * @example
+ * If `A` is not a multiple of `B`, the result is truncated towards zero,
+ * and only the quotient is returned.
+ *
+ * ```ts
+ * import { Integer } from "hkt-toolbelt";
+ *
+ * type Result = Integer._$divide<-100, 99>; // -1
+ * ```
+ */
 export type _$divide<
   A extends Number.Number,
   B extends Number.Number,
@@ -29,6 +60,47 @@ interface Divide_T<A extends Number.Number> extends Kind.Kind {
   ): Number._$isInteger<typeof x> extends true ? _$divide<A, typeof x> : never
 }
 
+/**
+ * `Divide` is a type-level function that takes in two integers and performs a division operation.
+ * It returns the result of the division operation.
+ *
+ * @param {Number.Number} A - An integer to divide.
+ * @param {Number.Number} B - An integer to divide by.
+ * @returns {Number.Number} An integer type.
+ *
+ * If `A` is not a multiple of `B`, the quotient is returned and the remainder is thrown away.
+ * The quotient is truncated towards zero. That is, `-1/2` is evaluated to be 0, not -1.
+ *
+ * If either input is not a integer, `never` is returned.
+ *
+ * @example
+ * For example, we can use `Divide` to create a division operation that divides 10 by -2:
+ *
+ * ```ts
+ * import { $, Integer } from "hkt-toolbelt";
+ *
+ * type Result = $<$<Integer.Divide, 10>, -2>; // -5
+ * ```
+ *
+ * @example
+ * If `A` is not a multiple of `B`, the result is truncated towards zero,
+ * and only the quotient is returned.
+ *
+ * ```ts
+ * import { $, Integer } from "hkt-toolbelt";
+ *
+ * type Result = $<$<Integer.Divide, -100>, 99>; // -1
+ * ```
+ *
+ * @example
+ * If one of the inputs is not a integer, `never` is returned.
+ *
+ * ```ts
+ * import { $, Integer } from "hkt-toolbelt";
+ *
+ * type IsNever = $<Integer.Divide, -42.42>; // never
+ * ```
+ */
 export interface Divide extends Kind.Kind {
   f(
     x: Type._$cast<this[Kind._], Number.Number>
