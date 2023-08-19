b79923bc47936b9d6891a7de78143b6ceb0ee052
Jongsun Suh <34228073+MajorLift@users.noreply.github.com>
2023-08-19 15:27:12 -0700

    Fix `Number.Absolute` to handle bigint
    Previously returned 0 for negative bigints.

--- src/number/absolute.spec.ts
+++ src/number/absolute.spec.ts
@@ -2,27 +2,37 @@ import { $, Test, Number } from '..'
 
 export type Absolute_Spec = [
   /**
-   * Can get the sign of a positive number.
+   * Can get the absolute value of a positive number.
    */
   Test.Expect<$<Number.Absolute, 42>, 42>,
 
   /**
-   * Can get the sign of a negative number.
+   * Can get the absolute value of a negative number.
    */
   Test.Expect<$<Number.Absolute, -42>, 42>,
 
   /**
-   * Can get the sign of zero.
+   * Can get the absolute value of zero.
    */
   Test.Expect<$<Number.Absolute, 0>, 0>,
 
   /**
-   * Can get the sign of a positive floating point number.
+   * Can get the absolute value of a positive floating point number.
    */
   Test.Expect<$<Number.Absolute, 42.42>, 42.42>,
 
   /**
-   * Can get the sign of a negative floating point number.
+   * Can get the absolute value of a negative floating point number.
    */
-  Test.Expect<$<Number.Absolute, -42.42>, 42.42>
+  Test.Expect<$<Number.Absolute, -42.42>, 42.42>,
+
+  /**
+   * Can get the absolute value of a negative floating point number encoded as a string.
+   */
+  Test.Expect<$<Number.Absolute, '-42.42'>, 42.42>,
+
+  /**
+   * Can get the absolute value of a bigint.
+   */
+  Test.Expect<$<Number.Absolute, -123456789123456789n>, 123456789123456789n>
 ]
