b2f9627b6d330688e53026cc8b235fbe3b0ec92c
o <poteat@users.noreply.github.com>
2023-08-20 00:29:03 -0700

    aider: Added JSDoc comments to `src/number/number.ts`.
    # Aider chat conversation:
    
    USER: I did `git reset --hard HEAD~1` to discard the last edits.
    
    ASSISTANT: Okay, that command will undo the last commit and discard the changes made in it. If you need any further assistance, feel free to ask.
    
    USER: Ah, can you write JSDoc for `src/number/number.ts` ?
    
    ASSISTANT: Sure, we can add JSDoc comments to the `src/number/number.ts` file.
    
    Here are the steps we need to follow:
    
    1. Add a JSDoc comment to the `Number` type. This comment should explain what the type does.
    2. Add similar JSDoc comments to the `MAX_SAFE_INTEGER` and `MIN_SAFE_INTEGER` types.
    
    Here are the proposed changes:
    
    <source>typescript
    src/number/number.ts
    <<<<<<< HEAD
    export type Number = string | number | bigint
    export type MAX_SAFE_INTEGER = 9007199254740991
    export type MIN_SAFE_INTEGER = -9007199254740991
    =======
    /**
     * `Number` is a type that represents a number. It can be a string, a number, or a bigint.
     */
    export type Number = string | number | bigint
    
    /**
     * `MAX_SAFE_INTEGER` is a type that represents the maximum safe integer in JavaScript.
     */
    export type MAX_SAFE_INTEGER = 9007199254740991
    
    /**
     * `MIN_SAFE_INTEGER` is a type that represents the minimum safe integer in JavaScript.
     */
    export type MIN_SAFE_INTEGER = -9007199254740991
    >>>>>>> updated
    </source>

--- src/number/number.ts
+++ src/number/number.ts
@@ -1,3 +1,14 @@
+/**
+ * `Number` is a type that represents a number. It can be a string, a number, or a bigint.
+ */
 export type Number = string | number | bigint
+
+/**
+ * `MAX_SAFE_INTEGER` is a type that represents the maximum safe integer in JavaScript.
+ */
 export type MAX_SAFE_INTEGER = 9007199254740991
+
+/**
+ * `MIN_SAFE_INTEGER` is a type that represents the minimum safe integer in JavaScript.
+ */
 export type MIN_SAFE_INTEGER = -9007199254740991
