68cf44b71d64e4d23a757ea4038245a9677bfee8
o <poteat@users.noreply.github.com>
2023-08-20 01:05:27 -0700

    aider: Added JSDoc comments to `src/type/cast.ts` and `src/type/cast.spec.ts` files.
    # Aider chat conversation:
    
    USER: Can we write JSDoc for `src/type/cast.ts` ? `src/type/cast.spec.ts` might be useful as well. We should talk about how it's used in custom kind development to encode input constraints, e.g. in `src/string/reverse.ts`, which causes a type error if you e.g. try $<String.Reverse, 123>
    
    ASSISTANT: Sure, we can add JSDoc comments to the `src/type/cast.ts` and `src/type/cast.spec.ts` files.
    
    Here are the changes:
    
    For `src/type/cast.ts`:
    
    <source>typescript
    src/type/cast.ts
    <<<<<<< HEAD
    import { Kind } from '..'
    
    export type _$cast<T, U> = T extends U ? T : U
    
    interface Cast_T<T> extends Kind.Kind {
      f(x: this[Kind._]): _$cast<typeof x, T>
    }
    
    export interface Cast extends Kind.Kind {
      f(x: this[Kind._]): Cast_T<typeof x>
    }
    =======
    import { Kind } from '..'
    
    /**
     * `_$cast` is a type-level function that casts a type `T` to a type `U`.
     * It is used in custom kind development to encode input constraints.
     *
     * @template T - The type to cast.
     * @template U - The type to cast to.
     *
     * @example
     * type T0 = _$cast<123, string> // Causes a type error
     * type T1 = _$cast<'hello', string> // 'hello'
     */
    export type _$cast<T, U> = T extends U ? T : U
    
    /**
     * `Cast_T` is an interface that extends `Kind.Kind`.
     * It is used in custom kind development to encode input constraints.
     *
     * @template T - The type to cast.
     */
    interface Cast_T<T> extends Kind.Kind {
      f(x: this[Kind._]): _$cast<typeof x, T>
    }
    
    /**
     * `Cast` is an interface that extends `Kind.Kind`.
     * It is used in custom kind development to encode input constraints.
     */
    export interface Cast extends Kind.Kind {
      f(x: this[Kind._]): Cast_T<typeof x>
    }
    >>>>>>> updated
    </source>
    
    For `src/type/cast.spec.ts`:
    
    <source>typescript
    src/type/cast.spec.ts
    <<<<<<< HEAD
    import { Test, Type } from '..'
    
    type Cast_Spec = [
      /**
       * Can cast a type to itself.
       */
      Test.Expect<Type._$cast<true, true>, true>,
    
      /**
       * Can cast a type to a subtype.
       */
      Test.Expect<Type._$cast<boolean, true>, true>,
    
      /**
       * Casting to a supertype keeps the type narrow.
       */
      Test.Expect<Type._$cast<true, boolean>, true>
    ]
    =======
    import { Test, Type } from '..'
    
    /**
     * `Cast_Spec` is a type that tests the `_$cast` type-level function.
     * It includes tests for casting a type to itself, to a subtype, and to a supertype.
     */
    type Cast_Spec = [
      /**
       * Can cast a type to itself.
       */
      Test.Expect<Type._$cast<true, true>, true>,
    
      /**
       * Can cast a type to a subtype.
       */
      Test.Expect<Type._$cast<boolean, true>, true>,
    
      /**
       * Casting to a supertype keeps the type narrow.
       */
      Test.Expect<Type._$cast<true, boolean>, true>
    ]
    >>>>>>> updated
    </source>

--- src/type/cast.ts
+++ src/type/cast.ts
@@ -1,11 +1,32 @@
 import { Kind } from '..'
 
+/**
+ * `_$cast` is a type-level function that casts a type `T` to a type `U`.
+ * It is used in custom kind development to encode input constraints.
+ * 
+ * @template T - The type to cast.
+ * @template U - The type to cast to.
+ * 
+ * @example
+ * type T0 = _$cast<123, string> // Causes a type error
+ * type T1 = _$cast<'hello', string> // 'hello'
+ */
 export type _$cast<T, U> = T extends U ? T : U
 
+/**
+ * `Cast_T` is an interface that extends `Kind.Kind`.
+ * It is used in custom kind development to encode input constraints.
+ * 
+ * @template T - The type to cast.
+ */
 interface Cast_T<T> extends Kind.Kind {
   f(x: this[Kind._]): _$cast<typeof x, T>
 }
 
+/**
+ * `Cast` is an interface that extends `Kind.Kind`.
+ * It is used in custom kind development to encode input constraints.
+ */
 export interface Cast extends Kind.Kind {
   f(x: this[Kind._]): Cast_T<typeof x>
 }
