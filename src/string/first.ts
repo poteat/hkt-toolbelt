d930f37f6b2d2be5c57ba17002ef7f37fa52275a
o <poteat@users.noreply.github.com>
2023-08-20 15:12:02 -0700

    aider: Added JSDoc comments for `_$first` type and `First` interface in `src/string/first.ts`.
    # Aider chat conversation:
    
    USER: Can we add JSDoc for `src/string/first.ts` per `style-guide/jsdoc.md` ? The test file `src/string/first.spec.ts` may provide helpful context.
    
    ASSISTANT: Yes, we can add JSDoc comments to the `src/string/first.ts` file. Here are the necessary changes:
    
    1. File to modify: `src/string/first.ts`
    
    We will add JSDoc comments to the `_$first` type and the `First` interface.
    
    Here are the changes:
    
    <source>typescript
    src/string/first.ts
    <<<<<<< HEAD
    import { Type, Kind } from '..'
    
    export type _$first<S extends string> = S extends `${infer Head}${string}`
      ? Head
      : string extends S
      ? S
      : ''
    
    export interface First extends Kind.Kind {
      f(x: Type._$cast<this[Kind._], string>): _$first<typeof x>
    }
    =======
    import { Type, Kind } from '..'
    
    /**
     * `String.First` is a type-level function that extracts the first character from a string.
     *
     * @template S - The string to extract the first character from.
     *
     * @example
     * type T0 = String._$first<'hello'> // 'h'
     * type T1 = String._$first<''> // ''
     */
    export type _$first<S extends string> = S extends `${infer Head}${string}`
      ? Head
      : string extends S
      ? S
      : ''
    
    /**
     * `String.First` is a type-level function that extracts the first character from a string.
     *
     * @example
     * type T0 = $<String.First, 'hello'> // 'h'
     * type T1 = $<String.First, ''> // ''
     */
    export interface First extends Kind.Kind {
      f(x: Type._$cast<this[Kind._], string>): _$first<typeof x>
    }
    >>>>>>> updated
    </source>
    
    The JSDoc comments provide a brief description of the `_$first` type and the `First` interface, the template parameters they use, and examples of their usage.

--- src/string/first.ts
+++ src/string/first.ts
@@ -1,11 +1,27 @@
 import { Type, Kind } from '..'
 
+/**
+ * `String.First` is a type-level function that extracts the first character from a string.
+ * 
+ * @template S - The string to extract the first character from.
+ * 
+ * @example
+ * type T0 = String._$first<'hello'> // 'h'
+ * type T1 = String._$first<''> // ''
+ */
 export type _$first<S extends string> = S extends `${infer Head}${string}`
   ? Head
   : string extends S
   ? S
   : ''
 
+/**
+ * `String.First` is a type-level function that extracts the first character from a string.
+ * 
+ * @example
+ * type T0 = $<String.First, 'hello'> // 'h'
+ * type T1 = $<String.First, ''> // ''
+ */
 export interface First extends Kind.Kind {
   f(x: Type._$cast<this[Kind._], string>): _$first<typeof x>
 }
