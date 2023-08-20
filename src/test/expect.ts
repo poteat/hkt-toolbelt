f9f930f1b2a0b43c122ce1af6c484a3f45f213f1
o <poteat@users.noreply.github.com>
2023-08-20 00:54:11 -0700

    aider: Clarified the purpose of the `Expect` function in JSDoc comments.
    # Aider chat conversation:
    
    USER: Can we clarify that the purpose of Expect is to cause a compiler error to be emitted if the types are not equivalent?
    
    ASSISTANT: Sure, we can clarify that in the JSDoc comments. Here's the updated comment:
    
    <source>typescript
    src/test/expect.ts
    <<<<<<< HEAD
    /**
     * `Expect` is a type-level function that checks if a type `X` equals a type `V`.
     * If `X` equals `V`, it returns `V`. If `X` does not equal `V`, it returns `V & _`.
     * If `V` is never, it returns `X`. If `X` is never, it returns `Expect<X, V>`.
     *
     * @template X - The type to check.
     * @template V - The type to compare with. Default is `true`.
     *
     * @example
     * type T0 = Expect<true> // true
     * type T1 = Expect<false> // false & _
     * type T2 = Expect<number, true> // true & _
     * type T3 = Expect<never, true> // Expect<never, true>
     */
    =======
    /**
     * `Expect` is a type-level function that checks if a type `X` equals a type `V`.
     * If `X` equals `V`, it returns `V`. If `X` does not equal `V`, it returns `V & _`.
     * If `V` is never, it returns `X`. If `X` is never, it returns `Expect<X, V>`.
     * The purpose of this function is to cause a compiler error to be emitted if the types are not equivalent.
     *
     * @template X - The type to check.
     * @template V - The type to compare with. Default is `true`.
     *
     * @example
     * type T0 = Expect<true> // true
     * type T1 = Expect<false> // false & _
     * type T2 = Expect<number, true> // true & _
     * type T3 = Expect<never, true> // Expect<never, true>
     */
    >>>>>>> updated
    </source>
    
    Please review and apply these changes if they are correct.

--- src/test/expect.ts
+++ src/test/expect.ts
@@ -11,6 +11,7 @@ export abstract class _ {
  * `Expect` is a type-level function that checks if a type `X` equals a type `V`.
  * If `X` equals `V`, it returns `V`. If `X` does not equal `V`, it returns `V & _`.
  * If `V` is never, it returns `X`. If `X` is never, it returns `Expect<X, V>`.
+ * The purpose of this function is to cause a compiler error to be emitted if the types are not equivalent.
  * 
  * @template X - The type to check.
  * @template V - The type to compare with. Default is `true`.
