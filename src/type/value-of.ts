73d73f75ffc3198c038a29990718e412c3813721
o <poteat@users.noreply.github.com>
2023-08-20 01:29:43 -0700

    Refactor `_$valueOf` and `ValueOf` to remove unnecessary comments and whitespace.

--- src/type/value-of.ts
+++ src/type/value-of.ts
@@ -3,9 +3,9 @@ import { Kind } from '..'
 /**
  * `_$valueOf` is a type-level function that extracts the values associated
  * with the type, if any exist, via `keyof`.
- * 
+ *
  * @template T - The type to extract values from.
- * 
+ *
  * @example
  * type T0 = _$valueOf<['foo', 'bar']> // 'foo' | 'bar'
  * type T1 = _$valueOf<{ foo: 'foo'; bar: 'bar' }> // 'foo' | 'bar'
@@ -15,9 +15,9 @@ export type _$valueOf<T> = T extends unknown[] ? T[number] : T[keyof T]
 /**
  * `ValueOf` is a type-level function that extracts the values associated
  * with the type, if any exist, via `keyof`.
- * 
+ *
  * @template T - The type to extract values from.
- * 
+ *
  * @example
  * type T0 = $<ValueOf, ['foo', 'bar']> // 'foo' | 'bar'
  * type T1 = $<ValueOf, { foo: 'foo'; bar: 'bar' }> // 'foo' | 'bar'
