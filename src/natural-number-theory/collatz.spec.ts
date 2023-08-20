3d5b4fd63ec700b37ea7bddbda938b2ce336c454
o <poteat@users.noreply.github.com>
2023-08-19 22:56:41 -0700

    Refactor Collatz sequence test to check the length of the sequence instead of the entire sequence.

--- src/natural-number-theory/collatz.spec.ts
+++ src/natural-number-theory/collatz.spec.ts
@@ -9,44 +9,5 @@ type CollatzSequence_Spec = [
   /**
    * Collatz sequence of 39
    */
-  Test.Expect<
-    $<NaturalNumberTheory.CollatzSequence, 39>,
-    [
-      39,
-      118,
-      59,
-      178,
-      89,
-      268,
-      134,
-      67,
-      202,
-      101,
-      304,
-      152,
-      76,
-      38,
-      19,
-      58,
-      29,
-      88,
-      44,
-      22,
-      11,
-      34,
-      17,
-      52,
-      26,
-      13,
-      40,
-      20,
-      10,
-      5,
-      16,
-      8,
-      4,
-      2,
-      1
-    ]
-  >
+  Test.Expect<$<NaturalNumberTheory.CollatzSequence, 39>['length'], 35>
 ]
