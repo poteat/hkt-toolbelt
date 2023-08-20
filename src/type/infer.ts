71fb2d122bedaed7c1887df60a696c77567c2f3d
o <poteat@users.noreply.github.com>
2023-08-20 01:19:22 -0700

    aider: Added JSDoc comments to `src/type/infer.ts` to provide a description and example usage for the `_$infer` type and `Infer` interface.
    # Aider chat conversation:
    
    USER: Can we add JSDoc for `src/type/infer.ts` ? The tests in `src/type/infer.spec.ts` may provide helpful context.
    
    ASSISTANT: Sure, we can add JSDoc comments to the `src/type/infer.ts` file.
    
    The JSDoc comments will provide a brief description of the `_$infer` type and `Infer` interface, and will include an example usage based on the test in `src/type/infer.spec.ts`.
    
    Here are the changes:
    
    <source>typescript
    src/type/infer.ts
    <<<<<<< HEAD
    import { Kind, Type, Function } from '..'
    
    type _$inferred =
      | string
      | number
      | boolean
      | undefined
      | null
      | Function.Function
      | Kind.Kind
      | _$inferredTuple
      | {
          [key: string]: _$inferred
        }
    
    type _$inferredTuple = _$inferred[] | ReadonlyArray<_$inferred>
    
    export type _$infer<
      X,
      Narrow = Type._$cast<X, _$inferred> | [...Type._$cast<X, _$inferredTuple>]
    > = Narrow extends unknown[] ? { [key in keyof X]: _$infer<X[key]> } : Narrow
    
    export interface Infer extends Kind.Kind {
      f(x: this[Kind._]): _$infer<typeof x>
    }
    =======
    import { Kind, Type, Function } from '..'
    
    /**
     * `_$inferred` is a type that represents the most specific type of a value.
     */
    type _$inferred =
      | string
      | number
      | boolean
      | undefined
      | null
      | Function.Function
      | Kind.Kind
      | _$inferredTuple
      | {
          [key: string]: _$inferred
        }
    
    type _$inferredTuple = _$inferred[] | ReadonlyArray<_$inferred>
    
    /**
     * `_$infer` is a type-level function that infers the most specific type of a value.
     *
     * @template X - The value to infer the type of.
     *
     * @example
     * type T0 = _$infer<'foo'> // 'foo'
     */
    export type _$infer<
      X,
      Narrow = Type._$cast<X, _$inferred> | [...Type._$cast<X, _$inferredTuple>]
    > = Narrow extends unknown[] ? { [key in keyof X]: _$infer<X[key]> } : Narrow
    
    /**
     * `Infer` is a type-level function that infers the most specific type of a value.
     *
     * @template X - The value to infer the type of.
     *
     * @example
     * type T0 = $<Infer, 'foo'> // 'foo'
     */
    export interface Infer extends Kind.Kind {
      f(x: this[Kind._]): _$infer<typeof x>
    }
    >>>>>>> updated
    </source>

--- src/type/infer.ts
+++ src/type/infer.ts
@@ -1,5 +1,8 @@
 import { Kind, Type, Function } from '..'
 
+/**
+ * `_$inferred` is a type that represents the most specific type of a value.
+ */
 type _$inferred =
   | string
   | number
@@ -15,11 +18,27 @@ type _$inferred =
 
 type _$inferredTuple = _$inferred[] | ReadonlyArray<_$inferred>
 
+/**
+ * `_$infer` is a type-level function that infers the most specific type of a value.
+ * 
+ * @template X - The value to infer the type of.
+ * 
+ * @example
+ * type T0 = _$infer<'foo'> // 'foo'
+ */
 export type _$infer<
   X,
   Narrow = Type._$cast<X, _$inferred> | [...Type._$cast<X, _$inferredTuple>]
 > = Narrow extends unknown[] ? { [key in keyof X]: _$infer<X[key]> } : Narrow
 
+/**
+ * `Infer` is a type-level function that infers the most specific type of a value.
+ * 
+ * @template X - The value to infer the type of.
+ * 
+ * @example
+ * type T0 = $<Infer, 'foo'> // 'foo'
+ */
 export interface Infer extends Kind.Kind {
   f(x: this[Kind._]): _$infer<typeof x>
 }
