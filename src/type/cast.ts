847cbbe4f8aac6f1a132d6f539e086b021fee35b
o <poteat@users.noreply.github.com>
2023-08-20 01:11:58 -0700

    aider: Remove note on custom kind development and update examples in JSDoc for `_$cast` to match the tests.
    # Aider chat conversation:
    
    USER: Actually, could we remove the note on custom kind development? As well, could we double check the stated examples? Are they consistent with the tests?
    
    ASSISTANT: Sure, we can remove the note on custom kind development from the JSDoc for `_$cast` in the `src/type/cast.ts` file.
    
    Regarding the examples, we need to cross-check them with the tests in the `src/type/cast.spec.ts` file.
    
    Here are the changes:
    
    <source>typescript
    src/type/cast.ts
    <<<<<<< HEAD
    /**
     * `_$cast` is a type-level function that casts a type `T` to a type `U`.
     * It is used in custom kind development to encode input constraints.
     *
     * @template T - The type to cast.
     * @template U - The type to cast to.
     *
     * @example
     * type T0 = _$cast<123, string> // Causes a type error
     * type T1 = _$cast<'hello', string> // 'hello'
     */
    export type _$cast<T, U> = T extends U ? T : U
    =======
    /**
     * `_$cast` is a type-level function that casts a type `T` to a type `U`.
     *
     * @template T - The type to cast.
     * @template U - The type to cast to.
     *
     * @example
     * type T0 = _$cast<123, string> // Causes a type error
     * type T1 = _$cast<'hello', string> // 'hello'
     */
    export type _$cast<T, U> = T extends U ? T : U
    >>>>>>> updated
    </source>
    
    Now, let's cross-check the examples with the tests:
    
    <source>typescript
    src/type/cast.spec.ts
    <<<<<<< HEAD
    type Cast_Spec = [
      /**
       * Can cast a type to itself.
       */
      Test.Expect<Type._$cast<true, true>, true>,
    
      /**
       * Can cast a type to a subtype.
       */
      Test.Expect<Type._$cast<boolean, true>, true>,
    
      /**
       * Casting to a supertype keeps the type narrow.
       */
      Test.Expect<Type._$cast<true, boolean>, true>
    ]
    =======
    type Cast_Spec = [
      /**
       * Can cast a type to itself.
       */
      Test.Expect<Type._$cast<true, true>, true>,
    
      /**
       * Can cast a type to a subtype.
       */
      Test.Expect<Type._$cast<boolean, true>, true>,
    
      /**
       * Casting to a supertype keeps the type narrow.
       */
      Test.Expect<Type._$cast<true, boolean>, true>
    ]
    >>>>>>> updated
    </source>
    
    The examples in the JSDoc for `_$cast` are not consistent with the tests. The tests are casting boolean types, while the examples are casting a number to a string and a string to a string. We should update the examples to match the tests. Here is the updated JSDoc:
    
    <source>typescript
    src/type/cast.ts
    <<<<<<< HEAD
    /**
     * `_$cast` is a type-level function that casts a type `T` to a type `U`.
     *
     * @template T - The type to cast.
     * @template U - The type to cast to.
     *
     * @example
     * type T0 = _$cast<123, string> // Causes a type error
     * type T1 = _$cast<'hello', string> // 'hello'
     */
    export type _$cast<T, U> = T extends U ? T : U
    =======
    /**
     * `_$cast` is a type-level function that casts a type `T` to a type `U`.
     *
     * @template T - The type to cast.
     * @template U - The type to cast to.
     *
     * @example
     * type T0 = _$cast<true, true> // true
     * type T1 = _$cast<boolean, true> // true
     * type T2 = _$cast<true, boolean> // true
     */
    export type _$cast<T, U> = T extends U ? T : U
    >>>>>>> updated
    </source>

--- src/type/cast.ts
+++ src/type/cast.ts
@@ -2,14 +2,14 @@ import { Kind } from '..'
 
 /**
  * `_$cast` is a type-level function that casts a type `T` to a type `U`.
- * It is used in custom kind development to encode input constraints.
  *
  * @template T - The type to cast.
  * @template U - The type to cast to.
  *
  * @example
- * type T0 = _$cast<123, string> // Causes a type error
- * type T1 = _$cast<'hello', string> // 'hello'
+ * type T0 = _$cast<true, true> // true
+ * type T1 = _$cast<boolean, true> // true
+ * type T2 = _$cast<true, boolean> // true
  */
 export type _$cast<T, U> = T extends U ? T : U
 
