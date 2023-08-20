712ffbb1fd74c44f773e742d7cb14637de64ed55
o <poteat@users.noreply.github.com>
2023-08-20 16:00:52 -0700

    aider: Initialized the jsdoc script to take a glob pattern as input, generate a list of commands based on that pattern, and ask for confirmation before running those commands in parallel using the `aider` tool.
    # Aider chat conversation:
    
    USER: Can you initialize the jsdoc script according to the instructions in it? I want to run aider in parallel on a bunch of files given a script template
    
    ASSISTANT: Sure, I understand that you want to create a script that can take a glob pattern as input, generate a list of commands based on that pattern, and then ask for confirmation before running those commands in parallel using the `aider` tool.
    
    To achieve this, we need to modify the `scaffolding/jsdoc.ts` file. We will use the `glob` package to match the files, the `inquirer` package to ask for confirmation, and the `child_process` package to run the commands in parallel.
    
    Here are the steps we need to take:
    
    1. Import the necessary packages (`glob`, `inquirer`, `child_process`).
    2. Parse the command-line arguments to get the glob pattern and the script template.
    3. Use the `glob` package to get a list of files that match the pattern.
    4. Generate the list of commands based on the script template and the matched files.
    5. Use the `inquirer` package to ask for confirmation before running the commands.
    6. If the user confirms, use the `child_process` package to run the commands in parallel.
    
    Here is the proposed change:
    
    <source>typescript
    scaffolding/jsdoc.ts
    <<<<<<< HEAD
    // aider -msg="Can we write JSDoc for 'src/string/from-list' per 'style-guide/jsdoc.md' ? The tests might provide helpful context" src/string/from-list.ts src/string/from-list.spec.ts style-guide/jsdoc.md
    
    // Basically I want this to be a cli command where I can pass in a glob syntax and run the above command in parallel for all files. It should list all of the commands and ask me whether I want to proceed before it runs aider.
    =======
    import * as glob from 'glob';
    import * as inquirer from 'inquirer';
    import { exec } from 'child_process';
    
    const argv = require('yargs/yargs')(process.argv.slice(2))
        .option('pattern', {
            alias: 'p',
            description: 'Glob pattern for input files',
            type: 'string',
            demandOption: true,
        })
        .option('template', {
            alias: 't',
            description: 'Script template',
            type: 'string',
            demandOption: true,
        })
        .help()
        .alias('help', 'h').argv;
    
    const files = glob.sync(argv.pattern);
    const commands = files.map(file => argv.template.replace('{}', file));
    
    console.log('The following commands will be run:');
    console.log(commands.join('\n'));
    
    inquirer.prompt([{
        type: 'confirm',
        name: 'proceed',
        message: 'Do you want to proceed?',
        default: false
    }]).then(answers => {
        if (answers.proceed) {
            commands.forEach(command => {
                exec(command, (error, stdout, stderr) => {
                    if (error) {
                        console.error(`exec error: ${error}`);
                        return;
                    }
                    console.log(`stdout: ${stdout}`);
                    console.error(`stderr: ${stderr}`);
                });
            });
        }
    });
    >>>>>>> updated
    </source>
    
    Please note that this script assumes that the `aider` tool is available in the system's PATH. Also, the script template should include `{}` where the file name should be inserted. For example, the script template could be `aider -msg="Can we write JSDoc for '{}' per 'style-guide/jsdoc.md' ? The tests might provide helpful context" {} {}.spec.ts style-guide/jsdoc.md`.

--- scaffolding/jsdoc.ts
+++ scaffolding/jsdoc.ts
@@ -1,3 +1,45 @@
-// aider -msg="Can we write JSDoc for 'src/string/from-list' per 'style-guide/jsdoc.md' ? The tests might provide helpful context" src/string/from-list.ts src/string/from-list.spec.ts style-guide/jsdoc.md
+import * as glob from 'glob';
+import * as inquirer from 'inquirer';
+import { exec } from 'child_process';
 
-// Basically I want this to be a cli command where I can pass in a glob syntax and run the above command in parallel for all files. It should list all of the commands and ask me whether I want to proceed before it runs aider.
+const argv = require('yargs/yargs')(process.argv.slice(2))
+    .option('pattern', {
+        alias: 'p',
+        description: 'Glob pattern for input files',
+        type: 'string',
+        demandOption: true,
+    })
+    .option('template', {
+        alias: 't',
+        description: 'Script template',
+        type: 'string',
+        demandOption: true,
+    })
+    .help()
+    .alias('help', 'h').argv;
+
+const files = glob.sync(argv.pattern);
+const commands = files.map(file => argv.template.replace('{}', file));
+
+console.log('The following commands will be run:');
+console.log(commands.join('\n'));
+
+inquirer.prompt([{
+    type: 'confirm',
+    name: 'proceed',
+    message: 'Do you want to proceed?',
+    default: false
+}]).then(answers => {
+    if (answers.proceed) {
+        commands.forEach(command => {
+            exec(command, (error, stdout, stderr) => {
+                if (error) {
+                    console.error(`exec error: ${error}`);
+                    return;
+                }
+                console.log(`stdout: ${stdout}`);
+                console.error(`stderr: ${stderr}`);
+            });
+        });
+    }
+});
