71d0c9eac5ce0405f80ae8a06e9b837a282bb515
o <poteat@users.noreply.github.com>
2023-08-20 00:09:38 -0700

    aider: Added JSDoc comments to `src/number/is-integer.ts`.
    # Aider chat conversation:
    
    USER: Can we add JSDoc to `src/number/is-integer.ts` ?
    
    ASSISTANT: Sure, we can add JSDoc comments to the `src/number/is-integer.ts` file. Here are the changes:
    
    1. Add JSDoc to the `_$isInteger` type utility.
    2. Add JSDoc to the `IsInteger` interface.
    
    Here are the proposed changes:
    
    <source>typescript
    src/number/is-integer.ts
    <<<<<<< HEAD
    export type _$isInteger<T extends Number.Number> = `${T}` extends `${bigint}`
      ? T extends `0x${string}`
        ? false
        : true
      : false
    
    export interface IsInteger extends Kind.Kind {
      f(x: Type._$cast<this[Kind._], Number.Number>): _$isInteger<typeof x>
    }
    =======
    /**
     * `Number.IsInteger` is a type-level function that checks if a number is an integer.
     *
     * @template T - The number to check.
     *
     * @example
     * type T0 = Number._$isInteger<1> // true
     * type T1 = Number._$isInteger<1.5> // false
     */
    export type _$isInteger<T extends Number.Number> = `${T}` extends `${bigint}`
      ? T extends `0x${string}`
        ? false
        : true
      : false
    
    /**
     * `Number.IsInteger` is a type-level function that checks if a number is an integer.
     *
     * @template T - The number to check.
     *
     * @example
     * type T0 = $<Number.IsInteger, 1> // true
     * type T1 = $<Number.IsInteger, 1.5> // false
     */
    export interface IsInteger extends Kind.Kind {
      f(x: Type._$cast<this[Kind._], Number.Number>): _$isInteger<typeof x>
    }
    >>>>>>> updated
    </source>

--- src/number/is-integer.ts
+++ src/number/is-integer.ts
@@ -1,11 +1,29 @@
 import { Type, Kind, Number } from '..'
 
+/**
+ * `Number.IsInteger` is a type-level function that checks if a number is an integer.
+ * 
+ * @template T - The number to check.
+ * 
+ * @example
+ * type T0 = Number._$isInteger<1> // true
+ * type T1 = Number._$isInteger<1.5> // false
+ */
 export type _$isInteger<T extends Number.Number> = `${T}` extends `${bigint}`
   ? T extends `0x${string}`
     ? false
     : true
   : false
 
+/**
+ * `Number.IsInteger` is a type-level function that checks if a number is an integer.
+ * 
+ * @template T - The number to check.
+ * 
+ * @example
+ * type T0 = $<Number.IsInteger, 1> // true
+ * type T1 = $<Number.IsInteger, 1.5> // false
+ */
 export interface IsInteger extends Kind.Kind {
   f(x: Type._$cast<this[Kind._], Number.Number>): _$isInteger<typeof x>
 }
