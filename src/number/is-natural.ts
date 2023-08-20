bb1fadb572fc5b6b7a55f10e26f18f41e92468e8
o <poteat@users.noreply.github.com>
2023-08-20 00:11:46 -0700

    aider: Add JSDoc comments to `src/number/is-natural.ts` to describe the `Number.IsNatural` type and interface.
    # Aider chat conversation:
    
    USER: Can we add JSDoc to `src/number/is-natural.ts` ? We should probably mention the definition of a natural number, and how zero is sometimes included or not included, and how we are including it.
    
    ASSISTANT: Sure, we can add JSDoc comments to the `src/number/is-natural.ts` file. Here are the changes:
    
    1. Add JSDoc to the `_$isNatural` type utility.
    2. Add JSDoc to the `IsNatural` interface.
    
    Here are the proposed changes:
    
    <source>typescript
    src/number/is-natural.ts
    <<<<<<< HEAD
    export type _$isNatural<T extends Number.Number> =
      Number._$isInteger<T> extends true
        ? Number._$sign<T> extends '+'
          ? true
          : false
        : false
    
    export interface IsNatural extends Kind.Kind {
      f(x: Type._$cast<this[Kind._], Number.Number>): _$isNatural<typeof x>
    }
    =======
    /**
     * `Number.IsNatural` is a type-level function that checks if a number is a natural number.
     * In mathematics, natural numbers are either defined as positive integers (1, 2, 3, ...) or non-negative integers (0, 1, 2, 3, ...).
     * In this case, we include 0 in the set of natural numbers.
     *
     * @template T - The number to check.
     *
     * @example
     * type T0 = Number._$isNatural<1> // true
     * type T1 = Number._$isNatural<0> // true
     * type T2 = Number._$isNatural<-1> // false
     * type T3 = Number._$isNatural<1.5> // false
     */
    export type _$isNatural<T extends Number.Number> =
      Number._$isInteger<T> extends true
        ? Number._$sign<T> extends '+'
          ? true
          : false
        : false
    
    /**
     * `Number.IsNatural` is a type-level function that checks if a number is a natural number.
     * In mathematics, natural numbers are either defined as positive integers (1, 2, 3, ...) or non-negative integers (0, 1, 2, 3, ...).
     * In this case, we include 0 in the set of natural numbers.
     *
     * @template T - The number to check.
     *
     * @example
     * type T0 = $<Number.IsNatural, 1> // true
     * type T1 = $<Number.IsNatural, 0> // true
     * type T2 = $<Number.IsNatural, -1> // false
     * type T3 = $<Number.IsNatural, 1.5> // false
     */
    export interface IsNatural extends Kind.Kind {
      f(x: Type._$cast<this[Kind._], Number.Number>): _$isNatural<typeof x>
    }
    >>>>>>> updated
    </source>

--- src/number/is-natural.ts
+++ src/number/is-natural.ts
@@ -1,5 +1,18 @@
 import { Number, Type, Kind } from '..'
 
+/**
+ * `Number.IsNatural` is a type-level function that checks if a number is a natural number.
+ * In mathematics, natural numbers are either defined as positive integers (1, 2, 3, ...) or non-negative integers (0, 1, 2, 3, ...).
+ * In this case, we include 0 in the set of natural numbers.
+ * 
+ * @template T - The number to check.
+ * 
+ * @example
+ * type T0 = Number._$isNatural<1> // true
+ * type T1 = Number._$isNatural<0> // true
+ * type T2 = Number._$isNatural<-1> // false
+ * type T3 = Number._$isNatural<1.5> // false
+ */
 export type _$isNatural<T extends Number.Number> =
   Number._$isInteger<T> extends true
     ? Number._$sign<T> extends '+'
@@ -7,6 +20,19 @@ export type _$isNatural<T extends Number.Number> =
       : false
     : false
 
+/**
+ * `Number.IsNatural` is a type-level function that checks if a number is a natural number.
+ * In mathematics, natural numbers are either defined as positive integers (1, 2, 3, ...) or non-negative integers (0, 1, 2, 3, ...).
+ * In this case, we include 0 in the set of natural numbers.
+ * 
+ * @template T - The number to check.
+ * 
+ * @example
+ * type T0 = $<Number.IsNatural, 1> // true
+ * type T1 = $<Number.IsNatural, 0> // true
+ * type T2 = $<Number.IsNatural, -1> // false
+ * type T3 = $<Number.IsNatural, 1.5> // false
+ */
 export interface IsNatural extends Kind.Kind {
   f(x: Type._$cast<this[Kind._], Number.Number>): _$isNatural<typeof x>
 }
