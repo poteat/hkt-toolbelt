d066b75fdb2bcab5618b097fba29fd3f7d980508
o <poteat@users.noreply.github.com>
2023-08-20 01:28:35 -0700

    aider: Added JSDoc comments to `src/type/value-of.ts` for `_$valueOf` and `ValueOf` types.
    # Aider chat conversation:
    
    USER: Can we add JSDoc for `src/type/value-of.ts` ? The tests in `src/type/value-of.spec.ts` may provide useful context.
    
    ASSISTANT: Sure, we can add JSDoc comments to the `src/type/value-of.ts` file. Here is the proposed change:
    
    <source>typescript
    src/type/value-of.ts
    <<<<<<< HEAD
    import { Kind } from '..'
    
    export type _$valueOf<T> = T extends unknown[] ? T[number] : T[keyof T]
    
    export interface ValueOf extends Kind.Kind {
      f(x: this[Kind._]): _$valueOf<typeof x>
    }
    =======
    import { Kind } from '..'
    
    /**
     * `_$valueOf` is a type-level function that extracts the values associated
     * with the type, if any exist, via `keyof`.
     *
     * @template T - The type to extract values from.
     *
     * @example
     * type T0 = _$valueOf<['foo', 'bar']> // 'foo' | 'bar'
     * type T1 = _$valueOf<{ foo: 'foo'; bar: 'bar' }> // 'foo' | 'bar'
     */
    export type _$valueOf<T> = T extends unknown[] ? T[number] : T[keyof T]
    
    /**
     * `ValueOf` is a type-level function that extracts the values associated
     * with the type, if any exist, via `keyof`.
     *
     * @template T - The type to extract values from.
     *
     * @example
     * type T0 = $<ValueOf, ['foo', 'bar']> // 'foo' | 'bar'
     * type T1 = $<ValueOf, { foo: 'foo'; bar: 'bar' }> // 'foo' | 'bar'
     */
    export interface ValueOf extends Kind.Kind {
      f(x: this[Kind._]): _$valueOf<typeof x>
    }
    >>>>>>> updated
    </source>

--- src/type/value-of.ts
+++ src/type/value-of.ts
@@ -1,7 +1,27 @@
 import { Kind } from '..'
 
+/**
+ * `_$valueOf` is a type-level function that extracts the values associated
+ * with the type, if any exist, via `keyof`.
+ * 
+ * @template T - The type to extract values from.
+ * 
+ * @example
+ * type T0 = _$valueOf<['foo', 'bar']> // 'foo' | 'bar'
+ * type T1 = _$valueOf<{ foo: 'foo'; bar: 'bar' }> // 'foo' | 'bar'
+ */
 export type _$valueOf<T> = T extends unknown[] ? T[number] : T[keyof T]
 
+/**
+ * `ValueOf` is a type-level function that extracts the values associated
+ * with the type, if any exist, via `keyof`.
+ * 
+ * @template T - The type to extract values from.
+ * 
+ * @example
+ * type T0 = $<ValueOf, ['foo', 'bar']> // 'foo' | 'bar'
+ * type T1 = $<ValueOf, { foo: 'foo'; bar: 'bar' }> // 'foo' | 'bar'
+ */
 export interface ValueOf extends Kind.Kind {
   f(x: this[Kind._]): _$valueOf<typeof x>
 }
