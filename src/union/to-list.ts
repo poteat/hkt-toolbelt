fa095235a8e480b23fd72b8de3c9cd59c1720233
o <poteat@users.noreply.github.com>
2023-08-20 00:49:40 -0700

    Refactor `_$toList` and `ToList` type-level functions to improve readability and consistency.

--- src/union/to-list.ts
+++ src/union/to-list.ts
@@ -2,10 +2,10 @@ import { Kind, Union } from '..'
 
 /**
  * `_$toList` is a type-level function that converts a union type to a list (tuple) type.
- * 
+ *
  * @template T - The union type to convert to a list.
  * @template O - An optional parameter for internal use.
- * 
+ *
  * @example
  * type T0 = _$toList<1 | 2 | 3> // [1, 2, 3]
  * type T1 = _$toList<string | boolean> // [string, boolean]
@@ -18,9 +18,9 @@ export type _$toList<T, O extends unknown[] = []> = Union._$toIntersection<
 
 /**
  * `ToList` is a type-level function that converts a union type to a list (tuple) type.
- * 
+ *
  * @template T - The union type to convert to a list.
- * 
+ *
  * @example
  * type T0 = $<ToList, 1 | 2 | 3> // [1, 2, 3]
  * type T1 = $<ToList, string | boolean> // [string, boolean]
