e3d6f4449ed5c7cc269c99f7fe0dd76e2b6c3ff7
o <poteat@users.noreply.github.com>
2023-08-20 01:17:13 -0700

    aider: Added JSDoc comments for `_$display` type and `Display` interface in `src/type/display.ts`.
    # Aider chat conversation:
    
    USER: Can we add JSDoc for `src/type/display.ts` ? The tests in `src/type/display.spec.ts` may give helpful context.
    
    ASSISTANT: Sure, we can add JSDoc comments to the `src/type/display.ts` file.
    
    The JSDoc comments will provide a brief description of the `_$display` type and `Display` interface, and will include an example usage based on the test in `src/type/display.spec.ts`.
    
    Here are the changes:
    
    <source>typescript
    src/type/display.ts
    <<<<<<< HEAD
    import { Kind } from '..'
    
    export type _$display<T> = T extends (...args: never[]) => unknown
      ? T
      : T extends abstract new (...args: never[]) => unknown
      ? T
      : {
          [key in keyof T]: T[key]
        }
    
    export interface Display extends Kind.Kind {
      f(x: this[Kind._]): _$display<this[Kind._]>
    }
    =======
    import { Kind } from '..'
    
    /**
     * `_$display` is a type-level function that forces the compiler to resolve
     * types such that IDEs can display them.
     *
     * @template T - The type to be displayed.
     *
     * @example
     * type T0 = _$display<'foo'> // 'foo'
     */
    export type _$display<T> = T extends (...args: never[]) => unknown
      ? T
      : T extends abstract new (...args: never[]) => unknown
      ? T
      : {
          [key in keyof T]: T[key]
        }
    
    /**
     * `Display` is a type-level function that forces the compiler to resolve
     * types such that IDEs can display them.
     *
     * @template T - The type to be displayed.
     *
     * @example
     * type T0 = $<Display, 'foo'> // 'foo'
     */
    export interface Display extends Kind.Kind {
      f(x: this[Kind._]): _$display<this[Kind._]>
    }
    >>>>>>> updated
    </source>

--- src/type/display.ts
+++ src/type/display.ts
@@ -1,5 +1,14 @@
 import { Kind } from '..'
 
+/**
+ * `_$display` is a type-level function that forces the compiler to resolve
+ * types such that IDEs can display them.
+ * 
+ * @template T - The type to be displayed.
+ * 
+ * @example
+ * type T0 = _$display<'foo'> // 'foo'
+ */
 export type _$display<T> = T extends (...args: never[]) => unknown
   ? T
   : T extends abstract new (...args: never[]) => unknown
@@ -8,6 +17,15 @@ export type _$display<T> = T extends (...args: never[]) => unknown
       [key in keyof T]: T[key]
     }
 
+/**
+ * `Display` is a type-level function that forces the compiler to resolve
+ * types such that IDEs can display them.
+ * 
+ * @template T - The type to be displayed.
+ * 
+ * @example
+ * type T0 = $<Display, 'foo'> // 'foo'
+ */
 export interface Display extends Kind.Kind {
   f(x: this[Kind._]): _$display<this[Kind._]>
 }
