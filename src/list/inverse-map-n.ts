9f85d2479520f95c42b5c7a9815e329c82e4c9b6
Jongsun Suh <34228073+MajorLift@users.noreply.github.com>
2023-08-20 09:47:28 -0700

    Apply linter fixes

--- src/list/inverse-map-n.ts
+++ src/list/inverse-map-n.ts
@@ -6,8 +6,8 @@ import { Type, Kind } from '..'
  * and a target list of arguments which will be passed into all of these functions using `$N`
  * It returns a list filled with the returned results of applying the functions to the arguments.
  *
- * @param T A list of type-level functions that transform inputs and return the result.
- * @param X A list of types. The arguments to be passed into every element of `T`.
+ * @param T - A list of type-level functions that transform inputs and return the result.
+ * @param X - A list of types. The arguments to be passed into every element of `T`.
  */
 export type _$inverseMapN<T extends Kind.Kind[], X extends unknown[]> = {
   [key in keyof T]: Kind._$uncurry<T[key], X>
@@ -23,8 +23,8 @@ interface InverseMapN_T<T extends Kind.Kind[]> extends Kind.Kind {
  * and a target list of arguments which will be passed into all of these functions using `$N`
  * It returns a list filled with the returned results of applying the functions to the arguments.
  *
- * @param T A list of type-level functions that transform inputs and return the result.
- * @param X A list of types. The arguments to be passed into every element of `T`.
+ * @param T - A list of type-level functions that transform inputs and return the result.
+ * @param X - A list of types. The arguments to be passed into every element of `T`.
  */
 export interface InverseMapN extends Kind.Kind {
   f(x: Type._$cast<this[Kind._], Kind.Kind[]>): InverseMapN_T<typeof x>
