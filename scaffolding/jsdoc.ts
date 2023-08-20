013f5feedcad5497968b393007c110501850639b
o <poteat@users.noreply.github.com>
2023-08-20 15:59:40 -0700

    aider: Added scaffolding/jsdoc.ts

---
+++ scaffolding/jsdoc.ts
@@ -0,0 +1,3 @@
+// aider -msg="Can we write JSDoc for 'src/string/from-list' per 'style-guide/jsdoc.md' ? The tests might provide helpful context" src/string/from-list.ts src/string/from-list.spec.ts style-guide/jsdoc.md
+
+// Basically I want this to be a cli command where I can pass in a glob syntax and run the above command in parallel for all files. It should list all of the commands and ask me whether I want to proceed before it runs aider.
