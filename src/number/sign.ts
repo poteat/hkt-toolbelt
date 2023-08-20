26ea0bd730a48618330794bf8f386afe984bcdcb
o <poteat@users.noreply.github.com>
2023-08-20 00:25:05 -0700

    aider: Specify that the sign of zero is considered positive in the JSDoc comments in `src/number/sign.ts`.
    # Aider chat conversation:
    
    USER: Can we additionally specify that the sign of zero is positive? This is not the case in math generally speaking.
    
    ASSISTANT: Sure, we can specify that the sign of zero is considered positive in this context. We will need to modify the JSDoc comments in `src/number/sign.ts` to include this information.
    
    Here are the specific changes:
    
    <source>typescript
    src/number/sign.ts
    <<<<<<< HEAD
    /**
     * `_$sign` is a type-level function that returns the sign of a number.
     *
     * @template T - The number to get the sign of.
     *
     * @example
     * type T0 = _$sign<5> // '+'
     * type T1 = _$sign<-5> // '-'
     */
    export type _$sign<T extends Number.Number> = number extends T
      ? '+' | '-'
      : `${T}` extends `-${string}`
      ? '-'
      : '+'
    
    /**
     * `Number.Sign` is a type-level function that returns the sign of a number.
     *
     * @template T - The number to get the sign of.
     *
     * @example
     * type T0 = $<Number.Sign, 5> // '+'
     * type T1 = $<Number.Sign, -5> // '-'
     */
    export interface Sign extends Kind.Kind {
      f(x: Type._$cast<this[Kind._], Number.Number>): _$sign<typeof x>
    }
    =======
    /**
     * `_$sign` is a type-level function that returns the sign of a number.
     * In this context, the sign of zero is considered positive.
     *
     * @template T - The number to get the sign of.
     *
     * @example
     * type T0 = _$sign<5> // '+'
     * type T1 = _$sign<-5> // '-'
     * type T2 = _$sign<0> // '+'
     */
    export type _$sign<T extends Number.Number> = number extends T
      ? '+' | '-'
      : `${T}` extends `-${string}`
      ? '-'
      : '+'
    
    /**
     * `Number.Sign` is a type-level function that returns the sign of a number.
     * In this context, the sign of zero is considered positive.
     *
     * @template T - The number to get the sign of.
     *
     * @example
     * type T0 = $<Number.Sign, 5> // '+'
     * type T1 = $<Number.Sign, -5> // '-'
     * type T2 = $<Number.Sign, 0> // '+'
     */
    export interface Sign extends Kind.Kind {
      f(x: Type._$cast<this[Kind._], Number.Number>): _$sign<typeof x>
    }
    >>>>>>> updated
    </source>

--- src/number/sign.ts
+++ src/number/sign.ts
@@ -2,12 +2,14 @@ import { Type, Kind, Number } from '..'
 
 /**
  * `_$sign` is a type-level function that returns the sign of a number.
+ * In this context, the sign of zero is considered positive.
  *
  * @template T - The number to get the sign of.
  *
  * @example
  * type T0 = _$sign<5> // '+'
  * type T1 = _$sign<-5> // '-'
+ * type T2 = _$sign<0> // '+'
  */
 export type _$sign<T extends Number.Number> = number extends T
   ? '+' | '-'
@@ -17,12 +19,14 @@ export type _$sign<T extends Number.Number> = number extends T
 
 /**
  * `Number.Sign` is a type-level function that returns the sign of a number.
+ * In this context, the sign of zero is considered positive.
  *
  * @template T - The number to get the sign of.
  *
  * @example
  * type T0 = $<Number.Sign, 5> // '+'
  * type T1 = $<Number.Sign, -5> // '-'
+ * type T2 = $<Number.Sign, 0> // '+'
  */
 export interface Sign extends Kind.Kind {
   f(x: Type._$cast<this[Kind._], Number.Number>): _$sign<typeof x>
