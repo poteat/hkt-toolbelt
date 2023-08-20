ae2c11ee4a1eb0788bcf812ca60a21c30751bc89
o <poteat@users.noreply.github.com>
2023-08-20 00:38:36 -0700

    aider: Added JSDoc comments to `src/union/length.ts` file.
    # Aider chat conversation:
    
    USER: Can we add JSDoc to `src/union/length.ts` ? `src/string/length.spec.ts` may be helpful.
    
    ASSISTANT: Sure, we can add JSDoc comments to the `src/union/length.ts` file. The `src/string/length.spec.ts` file provides examples of how to use the `Length` interface, which can be helpful in writing the JSDoc comments.
    
    Here are the steps we need to take:
    
    1. Open the `src/union/length.ts` file.
    2. Add JSDoc comments to the `_$length` type and the `Length` interface.
    
    Here are the changes:
    
    <source>typescript
    src/union/length.ts
    <<<<<<< HEAD
    export type _$length<T> = Type._$isNever<T> extends true
      ? 0
      : Union._$toList<T> extends infer X extends unknown[]
      ? X['length']
      : never
    
    export interface Length extends Kind.Kind {
      f(x: this[Kind._]): _$length<typeof x>
    }
    =======
    /**
     * `Union._$length` is a type-level function that returns the length of a union.
     *
     * @template T - The union to get the length of.
     *
     * @example
     * type T0 = Union._$length<1 | 2 | 3> // 3
     * type T1 = Union._$length<never> // 0
     */
    export type _$length<T> = Type._$isNever<T> extends true
      ? 0
      : Union._$toList<T> extends infer X extends unknown[]
      ? X['length']
      : never
    
    /**
     * `Union.Length` is a type-level function that returns the length of a union.
     *
     * @template T - The union to get the length of.
     *
     * @example
     * type T0 = $<Union.Length, 1 | 2 | 3> // 3
     * type T1 = $<Union.Length, never> // 0
     */
    export interface Length extends Kind.Kind {
      f(x: this[Kind._]): _$length<typeof x>
    }
    >>>>>>> updated
    </source>

--- src/union/length.ts
+++ src/union/length.ts
@@ -1,11 +1,29 @@
 import { Kind, Union, Type } from '..'
 
+/**
+ * `Union._$length` is a type-level function that returns the length of a union.
+ * 
+ * @template T - The union to get the length of.
+ * 
+ * @example
+ * type T0 = Union._$length<1 | 2 | 3> // 3
+ * type T1 = Union._$length<never> // 0
+ */
 export type _$length<T> = Type._$isNever<T> extends true
   ? 0
   : Union._$toList<T> extends infer X extends unknown[]
   ? X['length']
   : never
 
+/**
+ * `Union.Length` is a type-level function that returns the length of a union.
+ * 
+ * @template T - The union to get the length of.
+ * 
+ * @example
+ * type T0 = $<Union.Length, 1 | 2 | 3> // 3
+ * type T1 = $<Union.Length, never> // 0
+ */
 export interface Length extends Kind.Kind {
   f(x: this[Kind._]): _$length<typeof x>
 }
