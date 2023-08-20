6fde0f6a59358909d7292f1e3444c9394a8bb63c
o <poteat@users.noreply.github.com>
2023-08-20 00:59:00 -0700

    Clarify further about Expect mechanics

--- src/test/expect.ts
+++ src/test/expect.ts
@@ -16,14 +16,17 @@ export abstract class _ {
  * If `V` is never, it returns `X`. If `X` is never, it returns `Expect<X, V>`.
  * The purpose of this function is to cause a compiler error to be emitted if the types are not equivalent.
  *
+ * Finally, if `never` is provided and it doesn't match, we cause an infinite
+ * loop error to be emitted so that the tests fail.
+ *
  * @template X - The type to check.
  * @template V - The type to compare with. Default is `true`.
  *
  * @example
  * type T0 = Expect<true> // true
- * type T1 = Expect<false> // false & _
- * type T2 = Expect<number, true> // true & _
- * type T3 = Expect<never, true> // Expect<never, true>
+ * type T1 = Expect<false> // false & _ (compiler error)
+ * type T2 = Expect<number, true> // true & _ (compiler error)
+ * type T3 = Expect<never, true> // Expect<never, true> (inf compiler error)
  */
 export type Expect<
   X extends Conditional._$equals<X, V> extends true ? V : V & _,
