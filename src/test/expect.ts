58cfd5db1de03b01b9a586640d5499239f5660af
o <poteat@users.noreply.github.com>
2023-08-20 00:56:54 -0700

    Clarify purpose of _ in test module

--- src/test/expect.ts
+++ src/test/expect.ts
@@ -2,8 +2,9 @@ import { Conditional, Type } from '..'
 
 /**
  * Abstract class used for internal purposes.
- * The purpose of `_` is to allow us to show the expected type, but also not to accept mere subtype relations,
- * and to force a necessity of symmetric type equality.
+ * The purpose of `_` is to allow us to show the expected type, but also not
+ * allow only subtype relations to succeed. Instead, this type forces an error
+ * in the compiler if the types are not equivalent.
  */
 export abstract class _ {
   abstract readonly _: symbol
@@ -14,10 +15,10 @@ export abstract class _ {
  * If `X` equals `V`, it returns `V`. If `X` does not equal `V`, it returns `V & _`.
  * If `V` is never, it returns `X`. If `X` is never, it returns `Expect<X, V>`.
  * The purpose of this function is to cause a compiler error to be emitted if the types are not equivalent.
- * 
+ *
  * @template X - The type to check.
  * @template V - The type to compare with. Default is `true`.
- * 
+ *
  * @example
  * type T0 = Expect<true> // true
  * type T1 = Expect<false> // false & _
