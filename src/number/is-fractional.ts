26e6c35a1daf55e53e426a5a767f2f5f5346d140
o <poteat@users.noreply.github.com>
2023-08-20 00:08:35 -0700

    aider: Added JSDoc comments to `src/number/is-fractional.ts`.
    # Aider chat conversation:
    
    USER: Can we add JSDoc to `src/number/is-fractional.ts`?
    
    ASSISTANT: Sure, we can add JSDoc comments to the `src/number/is-fractional.ts` file. Here are the
    ^C KeyboardInterrupt
    
    USER: Can we add JSDoc to `src/number/is-fractional.ts` ?
    
    ASSISTANT: Sure, we can add JSDoc comments to the `src/number/is-fractional.ts` file. Here are the changes:
    
    1. Add JSDoc to the `_$isFractional` type utility.
    2. Add JSDoc to the `IsFractional` interface.
    
    Here are the proposed changes:
    
    <source>typescript
    src/number/is-fractional.ts
    <<<<<<< HEAD
    export type _$isFractional<
      T extends Number.Number,
      IS_INTEGER = Number._$isInteger<T>
    > = number extends T ? false : IS_INTEGER extends true ? false : true
    
    export interface IsFractional extends Kind.Kind {
      f(x: Type._$cast<this[Kind._], Number.Number>): _$isFractional<typeof x>
    }
    =======
    /**
     * `Number.IsFractional` is a type-level function that checks if a number is fractional.
     *
     * @template T - The number to check.
     *
     * @example
     * type T0 = Number._$isFractional<1.5> // true
     * type T1 = Number._$isFractional<1> // false
     */
    export type _$isFractional<
      T extends Number.Number,
      IS_INTEGER = Number._$isInteger<T>
    > = number extends T ? false : IS_INTEGER extends true ? false : true
    
    /**
     * `Number.IsFractional` is a type-level function that checks if a number is fractional.
     *
     * @template T - The number to check.
     *
     * @example
     * type T0 = $<Number.IsFractional, 1.5> // true
     * type T1 = $<Number.IsFractional, 1> // false
     */
    export interface IsFractional extends Kind.Kind {
      f(x: Type._$cast<this[Kind._], Number.Number>): _$isFractional<typeof x>
    }
    >>>>>>> updated
    </source>

--- src/number/is-fractional.ts
+++ src/number/is-fractional.ts
@@ -1,10 +1,28 @@
 import { Number, Kind, Type } from '..'
 
+/**
+ * `Number.IsFractional` is a type-level function that checks if a number is fractional.
+ * 
+ * @template T - The number to check.
+ * 
+ * @example
+ * type T0 = Number._$isFractional<1.5> // true
+ * type T1 = Number._$isFractional<1> // false
+ */
 export type _$isFractional<
   T extends Number.Number,
   IS_INTEGER = Number._$isInteger<T>
 > = number extends T ? false : IS_INTEGER extends true ? false : true
 
+/**
+ * `Number.IsFractional` is a type-level function that checks if a number is fractional.
+ * 
+ * @template T - The number to check.
+ * 
+ * @example
+ * type T0 = $<Number.IsFractional, 1.5> // true
+ * type T1 = $<Number.IsFractional, 1> // false
+ */
 export interface IsFractional extends Kind.Kind {
   f(x: Type._$cast<this[Kind._], Number.Number>): _$isFractional<typeof x>
 }
