89a7f0e5adb9f3de942a8010544767440eb48ec5
o <poteat@users.noreply.github.com>
2023-08-20 01:29:43 -0700

    aider: Refactor JSDoc comments in `src/type/value-of.ts` to provide a more detailed explanation of the `_$valueOf` and `ValueOf` type-level functions.
    # Aider chat conversation:
    
    ASSISTANT: Sure, we can add this explanation to the JSDoc comments in the `src/type/value-of.ts` file. Here is the proposed change:
    
    <source>typescript
    src/type/value-of.ts
    <<<<<<< HEAD
    /**
     * `_$valueOf` is a type-level function that extracts the values associated
     * with the type, if any exist, via `keyof`.
     *
     * @template T - The type to extract values from.
     *
     * @example
     * type T0 = _$valueOf<['foo', 'bar']> // 'foo' | 'bar'
     * type T1 = _$valueOf<{ foo: 'foo'; bar: 'bar' }> // 'foo' | 'bar'
     */
    export type _$valueOf<T> = T extends unknown[] ? T[number] : T[keyof T]
    
    /**
     * `ValueOf` is a type-level function that extracts the values associated
     * with the type, if any exist, via `keyof`.
     *
     * @template T - The type to extract values from.
     *
     * @example
     * type T0 = $<ValueOf, ['foo', 'bar']> // 'foo' | 'bar'
     * type T1 = $<ValueOf, { foo: 'foo'; bar: 'bar' }> // 'foo' | 'bar'
     */
    export interface ValueOf extends Kind.Kind {
      f(x: this[Kind._]): _$valueOf<typeof x>
    }
    =======
    /**
     * `_$valueOf` is a type-level function that extracts the values associated
     * with the type, if any exist, via `keyof`. For arrays, values consist of their
     * elements, while for objects, values consist of the values associated with each key.
     * In both circumstances, we receive a union of all possible entries.
     *
     * @template T - The type to extract values from.
     *
     * @example
     * type T0 = _$valueOf<['foo', 'bar']> // 'foo' | 'bar'
     * type T1 = _$valueOf<{ foo: 'foo'; bar: 'bar' }> // 'foo' | 'bar'
     */
    export type _$valueOf<T> = T extends unknown[] ? T[number] : T[keyof T]
    
    /**
     * `ValueOf` is a type-level function that extracts the values associated
     * with the type, if any exist, via `keyof`. For arrays, values consist of their
     * elements, while for objects, values consist of the values associated with each key.
     * In both circumstances, we receive a union of all possible entries.
     *
     * @template T - The type to extract values from.
     *
     * @example
     * type T0 = $<ValueOf, ['foo', 'bar']> // 'foo' | 'bar'
     * type T1 = $<ValueOf, { foo: 'foo'; bar: 'bar' }> // 'foo' | 'bar'
     */
    export interface ValueOf extends Kind.Kind {
      f(x: this[Kind._]): _$valueOf<typeof x>
    }
    >>>>>>> updated
    </source>

--- src/type/value-of.ts
+++ src/type/value-of.ts
@@ -2,7 +2,9 @@ import { Kind } from '..'
 
 /**
  * `_$valueOf` is a type-level function that extracts the values associated
- * with the type, if any exist, via `keyof`.
+ * with the type, if any exist, via `keyof`. For arrays, values consist of their
+ * elements, while for objects, values consist of the values associated with each key.
+ * In both circumstances, we receive a union of all possible entries.
  *
  * @template T - The type to extract values from.
  *
@@ -14,7 +16,9 @@ export type _$valueOf<T> = T extends unknown[] ? T[number] : T[keyof T]
 
 /**
  * `ValueOf` is a type-level function that extracts the values associated
- * with the type, if any exist, via `keyof`.
+ * with the type, if any exist, via `keyof`. For arrays, values consist of their
+ * elements, while for objects, values consist of the values associated with each key.
+ * In both circumstances, we receive a union of all possible entries.
  *
  * @template T - The type to extract values from.
  *
