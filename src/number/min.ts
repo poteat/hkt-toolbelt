ae3f3b31aac7d73bd74f70855206b5600086fe66
o <poteat@users.noreply.github.com>
2023-08-20 00:21:49 -0700

    aider: Added JSDoc comments to `src/number/min.ts` and followed the style guide.
    # Aider chat conversation:
    
    USER: Can we add similar JSDoc for `src/number/min.ts` ? As well, be sure to follow the style guide `style-guide/jsdoc.md`
    
    ASSISTANT: Sure, we can add JSDoc comments to the `src/number/min.ts` file. We will follow the style guide `style-guide/jsdoc.md` for the JSDoc comments.
    
    Here are the changes we need to make:
    
    1. Add JSDoc comments to the `_$min` type and `Min` interface in `src/number/min.ts`.
    
    Here are the specific changes:
    
    <source>typescript
    src/number/min.ts
    <<<<<<< HEAD
    export type _$min<
      A extends Number.Number,
      B extends Number.Number
    > = Number._$compare<A, B> extends -1 ? A : B
    
    interface Min_T<N extends Number.Number> extends Kind.Kind {
      f(x: Type._$cast<this[Kind._], Number.Number>): _$min<typeof x, N>
    }
    
    export interface Min extends Kind.Kind {
      f(x: Type._$cast<this[Kind._], Number.Number>): Min_T<typeof x>
    }
    =======
    /**
     * `_$min` is a type-level function that returns the minimum of two numbers.
     *
     * @template A - The first number.
     * @template B - The second number.
     *
     * @example
     * type T0 = _$min<1, 2> // 1
     * type T1 = _$min<5, 10> // 5
     */
    export type _$min<
      A extends Number.Number,
      B extends Number.Number
    > = Number._$compare<A, B> extends -1 ? A : B
    
    interface Min_T<N extends Number.Number> extends Kind.Kind {
      f(x: Type._$cast<this[Kind._], Number.Number>): _$min<typeof x, N>
    }
    
    /**
     * `Number.Min` is a type-level function that returns the minimum of two numbers.
     *
     * @template A - The first number.
     * @template B - The second number.
     *
     * @example
     * type T0 = $<$<Number.Min, 1>, 2> // 1
     * type T1 = $<$<Number.Min, 5>, 10> // 5
     */
    export interface Min extends Kind.Kind {
      f(x: Type._$cast<this[Kind._], Number.Number>): Min_T<typeof x>
    }
    >>>>>>> updated
    </source>

--- src/number/min.ts
+++ src/number/min.ts
@@ -1,5 +1,15 @@
 import { Kind, Type, Number } from '..'
 
+/**
+ * `_$min` is a type-level function that returns the minimum of two numbers.
+ *
+ * @template A - The first number.
+ * @template B - The second number.
+ *
+ * @example
+ * type T0 = _$min<1, 2> // 1
+ * type T1 = _$min<5, 10> // 5
+ */
 export type _$min<
   A extends Number.Number,
   B extends Number.Number
@@ -9,6 +19,16 @@ interface Min_T<N extends Number.Number> extends Kind.Kind {
   f(x: Type._$cast<this[Kind._], Number.Number>): _$min<typeof x, N>
 }
 
+/**
+ * `Number.Min` is a type-level function that returns the minimum of two numbers.
+ *
+ * @template A - The first number.
+ * @template B - The second number.
+ *
+ * @example
+ * type T0 = $<$<Number.Min, 1>, 2> // 1
+ * type T1 = $<$<Number.Min, 5>, 10> // 5
+ */
 export interface Min extends Kind.Kind {
   f(x: Type._$cast<this[Kind._], Number.Number>): Min_T<typeof x>
 }
