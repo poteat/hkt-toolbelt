3a96d02dd2bb7b6573f14488775b5dc66b12a8fe
o <poteat@users.noreply.github.com>
2023-08-20 00:06:28 -0700

    aider: Added JSDoc comments to `src/number/from-string.ts`.
    # Aider chat conversation:
    
    USER: Can we add JSDoc to `src/number/from-string.ts` per the style guide `style-guide/jsdoc.md` ?
    
    ASSISTANT: Sure, we can add JSDoc comments to the `src/number/from-string.ts` file. Here are the changes:
    
    1. Add JSDoc to the `_$fromString` type utility.
    2. Add JSDoc to the `FromString` interface.
    
    Here are the proposed changes:
    
    <source>typescript
    src/number/from-string.ts
    <<<<<<< HEAD
    export type _$fromString<T extends string> = T extends `${infer T extends
      | number
      | bigint}`
      ? T
      : never
    
    export interface FromString extends Kind.Kind {
      f(x: Type._$cast<this[Kind._], string>): _$fromString<typeof x>
    }
    =======
    /**
     * `Number.FromString` is a type-level function that converts a string to a number.
     *
     * @template T - The string to convert to a number.
     *
     * @example
     * type T0 = Number._$fromString<'123'> // 123
     * type T1 = Number._$fromString<'abc'> // never
     */
    export type _$fromString<T extends string> = T extends `${infer T extends
      | number
      | bigint}`
      ? T
      : never
    
    /**
     * `Number.FromString` is a type-level function that converts a string to a number.
     *
     * @template T - The string to convert to a number.
     *
     * @example
     * type T0 = $<Number.FromString, '123'> // 123
     * type T1 = $<Number.FromString, 'abc'> // never
     */
    export interface FromString extends Kind.Kind {
      f(x: Type._$cast<this[Kind._], string>): _$fromString<typeof x>
    }
    >>>>>>> updated
    </source>

--- src/number/from-string.ts
+++ src/number/from-string.ts
@@ -1,11 +1,29 @@
 import { Kind, Type } from '..'
 
+/**
+ * `Number.FromString` is a type-level function that converts a string to a number.
+ * 
+ * @template T - The string to convert to a number.
+ * 
+ * @example
+ * type T0 = Number._$fromString<'123'> // 123
+ * type T1 = Number._$fromString<'abc'> // never
+ */
 export type _$fromString<T extends string> = T extends `${infer T extends
   | number
   | bigint}`
   ? T
   : never
 
+/**
+ * `Number.FromString` is a type-level function that converts a string to a number.
+ * 
+ * @template T - The string to convert to a number.
+ * 
+ * @example
+ * type T0 = $<Number.FromString, '123'> // 123
+ * type T1 = $<Number.FromString, 'abc'> // never
+ */
 export interface FromString extends Kind.Kind {
   f(x: Type._$cast<this[Kind._], string>): _$fromString<typeof x>
 }
