6f81fed61b17005759fb88a8647f4d73b4438904
o <poteat@users.noreply.github.com>
2023-08-20 00:36:50 -0700

    aider: Added JSDoc comments to `src/union/from-list.ts` for `_$fromList` type and `FromList` interface.
    # Aider chat conversation:
    
    USER: Can we write JSDoc for `src/union/from-list.ts` ? The spec files at `src/union/from-list.spec.ts` may be helpful for context.
    
    ASSISTANT: Sure, we can add JSDoc comments to the `src/union/from-list.ts` file. Here's how we can do it:
    
    1. We need to modify the `src/union/from-list.ts` file.
    
    2. We will add JSDoc comments to the `_$fromList` type and the `FromList` interface.
    
    Here are the changes:
    
    <source>typescript
    src/union/from-list.ts
    <<<<<<< HEAD
    export type _$fromList<
      T extends List.List,
      FIRST = $<List.First, T>,
      REST extends List.List = $<List.Shift, T>,
      RESULT = T extends [] ? never : FIRST | _$fromList<REST>
    > = RESULT
    
    export interface FromList extends Kind.Kind {
      f(x: Type._$cast<this[Kind._], List.List>): _$fromList<typeof x>
    }
    =======
    /**
     * `Union._$fromList` is a type-level function that converts a list to a union.
     *
     * @template T - The list to convert to a union.
     *
     * @example
     * type T0 = Union._$fromList<[1, 2, 3]> // 1 | 2 | 3
     * type T1 = Union._$fromList<[]> // never
     */
    export type _$fromList<
      T extends List.List,
      FIRST = $<List.First, T>,
      REST extends List.List = $<List.Shift, T>,
      RESULT = T extends [] ? never : FIRST | _$fromList<REST>
    > = RESULT
    
    /**
     * `Union.FromList` is a type-level function that converts a list to a union.
     *
     * @template T - The list to convert to a union.
     *
     * @example
     * type T0 = $<Union.FromList, [1, 2, 3]> // 1 | 2 | 3
     * type T1 = $<Union.FromList, []> // never
     */
    export interface FromList extends Kind.Kind {
      f(x: Type._$cast<this[Kind._], List.List>): _$fromList<typeof x>
    }
    >>>>>>> updated
    </source>
    
    These changes add JSDoc comments to the `_$fromList` type and the `FromList` interface, providing a description of what they do, the template parameters they accept, and examples of their usage.

--- src/union/from-list.ts
+++ src/union/from-list.ts
@@ -1,5 +1,14 @@
 import { $, Kind, Type, List } from '..'
 
+/**
+ * `Union._$fromList` is a type-level function that converts a list to a union.
+ * 
+ * @template T - The list to convert to a union.
+ * 
+ * @example
+ * type T0 = Union._$fromList<[1, 2, 3]> // 1 | 2 | 3
+ * type T1 = Union._$fromList<[]> // never
+ */
 export type _$fromList<
   T extends List.List,
   FIRST = $<List.First, T>,
@@ -7,6 +16,15 @@ export type _$fromList<
   RESULT = T extends [] ? never : FIRST | _$fromList<REST>
 > = RESULT
 
+/**
+ * `Union.FromList` is a type-level function that converts a list to a union.
+ * 
+ * @template T - The list to convert to a union.
+ * 
+ * @example
+ * type T0 = $<Union.FromList, [1, 2, 3]> // 1 | 2 | 3
+ * type T1 = $<Union.FromList, []> // never
+ */
 export interface FromList extends Kind.Kind {
   f(x: Type._$cast<this[Kind._], List.List>): _$fromList<typeof x>
 }
