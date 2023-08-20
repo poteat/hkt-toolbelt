0566307b8ce21220e19b985ae4af3c844916e6e4
o <poteat@users.noreply.github.com>
2023-08-20 00:52:55 -0700

    aider: Added JSDoc comments to `src/test/expect.ts`.
    # Aider chat conversation:
    
    USER: Can you add JSDoc to `src/test/expect.ts`, per the style guide `style-guide/jsdoc.md` ? The corresponding spec file may be helpful `src/test/expect.spec.ts`
    
    ASSISTANT: Sure, I can add JSDoc comments to the `src/test/expect.ts` file.
    
    Here are the changes:
    
    <source>typescript
    src/test/expect.ts
    <<<<<<< HEAD
    import { Conditional, Type } from '..'
    
    export abstract class _ {
      abstract readonly _: symbol
    }
    
    export type Expect<
      X extends Conditional._$equals<X, V> extends true ? V : V & _,
      V = true
    > = Type._$isNever<V> extends true
      ? X
      : Type._$isNever<X> extends true
      ? Expect<X, V>
      : X
    =======
    import { Conditional, Type } from '..'
    
    /**
     * Abstract class used for internal purposes.
     */
    export abstract class _ {
      abstract readonly _: symbol
    }
    
    /**
     * `Expect` is a type-level function that checks if a type `X` equals a type `V`.
     * If `X` equals `V`, it returns `V`. If `X` does not equal `V`, it returns `V & _`.
     * If `V` is never, it returns `X`. If `X` is never, it returns `Expect<X, V>`.
     *
     * @template X - The type to check.
     * @template V - The type to compare with. Default is `true`.
     *
     * @example
     * type T0 = Expect<true> // true
     * type T1 = Expect<false> // false & _
     * type T2 = Expect<number, true> // true & _
     * type T3 = Expect<never, true> // Expect<never, true>
     */
    export type Expect<
      X extends Conditional._$equals<X, V> extends true ? V : V & _,
      V = true
    > = Type._$isNever<V> extends true
      ? X
      : Type._$isNever<X> extends true
      ? Expect<X, V>
      : X
    >>>>>>> updated
    </source>
    
    Please review and apply these changes if they are correct.

--- src/test/expect.ts
+++ src/test/expect.ts
@@ -1,9 +1,26 @@
 import { Conditional, Type } from '..'
 
+/**
+ * Abstract class used for internal purposes.
+ */
 export abstract class _ {
   abstract readonly _: symbol
 }
 
+/**
+ * `Expect` is a type-level function that checks if a type `X` equals a type `V`.
+ * If `X` equals `V`, it returns `V`. If `X` does not equal `V`, it returns `V & _`.
+ * If `V` is never, it returns `X`. If `X` is never, it returns `Expect<X, V>`.
+ * 
+ * @template X - The type to check.
+ * @template V - The type to compare with. Default is `true`.
+ * 
+ * @example
+ * type T0 = Expect<true> // true
+ * type T1 = Expect<false> // false & _
+ * type T2 = Expect<number, true> // true & _
+ * type T3 = Expect<never, true> // Expect<never, true>
+ */
 export type Expect<
   X extends Conditional._$equals<X, V> extends true ? V : V & _,
   V = true
