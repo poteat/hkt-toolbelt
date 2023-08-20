00c8f3298d051711faaf75bfb9e8cf3dfc0e34cb
o <poteat@users.noreply.github.com>
2023-08-20 00:26:19 -0700

    aider: Added JSDoc comments to `src/number/to-string.ts`.
    # Aider chat conversation:
    
    USER: Can we add JSDoc to `src/number/to-string.ts` ?
    
    ASSISTANT: Sure, we can add JSDoc comments to the `src/number/to-string.ts` file.
    
    Here are the steps we need to follow:
    
    1. Add a JSDoc comment to the `_$toString` type. This comment should explain what the type does, its template parameter, and provide an example of its usage.
    2. Add a similar JSDoc comment to the `ToString` interface.
    
    Here are the proposed changes:
    
    <source>typescript
    src/number/to-string.ts
    <<<<<<< HEAD
    import { Kind, Type, Number } from '..'
    
    export type _$toString<N extends Number.Number> = `${N}`
    
    export interface ToString extends Kind.Kind {
      f(x: Type._$cast<this[Kind._], Number.Number>): _$toString<typeof x>
    }
    =======
    import { Kind, Type, Number } from '..'
    
    /**
     * `_$toString` is a type-level function that converts a number to a string.
     *
     * @template N - The number to convert to a string.
     *
     * @example
     * type T0 = _$toString<5> // '5'
     */
    export type _$toString<N extends Number.Number> = `${N}`
    
    /**
     * `ToString` is a type-level function that converts a number to a string.
     *
     * @template N - The number to convert to a string.
     *
     * @example
     * type T0 = $<ToString, 5> // '5'
     */
    export interface ToString extends Kind.Kind {
      f(x: Type._$cast<this[Kind._], Number.Number>): _$toString<typeof x>
    }
    >>>>>>> updated
    </source>

--- src/number/to-string.ts
+++ src/number/to-string.ts
@@ -1,7 +1,23 @@
 import { Kind, Type, Number } from '..'
 
+/**
+ * `_$toString` is a type-level function that converts a number to a string.
+ * 
+ * @template N - The number to convert to a string.
+ * 
+ * @example
+ * type T0 = _$toString<5> // '5'
+ */
 export type _$toString<N extends Number.Number> = `${N}`
 
+/**
+ * `ToString` is a type-level function that converts a number to a string.
+ * 
+ * @template N - The number to convert to a string.
+ * 
+ * @example
+ * type T0 = $<ToString, 5> // '5'
+ */
 export interface ToString extends Kind.Kind {
   f(x: Type._$cast<this[Kind._], Number.Number>): _$toString<typeof x>
 }
