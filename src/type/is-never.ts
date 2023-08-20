9fb6e2fe12f0583a1ebf96e37bb8bd7989483a4a
o <poteat@users.noreply.github.com>
2023-08-20 01:26:46 -0700

    aider: Added JSDoc comments to `src/type/is-never.ts` to provide documentation for the `_$isNever` type and the `IsNever` interface.
    # Aider chat conversation:
    
    USER: Can we write JSDoc for `src/type/is-never.ts` ? The tests present in `src/type/is-never.spec.ts` may provide useful context.
    
    ASSISTANT: Sure, we can add JSDoc comments to the `src/type/is-never.ts` file. Here is the proposed change:
    
    <source>typescript
    src/type/is-never.ts
    <<<<<<< HEAD
    import { Conditional, Kind } from '..'
    
    export type _$isNever<X> = Conditional._$equals<X, never>
    
    export interface IsNever extends Kind.Kind {
      f(x: this[Kind._]): _$isNever<typeof x>
    }
    =======
    import { Conditional, Kind } from '..'
    
    /**
     * `_$isNever` is a type-level function that checks if a type is `never`.
     *
     * @template X - The type to check.
     *
     * @example
     * type T0 = _$isNever<'1'> // false
     * type T1 = _$isNever<never> // true
     * type T2 = _$isNever<unknown> // false
     * type T3 = _$isNever<any> // false
     */
    export type _$isNever<X> = Conditional._$equals<X, never>
    
    /**
     * `IsNever` is a type-level function that checks if a type is `never`.
     *
     * @template X - The type to check.
     *
     * @example
     * type T0 = $<IsNever, '1'> // false
     * type T1 = $<IsNever, never> // true
     * type T2 = $<IsNever, unknown> // false
     * type T3 = $<IsNever, any> // false
     */
    export interface IsNever extends Kind.Kind {
      f(x: this[Kind._]): _$isNever<typeof x>
    }
    >>>>>>> updated
    </source>

--- src/type/is-never.ts
+++ src/type/is-never.ts
@@ -1,7 +1,29 @@
 import { Conditional, Kind } from '..'
 
+/**
+ * `_$isNever` is a type-level function that checks if a type is `never`.
+ * 
+ * @template X - The type to check.
+ * 
+ * @example
+ * type T0 = _$isNever<'1'> // false
+ * type T1 = _$isNever<never> // true
+ * type T2 = _$isNever<unknown> // false
+ * type T3 = _$isNever<any> // false
+ */
 export type _$isNever<X> = Conditional._$equals<X, never>
 
+/**
+ * `IsNever` is a type-level function that checks if a type is `never`.
+ * 
+ * @template X - The type to check.
+ * 
+ * @example
+ * type T0 = $<IsNever, '1'> // false
+ * type T1 = $<IsNever, never> // true
+ * type T2 = $<IsNever, unknown> // false
+ * type T3 = $<IsNever, any> // false
+ */
 export interface IsNever extends Kind.Kind {
   f(x: this[Kind._]): _$isNever<typeof x>
 }
