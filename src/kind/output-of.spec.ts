3f9bbfc197048824042225184f5ef07099b7ceb6
o <poteat@users.noreply.github.com>
2023-08-19 22:20:51 -0700

    aider: Added spec file for `src/kind/output-of.ts` with tests for `Kind.OutputOf` utility.
    # Aider chat conversation:
    
    USER: Can you add a spec file to `src/kind/output-of.ts` ?
    
    ASSISTANT: Sure, I can help with that. However, I need more information about what you want to test in the spec file. Could you please provide more details about the tests you want to include?
    
    USER: Similar to the ones in `src/kind/input-of.spec.ts` I suppose, but also some ones referencing simple string utilities like `src/string/append.spec.ts`
    
    ASSISTANT: Alright, I understand. We will create a new spec file for `src/kind/output-of.ts` and include tests similar to those in `src/kind/input-of.spec.ts` and `src/string/append.spec.ts`.
    
    Here is the new file:
    
    ```typescript
    src/kind/output-of.spec.ts
    <<<<<<< HEAD
    
    =======
    import { $, Kind, String, Test } from '..'
    
    type AppendFoo = $<String.Append, 'foo'>
    type AppendBar = $<String.Append, 'bar'>
    
    type OutputOf_Spec = [
      // Test for String.Append with 'foo'
      Test.Expect<$<Kind.OutputOf, AppendFoo>, string>,
    
      // Test for String.Append with 'bar'
      Test.Expect<$<Kind.OutputOf, AppendBar>, string>,
    
      // Test for String.Append with empty string
      Test.Expect<$<Kind.OutputOf, $<String.Append, ''>>, string>,
    ]
    >>>>>>> updated
    ```
    
    This new file `src/kind/output-of.spec.ts` includes tests for the `Kind.OutputOf` utility with different string append operations. The expected output for all these operations is `string` as the `String.Append` operation always returns a string.

---
+++ src/kind/output-of.spec.ts
@@ -0,0 +1,15 @@
+import { $, Kind, String, Test } from '..'
+
+type AppendFoo = $<String.Append, 'foo'>
+type AppendBar = $<String.Append, 'bar'>
+
+type OutputOf_Spec = [
+  // Test for String.Append with 'foo'
+  Test.Expect<$<Kind.OutputOf, AppendFoo>, string>,
+
+  // Test for String.Append with 'bar'
+  Test.Expect<$<Kind.OutputOf, AppendBar>, string>,
+
+  // Test for String.Append with empty string
+  Test.Expect<$<Kind.OutputOf, $<String.Append, ''>>, string>,
+]
