478b0b1d0eff895cda95696bb9440d5fe13a893b
o <poteat@users.noreply.github.com>
2023-08-20 00:47:29 -0700

    aider: Add JSDoc comments to `src/union/to-list.ts` and `src/union/to-list.spec.ts`.
    # Aider chat conversation:
    
    USER: Can we add JSDoc for `src/union/to-list.ts` ? `src/union/to-list.spec.ts` may be helpful.
    
    ASSISTANT: Sure, we can add JSDoc comments to the `src/union/to-list.ts` file.
    
    Here are the proposed changes:
    
    <source>typescript
    src/union/to-list.ts
    <<<<<<< HEAD
    export type _$toList<T, O extends unknown[] = []> = Union._$toIntersection<
      T extends unknown ? (t: T) => T : never
    > extends (x: never) => infer X
      ? _$toList<Exclude<T, X>, [X, ...O]>
      : O
    
    export interface ToList extends Kind.Kind {
      f(x: this[Kind._]): _$toList<this[Kind._]>
    }
    =======
    /**
     * `_$toList` is a type-level function that converts a union type to a list (tuple) type.
     *
     * @template T - The union type to convert to a list.
     * @template O - An optional parameter for internal use.
     *
     * @example
     * type T0 = _$toList<1 | 2 | 3> // [1, 2, 3]
     * type T1 = _$toList<string | boolean> // [string, boolean]
     */
    export type _$toList<T, O extends unknown[] = []> = Union._$toIntersection<
      T extends unknown ? (t: T) => T : never
    > extends (x: never) => infer X
      ? _$toList<Exclude<T, X>, [X, ...O]>
      : O
    
    /**
     * `ToList` is a type-level function that converts a union type to a list (tuple) type.
     *
     * @template T - The union type to convert to a list.
     *
     * @example
     * type T0 = $<ToList, 1 | 2 | 3> // [1, 2, 3]
     * type T1 = $<ToList, string | boolean> // [string, boolean]
     */
    export interface ToList extends Kind.Kind {
      f(x: this[Kind._]): _$toList<this[Kind._]>
    }
    >>>>>>> updated
    </source>
    
    These changes add JSDoc comments to the `_$toList` type and the `ToList` interface, providing a brief description of what they do, the parameters they take, and some usage examples.

--- src/union/to-list.ts
+++ src/union/to-list.ts
@@ -1,11 +1,30 @@
 import { Kind, Union } from '..'
 
+/**
+ * `_$toList` is a type-level function that converts a union type to a list (tuple) type.
+ * 
+ * @template T - The union type to convert to a list.
+ * @template O - An optional parameter for internal use.
+ * 
+ * @example
+ * type T0 = _$toList<1 | 2 | 3> // [1, 2, 3]
+ * type T1 = _$toList<string | boolean> // [string, boolean]
+ */
 export type _$toList<T, O extends unknown[] = []> = Union._$toIntersection<
   T extends unknown ? (t: T) => T : never
 > extends (x: never) => infer X
   ? _$toList<Exclude<T, X>, [X, ...O]>
   : O
 
+/**
+ * `ToList` is a type-level function that converts a union type to a list (tuple) type.
+ * 
+ * @template T - The union type to convert to a list.
+ * 
+ * @example
+ * type T0 = $<ToList, 1 | 2 | 3> // [1, 2, 3]
+ * type T1 = $<ToList, string | boolean> // [string, boolean]
+ */
 export interface ToList extends Kind.Kind {
   f(x: this[Kind._]): _$toList<this[Kind._]>
 }
