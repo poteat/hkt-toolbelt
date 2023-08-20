91fb3a607e533a7d49d83b0ba5ad2bff1079018a
o <poteat@users.noreply.github.com>
2023-08-20 00:23:19 -0700

    aider: Added JSDoc comments to `src/number/sign.ts`.
    # Aider chat conversation:
    
    USER: Can we add JSDoc to `src/number/sign.ts` ?
    
    ASSISTANT: Sure, we can add JSDoc comments to the `src/number/sign.ts` file. We will follow the style guide `style-guide/jsdoc.md` for the JSDoc comments.
    
    Here are the changes we need to make:
    
    1. Add JSDoc comments to the `_$sign` type and `Sign` interface in `src/number/sign.ts`.
    
    Here are the specific changes:
    
    <source>typescript
    src/number/sign.ts
    <<<<<<< HEAD
    export type _$sign<T extends Number.Number> = number extends T
      ? '+' | '-'
      : `${T}` extends `-${string}`
      ? '-'
      : '+'
    
    export interface Sign extends Kind.Kind {
      f(x: Type._$cast<this[Kind._], Number.Number>): _$sign<typeof x>
    }
    =======
    /**
     * `_$sign` is a type-level function that returns the sign of a number.
     *
     * @template T - The number to get the sign of.
     *
     * @example
     * type T0 = _$sign<5> // '+'
     * type T1 = _$sign<-5> // '-'
     */
    export type _$sign<T extends Number.Number> = number extends T
      ? '+' | '-'
      : `${T}` extends `-${string}`
      ? '-'
      : '+'
    
    /**
     * `Number.Sign` is a type-level function that returns the sign of a number.
     *
     * @template T - The number to get the sign of.
     *
     * @example
     * type T0 = $<Number.Sign, 5> // '+'
     * type T1 = $<Number.Sign, -5> // '-'
     */
    export interface Sign extends Kind.Kind {
      f(x: Type._$cast<this[Kind._], Number.Number>): _$sign<typeof x>
    }
    >>>>>>> updated
    </source>

--- src/number/sign.ts
+++ src/number/sign.ts
@@ -1,11 +1,29 @@
 import { Type, Kind, Number } from '..'
 
+/**
+ * `_$sign` is a type-level function that returns the sign of a number.
+ *
+ * @template T - The number to get the sign of.
+ *
+ * @example
+ * type T0 = _$sign<5> // '+'
+ * type T1 = _$sign<-5> // '-'
+ */
 export type _$sign<T extends Number.Number> = number extends T
   ? '+' | '-'
   : `${T}` extends `-${string}`
   ? '-'
   : '+'
 
+/**
+ * `Number.Sign` is a type-level function that returns the sign of a number.
+ *
+ * @template T - The number to get the sign of.
+ *
+ * @example
+ * type T0 = $<Number.Sign, 5> // '+'
+ * type T1 = $<Number.Sign, -5> // '-'
+ */
 export interface Sign extends Kind.Kind {
   f(x: Type._$cast<this[Kind._], Number.Number>): _$sign<typeof x>
 }
