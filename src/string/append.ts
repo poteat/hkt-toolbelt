3a69c88c0049494e6454918b525a926b50d92542
o <poteat@users.noreply.github.com>
2023-08-20 14:37:44 -0700

    aider: Added JSDoc comments to `src/string/append.ts` for `String._$append` and `String.Append`.
    # Aider chat conversation:
    
    USER: Can you write JSDoc for `src/string/append.ts` per `style-guide/jsdoc.md` ? The test file `src/string/append.spec.ts` might be useful for context.
    
    ASSISTANT: Sure, I can add JSDoc comments to the `src/string/append.ts` file.
    
    Here are the changes:
    
    <source>typescript
    src/string/append.ts
    <<<<<<< HEAD
    import { Type, Kind } from '..'
    
    export type _$append<Suffix extends string, S extends string> = `${S}${Suffix}`
    
    interface Append_T<Suffix extends string> extends Kind.Kind {
      f(x: Type._$cast<this[Kind._], string>): _$append<Suffix, typeof x>
    }
    
    export interface Append extends Kind.Kind {
      f(x: Type._$cast<this[Kind._], string>): Append_T<typeof x>
    }
    =======
    import { Type, Kind } from '..'
    
    /**
     * `String._$append` is a type-level function that appends a suffix to a string.
     *
     * @template Suffix - The string to append.
     * @template S - The original string.
     *
     * @example
     * type T0 = String._$append<'bar', 'foo'> // 'foobar'
     * type T1 = String._$append<'', 'foo'> // 'foo'
     */
    export type _$append<Suffix extends string, S extends string> = `${S}${Suffix}`
    
    interface Append_T<Suffix extends string> extends Kind.Kind {
      f(x: Type._$cast<this[Kind._], string>): _$append<Suffix, typeof x>
    }
    
    /**
     * `String.Append` is a type-level function that appends a suffix to a string.
     *
     * @template Suffix - The string to append.
     *
     * @example
     * type T0 = $<String.Append, 'bar'>, 'foo'> // 'foobar'
     * type T1 = $<String.Append, ''>, 'foo'> // 'foo'
     */
    export interface Append extends Kind.Kind {
      f(x: Type._$cast<this[Kind._], string>): Append_T<typeof x>
    }
    >>>>>>> updated
    </source>
    
    These changes add JSDoc comments to the `_$append` type and `Append` interface, providing a brief description of their purpose, the template parameters they accept, and some usage examples.

--- src/string/append.ts
+++ src/string/append.ts
@@ -1,11 +1,30 @@
 import { Type, Kind } from '..'
 
+/**
+ * `String._$append` is a type-level function that appends a suffix to a string.
+ * 
+ * @template Suffix - The string to append.
+ * @template S - The original string.
+ * 
+ * @example
+ * type T0 = String._$append<'bar', 'foo'> // 'foobar'
+ * type T1 = String._$append<'', 'foo'> // 'foo'
+ */
 export type _$append<Suffix extends string, S extends string> = `${S}${Suffix}`
 
 interface Append_T<Suffix extends string> extends Kind.Kind {
   f(x: Type._$cast<this[Kind._], string>): _$append<Suffix, typeof x>
 }
 
+/**
+ * `String.Append` is a type-level function that appends a suffix to a string.
+ * 
+ * @template Suffix - The string to append.
+ * 
+ * @example
+ * type T0 = $<String.Append, 'bar'>, 'foo'> // 'foobar'
+ * type T1 = $<String.Append, ''>, 'foo'> // 'foo'
+ */
 export interface Append extends Kind.Kind {
   f(x: Type._$cast<this[Kind._], string>): Append_T<typeof x>
 }
