5fc9f42c69be79dcd2fde65f6ad9294db9a451c3
o <poteat@users.noreply.github.com>
2023-08-20 00:14:42 -0700

    aider: Add JSDoc to `src/number/max.ts` for the `Max` interface.
    # Aider chat conversation:
    
    USER: Can we add JSDoc to `src/number/max.ts` ?
    
    ASSISTANT: Sure, we can add JSDoc comments to the `src/number/max.ts` file.
    
    Here are the changes we need to make:
    
    1. Add JSDoc to the `_$max` type utility.
    2. Add JSDoc to the `Max_T` interface.
    3. Add JSDoc to the `Max` interface.
    
    Here are the proposed changes:
    
    <source>typescript
    src/number/max.ts
    <<<<<<< HEAD
    export type _$max<
      A extends Number.Number,
      B extends Number.Number
    > = Number._$compare<A, B> extends 1 ? A : B
    
    interface Max_T<N extends Number.Number> extends Kind.Kind {
      f(x: Type._$cast<this[Kind._], Number.Number>): _$max<typeof x, N>
    }
    
    export interface Max extends Kind.Kind {
      f(x: Type._$cast<this[Kind._], Number.Number>): Max_T<typeof x>
    }
    =======
    /**
     * `Number._$max` is a type-level function that returns the maximum of two numbers.
     *
     * @template A - The first number.
     * @template B - The second number.
     */
    export type _$
    ^C KeyboardInterrupt
    
    USER: Can we add JSDoc to `src/number/max.ts` ? You don't need to generate JSDoc for unexported utilities.
    
    ASSISTANT: Sure, we can add JSDoc comments to the `src/number/max.ts` file.
    
    Here are the changes we need to make:
    
    1. Add JSDoc to the `Max` interface.
    
    Here are the proposed changes:
    
    <source>typescript
    src/number/max.ts
    <<<<<<< HEAD
    export interface Max extends Kind.Kind {
      f(x: Type._$cast<this[Kind._], Number.Number>): Max_T<typeof x>
    }
    =======
    /**
     * `Number.Max` is a type-level function that returns the maximum of two numbers.
     *
     * @template N - The number to compare with.
     *
     * @example
     * type T0 = $
    ^C KeyboardInterrupt
    
    USER: Can we add JSDoc to `src/number/max.ts` ? We only need to add JSDoc to exported elements.
    
    ASSISTANT: Sure, we can add JSDoc comments to the `src/number/max.ts` file.
    
    Here are the changes we need to make:
    
    1. Add JSDoc to the `Max` interface.
    
    Here are the proposed changes:
    
    <source>typescript
    src/number/max.ts
    <<<<<<< HEAD
    export interface Max extends Kind.Kind {
      f(x: Type._$cast<this[Kind._], Number.Number>): Max_T<typeof x>
    }
    =======
    /**
     * `Number.Max` is a type-level function that returns the maximum of two numbers.
     *
     * @example
     * type T0 = $<Number.Max, 1> // 1
     * type T1 = $<Number.Max, 2> // 2
     */
    export interface Max extends Kind.Kind {
      f(x: Type._$cast<this[Kind._], Number.Number>): Max_T<typeof x>
    }
    >>>>>>> updated
    </source>

--- src/number/max.ts
+++ src/number/max.ts
@@ -9,6 +9,13 @@ interface Max_T<N extends Number.Number> extends Kind.Kind {
   f(x: Type._$cast<this[Kind._], Number.Number>): _$max<typeof x, N>
 }
 
+/**
+ * `Number.Max` is a type-level function that returns the maximum of two numbers.
+ *
+ * @example
+ * type T0 = $<Number.Max, 1> // 1
+ * type T1 = $<Number.Max, 2> // 2
+ */
 export interface Max extends Kind.Kind {
   f(x: Type._$cast<this[Kind._], Number.Number>): Max_T<typeof x>
 }
