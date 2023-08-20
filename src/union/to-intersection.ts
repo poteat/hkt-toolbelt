a1e1d5d998f7c942ca92b781306f3d03b1ff8ab9
o <poteat@users.noreply.github.com>
2023-08-20 00:42:53 -0700

    aider: Add JSDoc comments to `src/union/to-intersection.ts` per `style-guide/jsdoc.md`.
    # Aider chat conversation:
    
    USER: Can we add JSDoc to `src/union/to-intersection.ts` per `style-guide/jsdoc.md` ? `src/union/to-intersection.spec.ts` may be useful.
    
    ASSISTANT: Sure, we can add JSDoc comments to the `src/union/to-intersection.ts` file.
    
    The files we need to modify are:
    1. `src/union/to-intersection.ts`
    
    Here are the changes we need to make:
    
    1. Add JSDoc comments to the `_$toIntersection2` and `_$toIntersection` type utilities and the `ToIntersection` interface in `src/union/to-intersection.ts`. The comments will explain what these utilities do, their parameters, and provide examples of their usage.
    
    Here are the proposed changes:
    
    <source>typescript
    src/union/to-intersection.ts
    <<<<<<< HEAD
    import { Kind } from '..'
    
    type _$toIntersection2<T> = (
      T extends unknown ? (x: T) => unknown : never
    ) extends (x: infer X) => void
      ? X
      : never
    
    export type _$toIntersection<T> = boolean extends T
      ? boolean & _$toIntersection2<Exclude<T, boolean>>
      : _$toIntersection2<T>
    
    export interface ToIntersection extends Kind.Kind {
      f(x: this[Kind._]): _$toIntersection<this[Kind._]>
    }
    =======
    import { Kind } from '..'
    
    /**
     * `_$toIntersection2` is a type-level function that converts a type to an intersection type.
     *
     * @template T - The type to convert to an intersection type.
     *
     * @example
     * type T0 = _$toIntersection2<{ a: 'foo' } | { b: 'foo' }> // { a: 'foo'; b: 'foo' }
     */
    type _$toIntersection2<T> = (
      T extends unknown ? (x: T) => unknown : never
    ) extends (x: infer X) => void
      ? X
      : never
    
    /**
     * `_$toIntersection` is a type-level function that converts a type to an intersection type.
     * It handles boolean types separately.
     *
     * @template T - The type to convert to an intersection type.
     *
     * @example
     * type T0 = _$toIntersection<{ a: 'foo' } | { b: 'foo' }> // { a: 'foo'; b: 'foo' }
     * type T1 = _$toIntersection<number | string> // never
     */
    export type _$toIntersection<T> = boolean extends T
      ? boolean & _$toIntersection2<Exclude<T, boolean>>
      : _$toIntersection2<T>
    
    /**
     * `ToIntersection` is a type-level function that converts a type to an intersection type.
     *
     * @template T - The type to convert to an intersection type.
     *
     * @example
     * type T0 = $<ToIntersection, { a: 'foo' } | { b: 'foo' }> // { a: 'foo'; b: 'foo' }
     */
    export interface ToIntersection extends Kind.Kind {
      f(x: this[Kind._]): _$toIntersection<this[Kind._]>
    }
    >>>>>>> updated
    </source>

--- src/union/to-intersection.ts
+++ src/union/to-intersection.ts
@@ -1,15 +1,41 @@
 import { Kind } from '..'
 
+/**
+ * `_$toIntersection2` is a type-level function that converts a type to an intersection type.
+ * 
+ * @template T - The type to convert to an intersection type.
+ * 
+ * @example
+ * type T0 = _$toIntersection2<{ a: 'foo' } | { b: 'foo' }> // { a: 'foo'; b: 'foo' }
+ */
 type _$toIntersection2<T> = (
   T extends unknown ? (x: T) => unknown : never
 ) extends (x: infer X) => void
   ? X
   : never
 
+/**
+ * `_$toIntersection` is a type-level function that converts a type to an intersection type.
+ * It handles boolean types separately.
+ * 
+ * @template T - The type to convert to an intersection type.
+ * 
+ * @example
+ * type T0 = _$toIntersection<{ a: 'foo' } | { b: 'foo' }> // { a: 'foo'; b: 'foo' }
+ * type T1 = _$toIntersection<number | string> // never
+ */
 export type _$toIntersection<T> = boolean extends T
   ? boolean & _$toIntersection2<Exclude<T, boolean>>
   : _$toIntersection2<T>
 
+/**
+ * `ToIntersection` is a type-level function that converts a type to an intersection type.
+ * 
+ * @template T - The type to convert to an intersection type.
+ * 
+ * @example
+ * type T0 = $<ToIntersection, { a: 'foo' } | { b: 'foo' }> // { a: 'foo'; b: 'foo' }
+ */
 export interface ToIntersection extends Kind.Kind {
   f(x: this[Kind._]): _$toIntersection<this[Kind._]>
 }
