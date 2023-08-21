ba5272ccbae60d0ed7a5c9f787f5a327dc643876
o <poteat@users.noreply.github.com>
2023-08-20 18:49:23 -0700

    aider: Added JSDoc comments to `src/string/replace.ts` per style-guide/jsdoc.md.
    # Aider chat conversation:
    
    USER: "Can we write JSDoc for src/string/replace.ts per style-guide/jsdoc.md? The spec file src/string/replace.spec.ts may be useful."
    
    ASSISTANT: Sure, we can add JSDoc comments to the `src/string/replace.ts` file. We will follow the guidelines provided in the `style-guide/jsdoc.md` file. The `src/string/replace.spec.ts` file will be useful for providing examples in the JSDoc comments.
    
    Here are the steps we will follow:
    
    1. Add JSDoc comments to the `_$replace2` and `_$replace` type utilities.
    2. Add JSDoc comments to the `Replace_T2`, `Replace_T`, and `Replace` interfaces.
    
    Here are the proposed changes:
    
    <source>typescript
    src/string/replace.ts
    <<<<<<< HEAD
    type _$replace2<
      S extends string,
      From extends string,
      To extends string,
      O extends string = ''
    > = S extends `${infer Head}${From}${infer Tail}`
      ? _$replace2<Tail, From, To, `${O}${Head}${To}`>
      : `${O}${S}`
    
    export type _$replace<
      S extends string,
      From extends string,
      To extends string
    > = String._$isTemplate<From> extends true
      ? string
      : string extends From
      ? string
      : From extends ''
      ? `${To}${_$replace2<S, From, To>}`
      : _$replace2<S, From, To>
    
    interface Replace_T2<From extends string, To extends string> extends Kind.Kind {
      f(x: Type._$cast<this[Kind._], string>): _$replace<typeof x, From, To>
    }
    
    interface Replace_T<From extends string> extends Kind.Kind {
      f(x: Type._$cast<this[Kind._], string>): Replace_T2<From, typeof x>
    }
    
    export interface Replace extends Kind.Kind {
      f(x: Type._$cast<this[Kind._], string>): Replace_T<typeof x>
    }
    =======
    /**
     * `_$replace2` is a helper type utility for `_$replace`.
     *
     * @template S - The string to replace in.
     * @template From - The string to replace.
     * @template To - The string to replace with.
     * @template O - The output string.
     */
    type _$replace2<
      S extends string,
      From extends string,
      To extends string,
      O extends string = ''
    > = S extends `${infer Head}${From}${infer Tail}`
      ? _$replace2<Tail, From, To, `${O}${Head}${To}`>
      : `${O}${S}`
    
    /**
     * `String.Replace` is a type-level function that replaces all instances of a string with another string.
     *
     * @template S - The string to replace in.
     * @template From - The string to replace.
     * @template To - The string to replace with.
     *
     * @example
     * type T0 = String._$replace<'foobar', 'foo', 'bar'> // 'barbar'
     * type T1 = String._$replace<'foo', 'foo', ''> // ''
     */
    export type _$replace<
      S extends string,
      From extends string,
      To extends string
    > = String._$isTemplate<From> extends true
      ? string
      : string extends From
      ? string
      : From extends ''
      ? `${To}${_$replace2<S, From, To>}`
      : _$replace2<S, From, To>
    
    /**
     * `Replace_T2` is an intermediate interface for currying. It is not exported, so it doesn't have a JSDoc comment.
     */
    interface Replace_T2<From extends string, To extends string> extends Kind.Kind {
      f(x: Type._$cast<this[Kind._], string>): _$replace<typeof x, From, To>
    }
    
    /**
     * `Replace_T` is an intermediate interface for currying. It is not exported, so it doesn't have a JSDoc comment.
     */
    interface Replace_T<From extends string> extends Kind.Kind {
      f(x: Type._$cast<this[Kind._], string>): Replace_T2<From, typeof x>
    }
    
    /**
     * `String.Replace` is a type-level function that replaces all instances of a string with another string.
     *
     * @template From - The string to replace.
     * @template To - The string to replace with.
     *
     * @example
     * type T0 = $<$<$<String.Replace, 'foo'>, 'bar'>, 'foobar'> // 'barbar'
     * type T1 = $<$<$<String.Replace, 'foo'>, ''>, 'foo'> // ''
     */
    export interface Replace extends Kind.Kind {
      f(x: Type._$cast<this[Kind._], string>): Replace_T<typeof x>
    }
    >>>>>>> updated
    </source>

--- src/string/replace.ts
+++ src/string/replace.ts
@@ -1,5 +1,13 @@
 import { Type, Kind, String } from '..'
 
+/**
+ * `_$replace2` is a helper type utility for `_$replace`.
+ * 
+ * @template S - The string to replace in.
+ * @template From - The string to replace.
+ * @template To - The string to replace with.
+ * @template O - The output string.
+ */
 type _$replace2<
   S extends string,
   From extends string,
@@ -9,6 +17,17 @@ type _$replace2<
   ? _$replace2<Tail, From, To, `${O}${Head}${To}`>
   : `${O}${S}`
 
+/**
+ * `String.Replace` is a type-level function that replaces all instances of a string with another string.
+ * 
+ * @template S - The string to replace in.
+ * @template From - The string to replace.
+ * @template To - The string to replace with.
+ * 
+ * @example
+ * type T0 = String._$replace<'foobar', 'foo', 'bar'> // 'barbar'
+ * type T1 = String._$replace<'foo', 'foo', ''> // ''
+ */
 export type _$replace<
   S extends string,
   From extends string,
@@ -21,14 +40,30 @@ export type _$replace<
   ? `${To}${_$replace2<S, From, To>}`
   : _$replace2<S, From, To>
 
+/**
+ * `Replace_T2` is an intermediate interface for currying. It is not exported, so it doesn't have a JSDoc comment.
+ */
 interface Replace_T2<From extends string, To extends string> extends Kind.Kind {
   f(x: Type._$cast<this[Kind._], string>): _$replace<typeof x, From, To>
 }
 
+/**
+ * `Replace_T` is an intermediate interface for currying. It is not exported, so it doesn't have a JSDoc comment.
+ */
 interface Replace_T<From extends string> extends Kind.Kind {
   f(x: Type._$cast<this[Kind._], string>): Replace_T2<From, typeof x>
 }
 
+/**
+ * `String.Replace` is a type-level function that replaces all instances of a string with another string.
+ * 
+ * @template From - The string to replace.
+ * @template To - The string to replace with.
+ * 
+ * @example
+ * type T0 = $<$<$<String.Replace, 'foo'>, 'bar'>, 'foobar'> // 'barbar'
+ * type T1 = $<$<$<String.Replace, 'foo'>, ''>, 'foo'> // ''
+ */
 export interface Replace extends Kind.Kind {
   f(x: Type._$cast<this[Kind._], string>): Replace_T<typeof x>
 }
