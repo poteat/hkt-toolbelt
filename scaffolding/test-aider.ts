377dcf8005b98ab03c5cab1c6b600dfa4b1a8c41
o <poteat@users.noreply.github.com>
2023-08-20 18:14:29 -0700

    Refactor test-aider.ts to execute the "aider" command with additional arguments and log the stdout and stderr.

--- scaffolding/test-aider.ts
+++ scaffolding/test-aider.ts
@@ -1,10 +1,17 @@
-import { exec } from 'child_process';
+import { exec } from 'child_process'
 
-exec('aider', (error, stdout, stderr) => {
-  if (error) {
-    console.error(`exec error: ${error}`);
-    return;
+exec(
+  `aider\
+--msg="Can we write JSDoc for src/string/to-upper.ts per style-guide/jsdoc.md? The spec file src/string/to-upper.spec.ts may be useful."\
+src/string/to-upper.ts\
+src/string/to-upper.spec.ts\
+style-guide/jsdoc.md`,
+  (error, stdout, stderr) => {
+    if (error) {
+      console.error(`exec error: ${error}`)
+      return
+    }
+    console.log(`stdout: ${stdout}`)
+    console.error(`stderr: ${stderr}`)
   }
-  console.log(`stdout: ${stdout}`);
-  console.error(`stderr: ${stderr}`);
-});
+)
