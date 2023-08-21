c2710f120ea333dca8ff233f0da2dcd4dc25f424
o <poteat@users.noreply.github.com>
2023-08-20 19:01:09 -0700

    aider: Added JSDoc comments to `src/string/last.ts` per style-guide/jsdoc.md.
    # Aider chat conversation:
    
    USER: "Can we write JSDoc for src/string/last.ts per style-guide/jsdoc.md? The spec file src/string/last.spec.ts may be useful."
    
    ASSISTANT: Sure, we can add JSDoc comments to the `src/string/last.ts` file. We will follow the guidelines provided in `style-guide/jsdoc.md`. The `src/string/last.spec.ts` file will be useful for providing examples in the JSDoc comments.
    
    Here are the changes we need to make:
    
    1. Add JSDoc comments to the `_$last` type and the `Last` interface in `src/string/last.ts`.
    
    Here are the proposed changes:
    
    <source>typescript
    src/string/last.ts
    <<<<<<< HEAD
    import { Type, Kind } from '..'
    
    export type _$last<S extends string> = S extends `${string}${infer Tail}`
      ? Tail extends ''
        ? S
        : _$last<Tail>
      : string extends S
      ? S
      : ''
    
    export interface Last extends Kind.Kind {
      f(x: Type._$cast<this[Kind._], string>): _$last<typeof x>
    }
    =======
    import { Type, Kind } from '..'
    
    /**
     * `String._$last` is a type-level function that extracts the last character from a string.
     *
     * @template S - The string to extract the last character from.
     *
     * @example
     * type T0 = String._$last<'foo'> // 'o'
     * type T1 = String._$last<''> // ''
     */
    export type _$last<S extends string> = S extends `${string}${infer Tail}`
      ? Tail extends ''
        ? S
        : _$last<Tail>
      : string extends S
      ? S
      : ''
    
    /**
     * `String.Last` is a type-level function that extracts the last character from a string.
     *
     * @template S - The string to extract the last character from.
     *
     * @example
     * type T0 = $<String.Last, 'foo'> // 'o'
     * type T1 = $<String.Last, ''> // ''
     */
    export interface Last extends Kind.Kind {
      f(x: Type._$cast<this[Kind._], string>): _$last<typeof x>
    }
    >>>>>>> updated
    </source>

--- src/string/last.ts
+++ src/string/last.ts
@@ -1,5 +1,14 @@
 import { Type, Kind } from '..'
 
+/**
+ * `String._$last` is a type-level function that extracts the last character from a string.
+ * 
+ * @template S - The string to extract the last character from.
+ * 
+ * @example
+ * type T0 = String._$last<'foo'> // 'o'
+ * type T1 = String._$last<''> // ''
+ */
 export type _$last<S extends string> = S extends `${string}${infer Tail}`
   ? Tail extends ''
     ? S
@@ -8,6 +17,15 @@ export type _$last<S extends string> = S extends `${string}${infer Tail}`
   ? S
   : ''
 
+/**
+ * `String.Last` is a type-level function that extracts the last character from a string.
+ * 
+ * @template S - The string to extract the last character from.
+ * 
+ * @example
+ * type T0 = $<String.Last, 'foo'> // 'o'
+ * type T1 = $<String.Last, ''> // ''
+ */
 export interface Last extends Kind.Kind {
   f(x: Type._$cast<this[Kind._], string>): _$last<typeof x>
 }
