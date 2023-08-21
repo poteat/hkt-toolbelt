a7c8e7f82964986d123b92aa50940e5d09274fe8
o <poteat@users.noreply.github.com>
2023-08-21 14:15:23 -0700

    aider: Added JSDoc comments to `src/object/emplace.ts` per style-guide/jsdoc.md.
    # Aider chat conversation:
    
    USER: "Can we write JSDoc for src/object/emplace.ts per style-guide/jsdoc.md? The spec file src/object/emplace.spec.ts may be useful."
    
    ASSISTANT: Sure, we can add JSDoc comments to the `src/object/emplace.ts` file. Here are the changes:
    
    1. Add JSDoc to the `_$emplace` type.
    2. Add JSDoc to the `Emplace_T` interface.
    3. Add JSDoc to the `Emplace` interface.
    
    Here are the proposed changes:
    
    <source>typescript
    src/object/emplace.ts
    <<<<<<< HEAD
    export type _$emplace<K extends string | number | symbol, V> = {
      [k in K]: V
    }
    
    interface Emplace_T<K extends string | number | symbol> extends Kind.Kind {
      f(x: this[Kind._]): _$emplace<K, typeof x>
    }
    
    export interface Emplace extends Kind.Kind {
      f(x: Type._$cast<this[Kind._], string | number | symbol>): Emplace_T<typeof x>
    }
    =======
    /**
     * `Object._$emplace` is a type-level function that emplaces a value into a key.
     *
     * @template K - The key to emplace the value into.
     * @template V - The value to emplace.
     */
    export type _$emplace<K extends string | number | symbol, V> = {
      [k in K]: V
    }
    
    /**
     * `Object.Emplace_T` is an intermediate interface for currying.
     *
     * @template K - The key to emplace the value into.
     */
    interface Emplace_T<K extends string | number | symbol> extends Kind.Kind {
      f(x: this[Kind._]): _$emplace<K, typeof x>
    }
    
    /**
     * `Object.Emplace` is a type-level function that emplaces a value into a key.
     *
     * @template K - The key to emplace the value into.
     */
    export interface Emplace extends Kind.Kind {
      f(x: Type._$cast<this[Kind._], string | number | symbol>): Emplace_T<typeof x>
    }
    >>>>>>> updated
    </source>

--- src/object/emplace.ts
+++ src/object/emplace.ts
@@ -1,13 +1,29 @@
 import { Type, Kind } from '..'
 
+/**
+ * `Object._$emplace` is a type-level function that emplaces a value into a key.
+ * 
+ * @template K - The key to emplace the value into.
+ * @template V - The value to emplace.
+ */
 export type _$emplace<K extends string | number | symbol, V> = {
   [k in K]: V
 }
 
+/**
+ * `Object.Emplace_T` is an intermediate interface for currying.
+ * 
+ * @template K - The key to emplace the value into.
+ */
 interface Emplace_T<K extends string | number | symbol> extends Kind.Kind {
   f(x: this[Kind._]): _$emplace<K, typeof x>
 }
 
+/**
+ * `Object.Emplace` is a type-level function that emplaces a value into a key.
+ * 
+ * @template K - The key to emplace the value into.
+ */
 export interface Emplace extends Kind.Kind {
   f(x: Type._$cast<this[Kind._], string | number | symbol>): Emplace_T<typeof x>
 }
