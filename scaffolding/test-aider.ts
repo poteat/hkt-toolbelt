23b6c9a47a3f60a64e8d0b9e1d96b5922f75f34c
o <poteat@users.noreply.github.com>
2023-08-20 18:14:30 -0700

    aider: Added spaces between the command and its arguments in `test-aider.ts`.
    # Aider chat conversation:
    
    ASSISTANT: It seems like the command is not being parsed correctly due to missing spaces. The command should be split into multiple parts separated by spaces.
    
    Let's modify the `test-aider.ts` file to add spaces between the command and its arguments.
    
    Here is the *edit block* for the change:
    
    <source>typescript
    scaffolding/test-aider.ts
    <<<<<<< HEAD
    exec(
      `aider\
    --msg="Can we write JSDoc for src/string/to-upper.ts per style-guide/jsdoc.md? The spec file src/string/to-upper.spec.ts may be useful."\
    src/string/to-upper.ts\
    src/string/to-upper.spec.ts\
    style-guide/jsdoc.md`,
      (error, stdout, stderr) => {
        if (error) {
          console.error(`exec error: ${error}`)
          return
        }
        console.log(`stdout: ${stdout}`)
        console.error(`stderr: ${stderr}`)
      }
    )
    =======
    exec(
      `aider \
      --msg="Can we write JSDoc for src/string/to-upper.ts per style-guide/jsdoc.md? The spec file src/string/to-upper.spec.ts may be useful." \
      src/string/to-upper.ts \
      src/string/to-upper.spec.ts \
      style-guide/jsdoc.md`,
      (error, stdout, stderr) => {
        if (error) {
          console.error(`exec error: ${error}`)
          return
        }
        console.log(`stdout: ${stdout}`)
        console.error(`stderr: ${stderr}`)
      }
    )
    >>>>>>> updated
    </source>
    
    This should fix the issue. You can run the script again with `ts-node scaffolding/test-aider.ts` to see if 'aider' is available in the PATH for Node.js scripts.

--- scaffolding/test-aider.ts
+++ scaffolding/test-aider.ts
@@ -1,11 +1,11 @@
 import { exec } from 'child_process'
 
 exec(
-  `aider\
---msg="Can we write JSDoc for src/string/to-upper.ts per style-guide/jsdoc.md? The spec file src/string/to-upper.spec.ts may be useful."\
-src/string/to-upper.ts\
-src/string/to-upper.spec.ts\
-style-guide/jsdoc.md`,
+  `aider \
+  --msg="Can we write JSDoc for src/string/to-upper.ts per style-guide/jsdoc.md? The spec file src/string/to-upper.spec.ts may be useful." \
+  src/string/to-upper.ts \
+  src/string/to-upper.spec.ts \
+  style-guide/jsdoc.md`,
   (error, stdout, stderr) => {
     if (error) {
       console.error(`exec error: ${error}`)
