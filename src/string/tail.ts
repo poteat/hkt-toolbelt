1befa6d5ddcf18845113da80348c1bd1631944f2
o <poteat@users.noreply.github.com>
2023-08-20 18:30:50 -0700

    aider: Added JSDoc comments to `src/string/tail.ts` per style-guide/jsdoc.md.
    # Aider chat conversation:
    
    USER: "Can we write JSDoc for src/string/tail.ts per style-guide/jsdoc.md? The spec file src/string/tail.spec.ts may be useful."
    
    ASSISTANT: Sure, we can add JSDoc comments to the `src/string/tail.ts` file. We will follow the guidelines provided in `style-guide/jsdoc.md`. The `src/string/tail.spec.ts` file will be useful for providing examples in the JSDoc comments.
    
    Here are the changes we need to make:
    
    1. Add JSDoc comments to the `_$tail` type and `Tail` interface in `src/string/tail.ts`.
    
    Here are the proposed changes:
    
    <source>typescript
    src/string/tail.ts
    <<<<<<< HEAD
    import { Type, Kind } from '..'
    
    export type _$tail<S extends string> = S extends `${string}${infer Tail}`
      ? Tail extends ''
        ? S
        : Tail
      : string extends S
      ? S
      : ''
    
    export interface Tail extends Kind.Kind {
      f(x: Type._$cast<this[Kind._], string>): _$tail<typeof x>
    }
    =======
    import { Type, Kind } from '..'
    
    /**
     * `String._$tail` is a type-level function that extracts every element after the first element of a string.
     *
     * @template S - The string to extract the tail from.
     *
     * @example
     * type T0 = String._$tail<'hello'> // 'ello'
     * type T1 = String._$tail<''> // ''
     */
    export type _$tail<S extends string> = S extends `${string}${infer Tail}`
      ? Tail extends ''
        ? S
        : Tail
      : string extends S
      ? S
      : ''
    
    /**
     * `String.Tail` is a type-level function that extracts every element after the first element of a string.
     *
     * @example
     * type T0 = $<String.Tail, 'hello'> // 'ello'
     * type T1 = $<String.Tail, ''> // ''
     */
    export interface Tail extends Kind.Kind {
      f(x: Type._$cast<this[Kind._], string>): _$tail<typeof x>
    }
    >>>>>>> updated
    </source>

--- src/string/tail.ts
+++ src/string/tail.ts
@@ -1,5 +1,14 @@
 import { Type, Kind } from '..'
 
+/**
+ * `String._$tail` is a type-level function that extracts every element after the first element of a string.
+ * 
+ * @template S - The string to extract the tail from.
+ * 
+ * @example
+ * type T0 = String._$tail<'hello'> // 'ello'
+ * type T1 = String._$tail<''> // ''
+ */
 export type _$tail<S extends string> = S extends `${string}${infer Tail}`
   ? Tail extends ''
     ? S
@@ -8,6 +17,13 @@ export type _$tail<S extends string> = S extends `${string}${infer Tail}`
   ? S
   : ''
 
+/**
+ * `String.Tail` is a type-level function that extracts every element after the first element of a string.
+ * 
+ * @example
+ * type T0 = $<String.Tail, 'hello'> // 'ello'
+ * type T1 = $<String.Tail, ''> // ''
+ */
 export interface Tail extends Kind.Kind {
   f(x: Type._$cast<this[Kind._], string>): _$tail<typeof x>
 }
