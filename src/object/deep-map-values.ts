fec228d9b1eb3dd7701d69e341ca56d6b54b59b3
o <poteat@users.noreply.github.com>
2023-08-21 16:50:17 -0700

    aider: Added JSDoc comments to `src/object/deep-map-values.ts` per style-guide/jsdoc.md.
    # Aider chat conversation:
    
    USER: "Can we write JSDoc for src/object/deep-map-values.ts per style-guide/jsdoc.md? The spec file src/object/deep-map-values.spec.ts may be useful."
    
    ASSISTANT: Sure, we can add JSDoc comments to the `src/object/deep-map-values.ts` file.
    
    Here are the steps we need to follow:
    
    1. Add JSDoc comments to the `_$deepMapValues` type.
    2. Add JSDoc comments to the `DeepMapValues_T` interface.
    3. Add JSDoc comments to the `DeepMapValues` interface.
    
    Here are the proposed changes:
    
    <source>typescript
    src/object/deep-map-values.ts
    <<<<<<< HEAD
    export type _$deepMapValues<F extends Kind.Kind, O> = {
      [key in keyof O]: Type._$display<
        O[key] extends Record<string, unknown>
          ? _$deepMapValues<F, O[key]>
          : $<F, Type._$cast<O[key], Kind._$inputOf<F>>>
      >
    }
    
    interface DeepMapValues_T<T extends Kind.Kind> extends Kind.Kind {
      f(
        x: Type._$cast<this[Kind._], Object._$deepInputOf<T>>
      ): _$deepMapValues<T, typeof x>
    }
    
    export interface DeepMapValues extends Kind.Kind {
      f(x: Type._$cast<this[Kind._], Kind.Kind>): DeepMapValues_T<typeof x>
    }
    =======
    /**
     * `Object._$deepMapValues` is a type-level function that deeply maps over the values in an object.
     *
     * @template F - The function to apply to each value.
     * @template O - The object to map over.
     */
    export type _$deepMapValues<F extends Kind.Kind, O> = {
      [key in keyof O]: Type._$display<
        O[key] extends Record<string, unknown>
          ? _$deepMapValues<F, O[key]>
          : $<F, Type._$cast<O[key], Kind._$inputOf<F>>>
      >
    }
    
    /**
     * `Object.DeepMapValues_T` is an intermediate interface for currying.
     *
     * @template T - The function to apply to each value.
     */
    interface DeepMapValues_T<T extends Kind.Kind> extends Kind.Kind {
      f(
        x: Type._$cast<this[Kind._], Object._$deepInputOf<T>>
      ): _$deepMapValues<T, typeof x>
    }
    
    /**
     * `Object.DeepMapValues` is a type-level function that deeply maps over the values in an object.
     *
     * @template T - The function to apply to each value.
     */
    export interface DeepMapValues extends Kind.Kind {
      f(x: Type._$cast<this[Kind._], Kind.Kind>): DeepMapValues_T<typeof x>
    }
    >>>>>>> updated
    </source>
    
    Please review and apply these changes if they are correct.

--- src/object/deep-map-values.ts
+++ src/object/deep-map-values.ts
@@ -1,5 +1,11 @@
 import { $, Kind, Type, Object } from '..'
 
+/**
+ * `Object._$deepMapValues` is a type-level function that deeply maps over the values in an object.
+ * 
+ * @template F - The function to apply to each value.
+ * @template O - The object to map over.
+ */
 export type _$deepMapValues<F extends Kind.Kind, O> = {
   [key in keyof O]: Type._$display<
     O[key] extends Record<string, unknown>
@@ -8,12 +14,22 @@ export type _$deepMapValues<F extends Kind.Kind, O> = {
   >
 }
 
+/**
+ * `Object.DeepMapValues_T` is an intermediate interface for currying.
+ * 
+ * @template T - The function to apply to each value.
+ */
 interface DeepMapValues_T<T extends Kind.Kind> extends Kind.Kind {
   f(
     x: Type._$cast<this[Kind._], Object._$deepInputOf<T>>
   ): _$deepMapValues<T, typeof x>
 }
 
+/**
+ * `Object.DeepMapValues` is a type-level function that deeply maps over the values in an object.
+ * 
+ * @template T - The function to apply to each value.
+ */
 export interface DeepMapValues extends Kind.Kind {
   f(x: Type._$cast<this[Kind._], Kind.Kind>): DeepMapValues_T<typeof x>
 }
