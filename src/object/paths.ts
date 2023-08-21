eb26e0447af2d19cb5b4ed9720e1bd7d2f892abb
o <poteat@users.noreply.github.com>
2023-08-21 14:04:01 -0700

    aider: Added JSDoc comments to `src/object/paths.ts` per style-guide/jsdoc.md.
    # Aider chat conversation:
    
    USER: "Can we write JSDoc for src/object/paths.ts per style-guide/jsdoc.md? The spec file src/object/paths.spec.ts may be useful."
    
    ASSISTANT: Sure, we can add JSDoc comments to the `src/object/paths.ts` file. We will follow the guidelines provided in the `style-guide/jsdoc.md` file. The `src/object/paths.spec.ts` file will be useful for providing examples in the JSDoc comments.
    
    Here are the changes we need to make:
    
    1. Add JSDoc comments to the `_$paths2` type.
    2. Add JSDoc comments to the `_$paths` type.
    3. Add JSDoc comments to the `Paths` interface.
    
    Here are the proposed changes:
    
    <source>typescript
    src/object/paths.ts
    <<<<<<< HEAD
    type _$paths2<T, O extends unknown[] = []> =
      | {
          [K in keyof T]: T[K] extends Record<string, unknown>
            ? _$paths2<T[K], [...O, K]>
            : [...O, K]
        }[keyof T]
      | O
    =======
    /**
     * `_$paths2` is a type-level function that returns the paths to all values in an object as a tuple.
     *
     * @template T - The object to get the paths of.
     * @template O - The current path.
     */
    type _$paths2<T, O extends unknown[] = []> =
      | {
          [K in keyof T]: T[K] extends Record<string, unknown>
            ? _$paths2<T[K], [...O, K]>
            : [...O, K]
        }[keyof T]
      | O
    >>>>>>> updated
    </source>
    
    <source>typescript
    src/object/paths.ts
    <<<<<<< HEAD
    export type _$paths<T, U = _$paths2<T>> = Union._$toList<
      U extends [] ? never : U
    >
    =======
    /**
     * `_$paths` is a type-level function that returns the paths to all values in an object as a tuple.
     *
     * @template T - The object to get the paths of.
     * @template U - The paths of the object.
     */
    export type _$paths<T, U = _$paths2<T>> = Union._$toList<
      U extends [] ? never : U
    >
    >>>>>>> updated
    </source>
    
    <source>typescript
    src/object/paths.ts
    <<<<<<< HEAD
    export interface Paths extends Kind.Kind {
      f(x: Type._$cast<this[Kind._], Record<string, unknown>>): _$paths<typeof x>
    }
    =======
    /**
     * `Paths` is a type-level function that returns the paths to all values in an object as a tuple.
     *
     * @template x - The object to get the paths of.
     */
    export interface Paths extends Kind.Kind {
      f(x: Type._$cast<this[Kind._], Record<string, unknown>>): _$paths<typeof x>
    }
    >>>>>>> updated
    </source>

--- src/object/paths.ts
+++ src/object/paths.ts
@@ -1,5 +1,11 @@
 import { Kind, Type, Union } from '..'
 
+/**
+ * `_$paths2` is a type-level function that returns the paths to all values in an object as a tuple.
+ * 
+ * @template T - The object to get the paths of.
+ * @template O - The current path.
+ */
 type _$paths2<T, O extends unknown[] = []> =
   | {
       [K in keyof T]: T[K] extends Record<string, unknown>
@@ -8,10 +14,21 @@ type _$paths2<T, O extends unknown[] = []> =
     }[keyof T]
   | O
 
+/**
+ * `_$paths` is a type-level function that returns the paths to all values in an object as a tuple.
+ * 
+ * @template T - The object to get the paths of.
+ * @template U - The paths of the object.
+ */
 export type _$paths<T, U = _$paths2<T>> = Union._$toList<
   U extends [] ? never : U
 >
 
+/**
+ * `Paths` is a type-level function that returns the paths to all values in an object as a tuple.
+ * 
+ * @template x - The object to get the paths of.
+ */
 export interface Paths extends Kind.Kind {
   f(x: Type._$cast<this[Kind._], Record<string, unknown>>): _$paths<typeof x>
 }
