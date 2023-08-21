8d0c442a1e8b9984480106600dad4615ee6c5a9c
o <poteat@users.noreply.github.com>
2023-08-21 13:52:51 -0700

    aider: Added JSDoc comments to `src/object/values.ts` per style-guide/jsdoc.md.
    # Aider chat conversation:
    
    USER: "Can we write JSDoc for src/object/values.ts per style-guide/jsdoc.md? The spec file src/object/values.spec.ts may be useful."
    
    ASSISTANT: Sure, we can add JSDoc comments to the `src/object/values.ts` file. We will follow the guidelines provided in the `style-guide/jsdoc.md` file. The `src/object/values.spec.ts` file will be useful for providing examples in the JSDoc comments.
    
    Here are the steps we will follow:
    
    1. Add a JSDoc comment to the `_$values` type. This comment will explain what the type does, describe the template parameter `T`, and provide an example of usage. The example will be based on the tests in `src/object/values.spec.ts`.
    
    2. Add a JSDoc comment to the `Values` interface. This comment will explain what the interface does, describe the method `f`, and provide an example of usage. The example will be based on the tests in `src/object/values.spec.ts`.
    
    Here are the proposed changes:
    
    <source>typescript
    src/object/values.ts
    <<<<<<< HEAD
    import { Kind, Type, Object } from '..'
    
    export type _$values<
      T extends Record<string, unknown>,
      Keys = Object._$keys<T>
    > = {
      [key in keyof Keys]: T[Type._$cast<Keys[key], keyof T>]
    }
    
    export interface Values extends Kind.Kind {
      f(x: Type._$cast<this[Kind._], Record<string, unknown>>): _$values<typeof x>
    }
    =======
    import { Kind, Type, Object } from '..'
    
    /**
     * `Object._$values` is a type-level function that returns the values of an object as a tuple.
     *
     * @template T - The object to get the values of.
     *
     * @example
     * type T0 = Object._$values<{ a: 1; b: 2; c: 3 }> // [1, 2, 3]
     * type T1 = Object._$values<{}> // []
     */
    export type _$values<
      T extends Record<string, unknown>,
      Keys = Object._$keys<T>
    > = {
      [key in keyof Keys]: T[Type._$cast<Keys[key], keyof T>]
    }
    
    /**
     * `Object.Values` is a type-level function that returns the values of an object as a tuple.
     *
     * @template T - The object to get the values of.
     *
     * @example
     * type T0 = $<Object.Values, { a: 1; b: 2; c: 3 }>[number] // 1 | 2 | 3
     * type T1 = $<Object.Values, {}> // []
     */
    export interface Values extends Kind.Kind {
      f(x: Type._$cast<this[Kind._], Record<string, unknown>>): _$values<typeof x>
    }
    >>>>>>> updated
    </source>

--- src/object/values.ts
+++ src/object/values.ts
@@ -1,5 +1,14 @@
 import { Kind, Type, Object } from '..'
 
+/**
+ * `Object._$values` is a type-level function that returns the values of an object as a tuple.
+ * 
+ * @template T - The object to get the values of.
+ * 
+ * @example
+ * type T0 = Object._$values<{ a: 1; b: 2; c: 3 }> // [1, 2, 3]
+ * type T1 = Object._$values<{}> // []
+ */
 export type _$values<
   T extends Record<string, unknown>,
   Keys = Object._$keys<T>
@@ -7,6 +16,15 @@ export type _$values<
   [key in keyof Keys]: T[Type._$cast<Keys[key], keyof T>]
 }
 
+/**
+ * `Object.Values` is a type-level function that returns the values of an object as a tuple.
+ * 
+ * @template T - The object to get the values of.
+ * 
+ * @example
+ * type T0 = $<Object.Values, { a: 1; b: 2; c: 3 }>[number] // 1 | 2 | 3
+ * type T1 = $<Object.Values, {}> // []
+ */
 export interface Values extends Kind.Kind {
   f(x: Type._$cast<this[Kind._], Record<string, unknown>>): _$values<typeof x>
 }
