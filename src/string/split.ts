e3a5df24ccaa5a9c0797999fb31649ddc68aafc1
o <poteat@users.noreply.github.com>
2023-08-20 22:33:22 -0700

    Fix errors

--- src/string/split.ts
+++ src/string/split.ts
@@ -12,14 +12,17 @@ import { Type, Kind, String } from '..'
  */
 export type _$split<
   S extends string,
-  Delimiter extends string = ''
+  Delimiter extends string = '',
+  O extends unknown[] = []
 > = String._$isTemplate<Delimiter> extends true
   ? string[]
   : string extends Delimiter
   ? string[]
   : S extends `${infer Head}${Delimiter}${infer Tail}`
-  ? _$split<Tail, Delimiter>
-  : [S]
+  ? _$split<Tail, Delimiter, [...O, Head]>
+  : S extends Delimiter
+  ? O
+  : [...O, S]
 
 interface Split_T<Delimiter extends string> extends Kind.Kind {
   f(x: Type._$cast<this[Kind._], string>): _$split<typeof x, Delimiter>
@@ -35,7 +38,6 @@ interface Split_T<Delimiter extends string> extends Kind.Kind {
  * type T0 = $<$<String.Split, ''>, 'foobar'> // ['f', 'o', 'o', 'b', 'a', 'r']
  * type T1 = $<$<String.Split, ' '>, 'foo bar'> // ['foo', 'bar']
  */
-export interface Split<S extends string, Delimiter extends string = ''>
-  extends Kind.Kind {
-  f(x: Type._$cast<this[Kind._], string>): Split_T<Delimiter>
+export interface Split extends Kind.Kind {
+  f(x: Type._$cast<this[Kind._], string>): Split_T<typeof x>
 }
