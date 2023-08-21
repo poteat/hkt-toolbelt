004a12e8c3ada5ab1bc72299e2e2f2dccfdd3136
o <poteat@users.noreply.github.com>
2023-08-20 21:48:24 -0700

    Move JSDoc entry

--- src/string/init.ts
+++ src/string/init.ts
@@ -1,14 +1,5 @@
 import { Type, Kind } from '..'
 
-/**
- * `String._$init` is a type-level function that extracts every element before the last element of a string.
- * 
- * @template S - The string to extract the init from.
- * 
- * @example
- * type T0 = String._$init<'foo'> // 'fo'
- * type T1 = String._$init<''> // ''
- */
 type _$init2<
   S extends string,
   O extends string = ''
@@ -18,13 +9,22 @@ type _$init2<
     : _$init2<Tail, `${O}${Head}`>
   : O
 
+/**
+ * `String._$init` is a type-level function that extracts every element before the last element of a string.
+ *
+ * @template S - The string to extract the init from.
+ *
+ * @example
+ * type T0 = String._$init<'foo'> // 'fo'
+ * type T1 = String._$init<''> // ''
+ */
 export type _$init<S extends string> = string extends S ? string : _$init2<S>
 
 /**
  * `String.Init` is a type-level function that extracts every element before the last element of a string.
- * 
+ *
  * @template S - The string to extract the init from.
- * 
+ *
  * @example
  * type T0 = $<String.Init, 'foo'> // 'fo'
  * type T1 = $<String.Init, ''> // ''
