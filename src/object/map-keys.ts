08a6efc7d35f87b06884b2c9ea9a89defd653722
o <poteat@users.noreply.github.com>
2023-08-21 14:13:04 -0700

    aider: Added JSDoc comments to `src/object/map-keys.ts` per style-guide/jsdoc.md.
    # Aider chat conversation:
    
    USER: "Can we write JSDoc for src/object/map-keys.ts per style-guide/jsdoc.md? The spec file src/object/map-keys.spec.ts may be useful."
    
    ASSISTANT: Sure, we can add JSDoc comments to the `src/object/map-keys.ts` file. We will need to modify the following file:
    
    - `src/object/map-keys.ts`
    
    Here are the steps we will follow:
    
    1. Add a JSDoc comment to the `_$mapKeys` type. This comment will explain what the type does, its template parameters, and provide an example of its usage.
    2. Add a JSDoc comment to the `MapKeys_T` interface. This comment will explain what the interface does and its template parameters. As per the style guide, we do not need to provide an example for this interface since it is not exported.
    3. Add a JSDoc comment to the `MapKeys` interface. This comment will explain what the interface does, its template parameters, and provide an example of its usage.
    
    Here are the proposed changes:
    
    <source>typescript
    src/object/map-keys.ts
    <<<<<<< HEAD
    import { $, Kind, Type } from '..'
    
    export type _$mapKeys<
      T extends Record<string, unknown>,
      F extends Kind.Kind<(x: string) => string | number | symbol>
    > = {
      [key in keyof T as $<F, Type._$cast<key, Kind._$inputOf<F>>>]: T[key]
    }
    
    interface MapKeys_T<
      F extends Kind.Kind<(x: string) => string | number | symbol>
    > extends Kind.Kind {
      f(
        x: Type._$cast<this[Kind._], Record<string, unknown>>
      ): _$mapKeys<typeof x, F>
    }
    
    export interface MapKeys extends Kind.Kind {
      f(
        x: Type._$cast<
          this[Kind._],
          Kind.Kind<(x: string) => string | number | symbol>
        >
      ): MapKeys_T<typeof x>
    }
    =======
    import { $, Kind, Type } from '..'
    
    /**
     * `Object._$mapKeys` is a type-level function that maps the keys of an object.
     *
     * @template T - The object to map the keys of.
     * @template F - The function to apply to each key.
     *
     * @example
     * type T0 = Object._$mapKeys<{ a: 1; b: 2; c: 3 }, String.ToUpper> // { A: 1; B: 2; C: 3 }
     */
    export type _$mapKeys<
      T extends Record<string, unknown>,
      F extends Kind.Kind<(x: string) => string | number | symbol>
    > = {
      [key in keyof T as $<F, Type._$cast<key, Kind._$inputOf<F>>>]: T[key]
    }
    
    /**
     * `Object.MapKeys_T` is an intermediate interface for currying.
     *
     * @template F - The function to apply to each key.
     */
    interface MapKeys_T<
      F extends Kind.Kind<(x: string) => string | number | symbol>
    > extends Kind.Kind {
      f(
        x: Type._$cast<this[Kind._], Record<string, unknown>>
      ): _$mapKeys<typeof x, F>
    }
    
    /**
     * `Object.MapKeys` is a type-level function that maps the keys of an object.
     *
     * @template F - The function to apply to each key.
     *
     * @example
     * type T0 = $<$<Object.MapKeys, String.ToUpper>, { a: 1; b: 2; c: 3 }> // { A: 1; B: 2; C: 3 }
     */
    export interface MapKeys extends Kind.Kind {
      f(
        x: Type._$cast<
          this[Kind._],
          Kind.Kind<(x: string) => string | number | symbol>
        >
      ): MapKeys_T<typeof x>
    }
    >>>>>>> updated
    </source>

--- src/object/map-keys.ts
+++ src/object/map-keys.ts
@@ -1,5 +1,14 @@
 import { $, Kind, Type } from '..'
 
+/**
+ * `Object._$mapKeys` is a type-level function that maps the keys of an object.
+ * 
+ * @template T - The object to map the keys of.
+ * @template F - The function to apply to each key.
+ * 
+ * @example
+ * type T0 = Object._$mapKeys<{ a: 1; b: 2; c: 3 }, String.ToUpper> // { A: 1; B: 2; C: 3 }
+ */
 export type _$mapKeys<
   T extends Record<string, unknown>,
   F extends Kind.Kind<(x: string) => string | number | symbol>
@@ -7,6 +16,11 @@ export type _$mapKeys<
   [key in keyof T as $<F, Type._$cast<key, Kind._$inputOf<F>>>]: T[key]
 }
 
+/**
+ * `Object.MapKeys_T` is an intermediate interface for currying.
+ * 
+ * @template F - The function to apply to each key.
+ */
 interface MapKeys_T<
   F extends Kind.Kind<(x: string) => string | number | symbol>
 > extends Kind.Kind {
@@ -15,6 +29,14 @@ interface MapKeys_T<
   ): _$mapKeys<typeof x, F>
 }
 
+/**
+ * `Object.MapKeys` is a type-level function that maps the keys of an object.
+ * 
+ * @template F - The function to apply to each key.
+ * 
+ * @example
+ * type T0 = $<$<Object.MapKeys, String.ToUpper>, { a: 1; b: 2; c: 3 }> // { A: 1; B: 2; C: 3 }
+ */
 export interface MapKeys extends Kind.Kind {
   f(
     x: Type._$cast<
