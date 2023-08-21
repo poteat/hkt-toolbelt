b1b227a32a48f424a6828bc3a17c4783b32050b0
o <poteat@users.noreply.github.com>
2023-08-21 14:19:38 -0700

    aider: Updated JSDoc comments for `Object._$emplace` and `Object.Emplace` to include examples and clarify their behavior.
    # Aider chat conversation:
    
    ASSISTANT: Sure, we can definitely improve the JSDoc comments in `src/object/emplace.ts` by providing examples and clarifying the behavior of the function.
    
    Here are the steps we need to take:
    
    1. Modify the JSDoc comments for `_$emplace` to include examples and clarify its behavior.
    2. Modify the JSDoc comments for `Emplace` to include examples and clarify its behavior.
    
    Here are the proposed changes:
    
    <source>typescript
    src/object/emplace.ts
    <<<<<<< HEAD
    /**
     * `Object._$emplace` is a type-level function that emplaces a value into a key.
     *
     * @template K - The key to emplace the value into.
     * @template V - The value to emplace.
     */
    export type _$emplace<K extends string | number | symbol, V> = {
      [k in K]: V
    }
    =======
    /**
     * `Object._$emplace` is a type-level function that emplaces a value into a key.
     * It generates a new object with a single key-value pair, it doesn't modify or add to an existing object.
     *
     * @template K - The key to emplace the value into.
     * @template V - The value to emplace.
     *
     * @example
     * type T0 = Object._$emplace<'a', 1> // { a: 1 }
     * type T1 = Object._$emplace<'b', 'hello'> // { b: 'hello' }
     */
    export type _$emplace<K extends string | number | symbol, V> = {
      [k in K]: V
    }
    >>>>>>> updated
    </source>
    
    <source>typescript
    src/object/emplace.ts
    <<<<<<< HEAD
    /**
     * `Object.Emplace` is a type-level function that emplaces a value into a key.
     *
     * @template K - The key to emplace the value into.
     * @template V - The value to emplace.
     */
    export interface Emplace extends Kind.Kind {
      f(x: Type._$cast<this[Kind._], string | number | symbol>): Emplace_T<typeof x>
    }
    =======
    /**
     * `Object.Emplace` is a type-level function that emplaces a value into a key.
     * It generates a new object with a single key-value pair, it doesn't modify or add to an existing object.
     *
     * @template K - The key to emplace the value into.
     * @template V - The value to emplace.
     *
     * @example
     * type T0 = $<$<Object.Emplace, 'a'>, 1> // { a: 1 }
     * type T1 = $<$<Object.Emplace, 'b'>, 'hello'> // { b: 'hello' }
     */
    export interface Emplace extends Kind.Kind {
      f(x: Type._$cast<this[Kind._], string | number | symbol>): Emplace_T<typeof x>
    }
    >>>>>>> updated
    </source>

--- src/object/emplace.ts
+++ src/object/emplace.ts
@@ -2,9 +2,14 @@ import { Type, Kind } from '..'
 
 /**
  * `Object._$emplace` is a type-level function that emplaces a value into a key.
+ * It generates a new object with a single key-value pair, it doesn't modify or add to an existing object.
  *
  * @template K - The key to emplace the value into.
  * @template V - The value to emplace.
+ * 
+ * @example
+ * type T0 = Object._$emplace<'a', 1> // { a: 1 }
+ * type T1 = Object._$emplace<'b', 'hello'> // { b: 'hello' }
  */
 export type _$emplace<K extends string | number | symbol, V> = {
   [k in K]: V
@@ -21,9 +26,14 @@ interface Emplace_T<K extends string | number | symbol> extends Kind.Kind {
 
 /**
  * `Object.Emplace` is a type-level function that emplaces a value into a key.
+ * It generates a new object with a single key-value pair, it doesn't modify or add to an existing object.
  *
  * @template K - The key to emplace the value into.
  * @template V - The value to emplace.
+ * 
+ * @example
+ * type T0 = $<$<Object.Emplace, 'a'>, 1> // { a: 1 }
+ * type T1 = $<$<Object.Emplace, 'b'>, 'hello'> // { b: 'hello' }
  */
 export interface Emplace extends Kind.Kind {
   f(x: Type._$cast<this[Kind._], string | number | symbol>): Emplace_T<typeof x>
