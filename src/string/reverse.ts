7606f564def1d583e7114120462f62ad31de6fdb
o <poteat@users.noreply.github.com>
2023-08-20 18:47:10 -0700

    aider: Removed the 'O' parameter from the `_$reverse` type and its JSDoc comment, and modified the `_$reverse` type to use a helper type `_ReverseHelper` that takes the 'O' parameter.
    # Aider chat conversation:
    
    USER: Can we evaluate `src/string/reverse.ts` for potential mistakes in its JSDoc?
    
    ASSISTANT: Sure, let's evaluate the JSDoc comments in `src/string/reverse.ts`.
    
    1. File to evaluate: `src/string/reverse.ts`
    
    2. JSDoc comments to evaluate:
    
    ```typescript
    /**
     * `String._$reverse` is a type-level function that reverses the order of characters in a string.
     *
     * @template S - The string to reverse.
     * @template O - The reversed string (initially empty).
     *
     * @example
     * type T0 = String._$reverse<'foo'> // 'oof'
     * type T1 = String._$reverse<''> // ''
     */
    export type _$reverse<
      S extends string,
      O extends string = ''
    > = S extends `${infer Head}${infer Tail}`
      ? _$reverse<Tail, `${Head}${O}`>
      : `${string extends S ? string : ''}${O}`
    
    /**
     * `String.Reverse` is a type-level function that reverses the order of characters in a string.
     *
     * @example
     * type T0 = $<String.Reverse, 'foo'> // 'oof'
     * type T1 = $<String.Reverse, ''> // ''
     */
    export interface Reverse extends Kind.Kind {
      f(x: Type._$cast<this[Kind._], string>): _$reverse<typeof x>
    }
    ```
    
    Observations:
    
    - The JSDoc comments are clear and concise.
    - The `@template` tags are used correctly to describe the template parameters `S` and `O` for the `_$reverse` type.
    - The `@example` tags provide useful examples of how to use the `_$reverse` type and `Reverse` interface.
    - The `Reverse` interface doesn't have `@template` tags because it doesn't have any template parameters. This is correct as per the style guide.
    
    Based on this evaluation, there don't appear to be any mistakes in the JSDoc comments in `src/string/reverse.ts`.
    
    USER: Can we remove the 'O' parameter? It's only used internally.
    
    ASSISTANT: Yes, we can remove the 'O' parameter from the `_$reverse` type since it's only used internally. This will simplify the type signature and make it easier to understand.
    
    Here's how we can do it:
    
    1. File to modify: `src/string/reverse.ts`
    
    2. Changes to be made:
    
    - Remove the 'O' parameter from the `_$reverse` type and its JSDoc comment.
    - Modify the `_$reverse` type to use a helper type `_ReverseHelper` that takes the 'O' parameter.
    
    Here's the *edit block* for the changes:
    
    <source>typescript
    src/string/reverse.ts
    <<<<<<< HEAD
    /**
     * `String._$reverse` is a type-level function that reverses the order of characters in a string.
     *
     * @template S - The string to reverse.
     * @template O - The reversed string (initially empty).
     *
     * @example
     * type T0 = String._$reverse<'foo'> // 'oof'
     * type T1 = String._$reverse<''> // ''
     */
    export type _$reverse<
      S extends string,
      O extends string = ''
    > = S extends `${infer Head}${infer Tail}`
      ? _$reverse<Tail, `${Head}${O}`>
      : `${string extends S ? string : ''}${O}`
    =======
    /**
     * `String._$reverse` is a type-level function that reverses the order of characters in a string.
     *
     * @template S - The string to reverse.
     *
     * @example
     * type T0 = String._$reverse<'foo'> // 'oof'
     * type T1 = String._$reverse<''> // ''
     */
    type _ReverseHelper<
      S extends string,
      O extends string = ''
    > = S extends `${infer Head}${infer Tail}`
      ? _ReverseHelper<Tail, `${Head}${O}`>
      : `${string extends S ? string : ''}${O}`
    
    export type _$reverse<S extends string> = _ReverseHelper<S>
    >>>>>>> updated
    </source>

--- src/string/reverse.ts
+++ src/string/reverse.ts
@@ -4,18 +4,19 @@ import { Type, Kind } from '..'
  * `String._$reverse` is a type-level function that reverses the order of characters in a string.
  * 
  * @template S - The string to reverse.
- * @template O - The reversed string (initially empty).
  * 
  * @example
  * type T0 = String._$reverse<'foo'> // 'oof'
  * type T1 = String._$reverse<''> // ''
  */
-export type _$reverse<
+type _ReverseHelper<
   S extends string,
   O extends string = ''
 > = S extends `${infer Head}${infer Tail}`
-  ? _$reverse<Tail, `${Head}${O}`>
+  ? _ReverseHelper<Tail, `${Head}${O}`>
   : `${string extends S ? string : ''}${O}`
+
+export type _$reverse<S extends string> = _ReverseHelper<S>
 
 /**
  * `String.Reverse` is a type-level function that reverses the order of characters in a string.
