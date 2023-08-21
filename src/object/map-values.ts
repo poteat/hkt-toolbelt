7827df5dee52ee855bddc4e40d2e638f6fd240a7
o <poteat@users.noreply.github.com>
2023-08-21 14:11:50 -0700

    aider: Added JSDoc comments to `src/object/map-values.ts` following the guidelines in `style-guide/jsdoc.md` and provided examples in the comments using the `src/object/map-values.spec.ts` file.
    # Aider chat conversation:
    
    USER: "Can we write JSDoc for src/object/map-values.ts per style-guide/jsdoc.md? The spec file src/object/map-values.spec.ts may be useful."
    
    ASSISTANT: Sure, we can add JSDoc comments to the `src/object/map-values.ts` file. We will follow the guidelines provided in `style-guide/jsdoc.md`. The `src/object/map-values.spec.ts` file will be useful for providing examples in the JSDoc comments.
    
    Here are the changes:
    
    <source>typescript
    src/object/map-values.ts
    <<<<<<< HEAD
    import { $, Kind, Type } from '..'
    
    export type _$mapValues<
      T extends Record<string, unknown>,
      F extends Kind.Kind
    > = {
      [key in keyof T]: $<F, Type._$cast<T[key], Kind._$inputOf<F>>>
    }
    
    interface MapValues_T<F extends Kind.Kind> extends Kind.Kind {
      f(
        x: Type._$cast<this[Kind._], Record<string, Kind._$inputOf<F>>>
      ): _$mapValues<typeof x, F>
    }
    
    export interface MapValues extends Kind.Kind {
      f(x: Type._$cast<this[Kind._], Kind.Kind>): MapValues_T<typeof x>
    }
    =======
    import { $, Kind, Type } from '..'
    
    /**
     * `Object._$mapValues` is a type-level function that maps the values of an object.
     *
     * @template T - The object to map.
     * @template F - The function to apply to each value.
     *
     * @example
     * type T0 = Object._$mapValues<{ a: 'foo'; b: 'bar'; c: 'baz' }, String.ToUpper> // { a: 'FOO'; b: 'BAR'; c: 'BAZ' }
     */
    export type _$mapValues<
      T extends Record<string, unknown>,
      F extends Kind.Kind
    > = {
      [key in keyof T]: $<F, Type._$cast<T[key], Kind._$inputOf<F>>>
    }
    
    /**
     * `Object.MapValues_T` is an intermediate interface for currying.
     *
     * @template F - The function to apply to each value.
     */
    interface MapValues_T<F extends Kind.Kind> extends Kind.Kind {
      f(
        x: Type._$cast<this[Kind._], Record<string, Kind._$inputOf<F>>>
      ): _$mapValues<typeof x, F>
    }
    
    /**
     * `Object.MapValues` is a type-level function that maps the values of an object.
     *
     * @template T - The object to map.
     * @template F - The function to apply to each value.
     *
     * @example
     * type T0 = $<$<Object.MapValues, String.ToUpper>, { a: 'foo'; b: 'bar'; c: 'baz' }> // { a: 'FOO'; b: 'BAR'; c: 'BAZ' }
     */
    export interface MapValues extends Kind.Kind {
      f(x: Type._$cast<this[Kind._], Kind.Kind>): MapValues_T<typeof x>
    }
    >>>>>>> updated
    </source>

--- src/object/map-values.ts
+++ src/object/map-values.ts
@@ -1,5 +1,14 @@
 import { $, Kind, Type } from '..'
 
+/**
+ * `Object._$mapValues` is a type-level function that maps the values of an object.
+ * 
+ * @template T - The object to map.
+ * @template F - The function to apply to each value.
+ * 
+ * @example
+ * type T0 = Object._$mapValues<{ a: 'foo'; b: 'bar'; c: 'baz' }, String.ToUpper> // { a: 'FOO'; b: 'BAR'; c: 'BAZ' }
+ */
 export type _$mapValues<
   T extends Record<string, unknown>,
   F extends Kind.Kind
@@ -7,12 +16,26 @@ export type _$mapValues<
   [key in keyof T]: $<F, Type._$cast<T[key], Kind._$inputOf<F>>>
 }
 
+/**
+ * `Object.MapValues_T` is an intermediate interface for currying.
+ * 
+ * @template F - The function to apply to each value.
+ */
 interface MapValues_T<F extends Kind.Kind> extends Kind.Kind {
   f(
     x: Type._$cast<this[Kind._], Record<string, Kind._$inputOf<F>>>
   ): _$mapValues<typeof x, F>
 }
 
+/**
+ * `Object.MapValues` is a type-level function that maps the values of an object.
+ * 
+ * @template T - The object to map.
+ * @template F - The function to apply to each value.
+ * 
+ * @example
+ * type T0 = $<$<Object.MapValues, String.ToUpper>, { a: 'foo'; b: 'bar'; c: 'baz' }> // { a: 'FOO'; b: 'BAR'; c: 'BAZ' }
+ */
 export interface MapValues extends Kind.Kind {
   f(x: Type._$cast<this[Kind._], Kind.Kind>): MapValues_T<typeof x>
 }
