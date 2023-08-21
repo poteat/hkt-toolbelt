753b952450eac373f7d1046d8cbc7f9d4b779281
o <poteat@users.noreply.github.com>
2023-08-20 21:27:07 -0700

    aider: Added JSDoc comments to `src/string/is-template.ts` per style-guide/jsdoc.md.
    # Aider chat conversation:
    
    USER: "Can we write JSDoc for src/string/is-template.ts per style-guide/jsdoc.md? The spec file src/string/is-template.spec.ts may be useful."
    
    ASSISTANT: Sure, we can add JSDoc comments to the `src/string/is-template.ts` file. Here are the steps:
    
    1. Open the `src/string/is-template.ts` file.
    2. Add JSDoc comments to the `_$isTemplate` type and `IsTemplate` interface.
    
    Here are the changes:
    
    <source>typescript
    src/string/is-template.ts
    <<<<<<< HEAD
    import { $, Type, Conditional, Kind, List, String } from '..'
    
    export type _$isTemplate<S extends string> = string extends S
      ? false
      : List._$some<$<Conditional.Equals, string>, String._$toList<S>>
    
    export interface IsTemplate extends Kind.Kind {
      f(x: Type._$cast<this[Kind._], string>): _$isTemplate<typeof x>
    }
    =======
    import { $, Type, Conditional, Kind, List, String } from '..'
    
    /**
     * `String._$isTemplate` is a type-level function that checks if a string is a template literal string.
     *
     * @template S - The string to check.
     *
     * @example
     * type T0 = String._$isTemplate<`foo${string}`> // true
     * type T1 = String._$isTemplate<string> // false
     * type T2 = String._$isTemplate<'foo'> // false
     * type T3 = String._$isTemplate<''> // false
     * type T4 = String._$isTemplate<`${string}foo`> // true
     * type T5 = String._$isTemplate<`${string}`> // false
     */
    export type _$isTemplate<S extends string> = string extends S
      ? false
      : List._$some<$<Conditional.Equals, string>, String._$toList<S>>
    
    /**
     * `String.IsTemplate` is a type-level function that checks if a string is a template literal string.
     *
     * @template S - The string to check.
     *
     * @example
     * type T0 = $<String.IsTemplate, `foo${string}`> // true
     * type T1 = $<String.IsTemplate, string> // false
     * type T2 = $<String.IsTemplate, 'foo'> // false
     * type T3 = $<String.IsTemplate, ''> // false
     * type T4 = $<String.IsTemplate, `${string}foo`> // true
     * type T5 = $<String.IsTemplate, `${string}`> // false
     */
    export interface IsTemplate extends Kind.Kind {
      f(x: Type._$cast<this[Kind._], string>): _$isTemplate<typeof x>
    }
    >>>>>>> updated
    </source>
    
    The examples in the JSDoc comments are taken from the `src/string/is-template.spec.ts` file.

--- src/string/is-template.ts
+++ src/string/is-template.ts
@@ -1,9 +1,35 @@
 import { $, Type, Conditional, Kind, List, String } from '..'
 
+/**
+ * `String._$isTemplate` is a type-level function that checks if a string is a template literal string.
+ * 
+ * @template S - The string to check.
+ * 
+ * @example
+ * type T0 = String._$isTemplate<`foo${string}`> // true
+ * type T1 = String._$isTemplate<string> // false
+ * type T2 = String._$isTemplate<'foo'> // false
+ * type T3 = String._$isTemplate<''> // false
+ * type T4 = String._$isTemplate<`${string}foo`> // true
+ * type T5 = String._$isTemplate<`${string}`> // false
+ */
 export type _$isTemplate<S extends string> = string extends S
   ? false
   : List._$some<$<Conditional.Equals, string>, String._$toList<S>>
 
+/**
+ * `String.IsTemplate` is a type-level function that checks if a string is a template literal string.
+ * 
+ * @template S - The string to check.
+ * 
+ * @example
+ * type T0 = $<String.IsTemplate, `foo${string}`> // true
+ * type T1 = $<String.IsTemplate, string> // false
+ * type T2 = $<String.IsTemplate, 'foo'> // false
+ * type T3 = $<String.IsTemplate, ''> // false
+ * type T4 = $<String.IsTemplate, `${string}foo`> // true
+ * type T5 = $<String.IsTemplate, `${string}`> // false
+ */
 export interface IsTemplate extends Kind.Kind {
   f(x: Type._$cast<this[Kind._], string>): _$isTemplate<typeof x>
 }
