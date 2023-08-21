1b8b29e48e11b23691031c8f7a72ac7c249a311f
o <poteat@users.noreply.github.com>
2023-08-20 18:44:06 -0700

    aider: Added JSDoc comments to `src/string/slice.ts` per style-guide/jsdoc.md.
    # Aider chat conversation:
    
    USER: "Can we write JSDoc for src/string/slice.ts per style-guide/jsdoc.md? The spec file src/string/slice.spec.ts may be useful."
    
    ASSISTANT: Sure, we can add JSDoc comments to the `src/string/slice.ts` file.
    
    Here are the changes:
    
    <source>typescript
    src/string/slice.ts
    <<<<<<< HEAD
    import { Kind, Type, String, Number, List } from '..'
    
    export type _$slice<
      S extends string,
      N extends Number.Number
    > = String._$fromList<List._$shiftN<String._$toList<S>, N>>
    
    interface Slice_T<N extends Number.Number> extends Kind.Kind {
      f(x: Type._$cast<this[Kind._], string>): _$slice<typeof x, N>
    }
    
    export interface Slice extends Kind.Kind {
      f(x: Type._$cast<this[Kind._], Number.Number>): Slice_T<typeof x>
    }
    =======
    /**
     * `String._$slice` is a type-level function that slices a string from a given index.
     *
     * @template S - The string to slice.
     * @template N - The index from which to start the slice.
     *
     * @example
     * type T0 = String._$slice<'hello', 1> // 'ello'
     * type T1 = String._$slice<'hello', 0> // 'hello'
     */
    export type _$slice<
      S extends string,
      N extends Number.Number
    > = String._$fromList<List._$shiftN<String._$toList<S>, N>>
    
    interface Slice_T<N extends Number.Number> extends Kind.Kind {
      f(x: Type._$cast<this[Kind._], string>): _$slice<typeof x, N>
    }
    
    /**
     * `String.Slice` is a type-level function that slices a string from a given index.
     *
     * @example
     * type T0 = $<$<String.Slice, 1>, 'hello'> // 'ello'
     * type T1 = $<$<String.Slice, 0>, 'hello'> // 'hello'
     */
    export interface Slice extends Kind.Kind {
      f(x: Type._$cast<this[Kind._], Number.Number>): Slice_T<typeof x>
    }
    >>>>>>> updated
    </source>
    
    This change adds JSDoc comments to the `_$slice` type and `Slice` interface in `src/string/slice.ts`, following the guidelines in `style-guide/jsdoc.md`. The examples in the comments are based on the tests in `src/string/slice.spec.ts`.

--- src/string/slice.ts
+++ src/string/slice.ts
@@ -1,5 +1,13 @@
-import { Kind, Type, String, Number, List } from '..'
-
+/**
+ * `String._$slice` is a type-level function that slices a string from a given index.
+ * 
+ * @template S - The string to slice.
+ * @template N - The index from which to start the slice.
+ * 
+ * @example
+ * type T0 = String._$slice<'hello', 1> // 'ello'
+ * type T1 = String._$slice<'hello', 0> // 'hello'
+ */
 export type _$slice<
   S extends string,
   N extends Number.Number
@@ -9,6 +17,13 @@ interface Slice_T<N extends Number.Number> extends Kind.Kind {
   f(x: Type._$cast<this[Kind._], string>): _$slice<typeof x, N>
 }
 
+/**
+ * `String.Slice` is a type-level function that slices a string from a given index.
+ * 
+ * @example
+ * type T0 = $<$<String.Slice, 1>, 'hello'> // 'ello'
+ * type T1 = $<$<String.Slice, 0>, 'hello'> // 'hello'
+ */
 export interface Slice extends Kind.Kind {
   f(x: Type._$cast<this[Kind._], Number.Number>): Slice_T<typeof x>
 }
