a2edad4ff6838639a1f32a1952a4c4f0db11959c
o <poteat@users.noreply.github.com>
2023-08-20 19:02:01 -0700

    aider: Added JSDoc comments to `src/string/join.ts` per style-guide/jsdoc.md.
    # Aider chat conversation:
    
    USER: "Can we write JSDoc for src/string/join.ts per style-guide/jsdoc.md? The spec file src/string/join.spec.ts may be useful."
    
    ASSISTANT: Sure, we can add JSDoc comments to the `src/string/join.ts` file.
    
    Here are the steps we need to follow:
    
    1. Identify the exported types and interfaces in the `src/string/join.ts` file that need JSDoc comments. These are `_$join`, `Join_T`, and `Join`.
    2. Write JSDoc comments for each of these, following the guidelines in `style-guide/jsdoc.md`. We can use the test cases in `src/string/join.spec.ts` to provide examples of usage.
    
    Here are the proposed changes:
    
    <source>typescript
    src/string/join.ts
    <<<<<<< HEAD
    export type _$join<
      T extends (string | unknown)[],
      D extends string = '',
      O extends string = ''
    > = List._$isVariadic<T> extends true
      ? string
      : T extends [infer Head, ...infer Tail]
      ? Tail extends []
        ? `${O}${O extends '' ? '' : D}${Type._$cast<Head, string>}`
        : _$join<
            Type._$cast<Tail, string[]>,
            D,
            `${O}${O extends '' ? '' : D}${Type._$cast<Head, string>}`
          >
      : string[] extends T
      ? `${O}${string}`
      : O
    
    interface Join_T<D extends string> extends Kind.Kind {
      f(x: Type._$cast<this[Kind._], (string | unknown)[]>): _$join<typeof x, D>
    }
    
    export interface Join extends Kind.Kind {
      f(x: Type._$cast<this[Kind._], string>): Join_T<typeof x>
    }
    =======
    /**
     * `String._$join` is a type-level function that joins an array of strings into a single string.
     *
     * @template T - The array of strings to join.
     * @template D - The delimiter to use when joining the strings.
     * @template O - The output string.
     *
     * @example
     * type T0 = String._$join<['foo', 'bar'], '-', ''> // 'foo-bar'
     * type T1 = String._$join<['foo', 'bar'], '', ''> // 'foobar'
     */
    export type _$join<
      T extends (string | unknown)[],
      D extends string = '',
      O extends string = ''
    > = List._$isVariadic<T> extends true
      ? string
      : T extends [infer Head, ...infer Tail]
      ? Tail extends []
        ? `${O}${O extends '' ? '' : D}${Type._$cast<Head, string>}`
        : _$join<
            Type._$cast<Tail, string[]>,
            D,
            `${O}${O extends '' ? '' : D}${Type._$cast<Head, string>}`
          >
      : string[] extends T
      ? `${O}${string}`
      : O
    
    /**
     * `String.Join_T` is an intermediate interface for currying.
     *
     * @template D - The delimiter to use when joining the strings.
     */
    interface Join_T<D extends string> extends Kind.Kind {
      f(x: Type._$cast<this[Kind._], (string | unknown)[]>): _$join<typeof x, D>
    }
    
    /**
     * `String.Join` is a type-level function that joins an array of strings into a single string.
     *
     * @template D - The delimiter to use when joining the strings.
     *
     * @example
     * type T0 = $<String.Join, ['foo', 'bar']> // 'foobar'
     * type T1 = $<String.Join, ['foo', 'bar', 'qux']> // 'foo bar qux'
     */
    export interface Join extends Kind.Kind {
      f(x: Type._$cast<this[Kind._], string>): Join_T<typeof x>
    }
    >>>>>>> updated
    </source>
    
    Please confirm these changes.

--- src/string/join.ts
+++ src/string/join.ts
@@ -1,5 +1,16 @@
 import { Type, Kind, List } from '..'
 
+/**
+ * `String._$join` is a type-level function that joins an array of strings into a single string.
+ * 
+ * @template T - The array of strings to join.
+ * @template D - The delimiter to use when joining the strings.
+ * @template O - The output string.
+ * 
+ * @example
+ * type T0 = String._$join<['foo', 'bar'], '-', ''> // 'foo-bar'
+ * type T1 = String._$join<['foo', 'bar'], '', ''> // 'foobar'
+ */
 export type _$join<
   T extends (string | unknown)[],
   D extends string = '',
@@ -18,10 +29,24 @@ export type _$join<
   ? `${O}${string}`
   : O
 
+/**
+ * `String.Join_T` is an intermediate interface for currying.
+ * 
+ * @template D - The delimiter to use when joining the strings.
+ */
 interface Join_T<D extends string> extends Kind.Kind {
   f(x: Type._$cast<this[Kind._], (string | unknown)[]>): _$join<typeof x, D>
 }
 
+/**
+ * `String.Join` is a type-level function that joins an array of strings into a single string.
+ * 
+ * @template D - The delimiter to use when joining the strings.
+ * 
+ * @example
+ * type T0 = $<String.Join, ['foo', 'bar']> // 'foobar'
+ * type T1 = $<String.Join, ['foo', 'bar', 'qux']> // 'foo bar qux'
+ */
 export interface Join extends Kind.Kind {
   f(x: Type._$cast<this[Kind._], string>): Join_T<typeof x>
 }
