aaf28874d0dabba1cb0a70052c012098725fd439
o <poteat@users.noreply.github.com>
2023-08-20 19:00:41 -0700

    aider: Added JSDoc comments to `src/string/length.ts` per style-guide/jsdoc.md.
    # Aider chat conversation:
    
    USER: "Can we write JSDoc for src/string/length.ts per style-guide/jsdoc.md? The spec file src/string/length.spec.ts may be useful."
    
    ASSISTANT: Sure, we can add JSDoc comments to the `src/string/length.ts` file. We will follow the guidelines provided in the `style-guide/jsdoc.md` file. The `src/string/length.spec.ts` file will be useful for providing examples in the JSDoc comments.
    
    Here are the changes we need to make:
    
    1. Add JSDoc comments to the `_$length` type and the `Length` interface in the `src/string/length.ts` file.
    
    Here are the proposed changes:
    
    <source>typescript
    src/string/length.ts
    <<<<<<< HEAD
    import { Type, Kind, String } from '..'
    
    export type _$length<S extends string> = String._$isTemplate<S> extends true
      ? number
      : string extends S
      ? number
      : String._$toList<S>['length']
    
    export interface Length extends Kind.Kind {
      f(x: Type._$cast<this[Kind._], string>): _$length<typeof x>
    }
    =======
    import { Type, Kind, String } from '..'
    
    /**
     * `String._$length` is a type-level function that returns the length of a string.
     *
     * @template S - The string to get the length of.
     *
     * @example
     * type T0 = String._$length<'hello'> // 5
     * type T1 = String._$length<''> // 0
     */
    export type _$length<S extends string> = String._$isTemplate<S> extends true
      ? number
      : string extends S
      ? number
      : String._$toList<S>['length']
    
    /**
     * `String.Length` is a type-level function that returns the length of a string.
     *
     * @template S - The string to get the length of.
     *
     * @example
     * type T0 = $<String.Length, 'hello'> // 5
     * type T1 = $<String.Length, ''> // 0
     */
    export interface Length extends Kind.Kind {
      f(x: Type._$cast<this[Kind._], string>): _$length<typeof x>
    }
    >>>>>>> updated
    </source>

--- src/string/length.ts
+++ src/string/length.ts
@@ -1,11 +1,29 @@
 import { Type, Kind, String } from '..'
 
+/**
+ * `String._$length` is a type-level function that returns the length of a string.
+ * 
+ * @template S - The string to get the length of.
+ * 
+ * @example
+ * type T0 = String._$length<'hello'> // 5
+ * type T1 = String._$length<''> // 0
+ */
 export type _$length<S extends string> = String._$isTemplate<S> extends true
   ? number
   : string extends S
   ? number
   : String._$toList<S>['length']
 
+/**
+ * `String.Length` is a type-level function that returns the length of a string.
+ * 
+ * @template S - The string to get the length of.
+ * 
+ * @example
+ * type T0 = $<String.Length, 'hello'> // 5
+ * type T1 = $<String.Length, ''> // 0
+ */
 export interface Length extends Kind.Kind {
   f(x: Type._$cast<this[Kind._], string>): _$length<typeof x>
 }
