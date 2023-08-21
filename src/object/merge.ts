072f012065c6272f494cba2140e8053bbc882ff5
o <poteat@users.noreply.github.com>
2023-08-21 14:06:31 -0700

    aider: Added JSDoc comments to `src/object/merge.ts` per style-guide/jsdoc.md.
    # Aider chat conversation:
    
    USER: "Can we write JSDoc for src/object/merge.ts per style-guide/jsdoc.md? The spec file src/object/merge.spec.ts may be useful."
    
    ASSISTANT: Sure, we can add JSDoc comments to the `src/object/merge.ts` file. We will follow the guidelines provided in `style-guide/jsdoc.md`. The `src/object/merge.spec.ts` file will be useful for providing examples in the JSDoc comments.
    
    Here are the changes we need to make:
    
    1. Add JSDoc comments to the `_$merge` type.
    2. Add JSDoc comments to the `Merge_T` interface.
    3. Add JSDoc comments to the `Merge` interface.
    
    Here are the proposed changes:
    
    <source>typescript
    src/object/merge.ts
    <<<<<<< HEAD
    export type _$merge<
      A extends Record<string, unknown>,
      B extends Record<string, unknown>
    > = Type._$display<{
      [K in keyof A | keyof B]: K extends keyof B
        ? B[K]
        : K extends keyof A
        ? A[K]
        : never
    }>
    
    interface Merge_T<A extends Record<string, unknown>> extends Kind.Kind {
      f(x: Type._$cast<this[Kind._], Record<string, unknown>>): _$merge<A, typeof x>
    }
    
    export interface Merge extends Kind.Kind {
      f(x: Type._$cast<this[Kind._], Record<string, unknown>>): Merge_T<typeof x>
    }
    =======
    /**
     * `Object._$merge` is a type-level function that merges two objects.
     *
     * @template A - The first object to merge.
     * @template B - The second object to merge.
     *
     * @example
     * type T0 = Object._$merge<{ a: 1; b: 2 }, { c: 3; d: 4 }> // { a: 1; b: 2; c: 3; d: 4 }
     * type T1 = Object._$merge<{}, { a: 1; b: 2 }> // { a: 1; b: 2 }
     * type T2 = Object._$merge<{ a: 1 }, { a: 2 }> // { a: 2 }
     */
    export type _$merge<
      A extends Record<string, unknown>,
      B extends Record<string, unknown>
    > = Type._$display<{
      [K in keyof A | keyof B]: K extends keyof B
        ? B[K]
        : K extends keyof A
        ? A[K]
        : never
    }>
    
    /**
     * `Object.Merge_T` is an intermediate interface for currying.
     *
     * @template A - The first object to merge.
     */
    interface Merge_T<A extends Record<string, unknown>> extends Kind.Kind {
      f(x: Type._$cast<this[Kind._], Record<string, unknown>>): _$merge<A, typeof x>
    }
    
    /**
     * `Object.Merge` is a type-level function that merges two objects.
     *
     * @template A - The first object to merge.
     * @template B - The second object to merge.
     *
     * @example
     * type T0 = $<$<Object.Merge, { a: 1; b: 2 }>, { c: 3; d: 4 }> // { a: 1; b: 2; c: 3; d: 4 }
     * type T1 = $<$<Object.Merge, {}>, { a: 1; b: 2 }> // { a: 1; b: 2 }
     * type T2 = $<$<Object.Merge, { a: 1 }>, { a: 2 }> // { a: 2 }
     */
    export interface Merge extends Kind.Kind {
      f(x: Type._$cast<this[Kind._], Record<string, unknown>>): Merge_T<typeof x>
    }
    >>>>>>> updated
    </source>

--- src/object/merge.ts
+++ src/object/merge.ts
@@ -1,5 +1,16 @@
 import { Kind, Type } from '..'
 
+/**
+ * `Object._$merge` is a type-level function that merges two objects.
+ * 
+ * @template A - The first object to merge.
+ * @template B - The second object to merge.
+ * 
+ * @example
+ * type T0 = Object._$merge<{ a: 1; b: 2 }, { c: 3; d: 4 }> // { a: 1; b: 2; c: 3; d: 4 }
+ * type T1 = Object._$merge<{}, { a: 1; b: 2 }> // { a: 1; b: 2 }
+ * type T2 = Object._$merge<{ a: 1 }, { a: 2 }> // { a: 2 }
+ */
 export type _$merge<
   A extends Record<string, unknown>,
   B extends Record<string, unknown>
@@ -11,10 +22,26 @@ export type _$merge<
     : never
 }>
 
+/**
+ * `Object.Merge_T` is an intermediate interface for currying.
+ * 
+ * @template A - The first object to merge.
+ */
 interface Merge_T<A extends Record<string, unknown>> extends Kind.Kind {
   f(x: Type._$cast<this[Kind._], Record<string, unknown>>): _$merge<A, typeof x>
 }
 
+/**
+ * `Object.Merge` is a type-level function that merges two objects.
+ * 
+ * @template A - The first object to merge.
+ * @template B - The second object to merge.
+ * 
+ * @example
+ * type T0 = $<$<Object.Merge, { a: 1; b: 2 }>, { c: 3; d: 4 }> // { a: 1; b: 2; c: 3; d: 4 }
+ * type T1 = $<$<Object.Merge, {}>, { a: 1; b: 2 }> // { a: 1; b: 2 }
+ * type T2 = $<$<Object.Merge, { a: 1 }>, { a: 2 }> // { a: 2 }
+ */
 export interface Merge extends Kind.Kind {
   f(x: Type._$cast<this[Kind._], Record<string, unknown>>): Merge_T<typeof x>
 }
