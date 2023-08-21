e04b91b75de25c3673bf5c2c8d87a92f76a47788
o <poteat@users.noreply.github.com>
2023-08-20 18:26:22 -0700

    aider: Added JSDoc comments to `src/string/to-upper.ts` per style-guide/jsdoc.md.
    # Aider chat conversation:
    
    USER: "Can we write JSDoc for src/string/to-upper.ts per style-guide/jsdoc.md? The spec file src/string/to-upper.spec.ts may be useful."
    
    ASSISTANT: Sure, we can add JSDoc comments to the `src/string/to-upper.ts` file. We will follow the guidelines provided in the `style-guide/jsdoc.md` file. The `src/string/to-upper.spec.ts` file will be useful to provide examples of usage in the JSDoc comments.
    
    Here are the changes we need to make:
    
    1. Add JSDoc comments to the `_$toUpper` type and `ToUpper` interface in `src/string/to-upper.ts`.
    
    Here is the *edit block* for the changes:
    
    <source>typescript
    src/string/to-upper.ts
    <<<<<<< HEAD
    import { Type, Kind } from '..'
    
    export type _$toUpper<S extends string> = Uppercase<S>
    
    export interface ToUpper extends Kind.Kind {
      f(x: Type._$cast<this[Kind._], string>): _$toUpper<typeof x>
    }
    =======
    import { Type, Kind } from '..'
    
    /**
     * `String._$toUpper` is a type-level function that converts a string to uppercase.
     *
     * @template S - The string to convert to uppercase.
     *
     * @example
     * type T0 = String._$toUpper<'foo'> // 'FOO'
     * type T1 = String._$toUpper<'bar'> // 'BAR'
     */
    export type _$toUpper<S extends string> = Uppercase<S>
    
    /**
     * `String.ToUpper` is a type-level function that converts a string to uppercase.
     *
     * @example
     * type T0 = $<String.ToUpper, 'foo'> // 'FOO'
     * type T1 = $<String.ToUpper, 'bar'> // 'BAR'
     */
    export interface ToUpper extends Kind.Kind {
      f(x: Type._$cast<this[Kind._], string>): _$toUpper<typeof x>
    }
    >>>>>>> updated
    </source>

--- src/string/to-upper.ts
+++ src/string/to-upper.ts
@@ -1,7 +1,23 @@
 import { Type, Kind } from '..'
 
+/**
+ * `String._$toUpper` is a type-level function that converts a string to uppercase.
+ * 
+ * @template S - The string to convert to uppercase.
+ * 
+ * @example
+ * type T0 = String._$toUpper<'foo'> // 'FOO'
+ * type T1 = String._$toUpper<'bar'> // 'BAR'
+ */
 export type _$toUpper<S extends string> = Uppercase<S>
 
+/**
+ * `String.ToUpper` is a type-level function that converts a string to uppercase.
+ * 
+ * @example
+ * type T0 = $<String.ToUpper, 'foo'> // 'FOO'
+ * type T1 = $<String.ToUpper, 'bar'> // 'BAR'
+ */
 export interface ToUpper extends Kind.Kind {
   f(x: Type._$cast<this[Kind._], string>): _$toUpper<typeof x>
 }
