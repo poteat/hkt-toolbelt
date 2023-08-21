19b893730bdc4ca2fc5f97a044bb9be844f3737f
o <poteat@users.noreply.github.com>
2023-08-20 18:32:13 -0700

    aider: Add JSDoc comments to `src/string/split.ts` per style-guide/jsdoc.md.
    # Aider chat conversation:
    
    USER: "Can we write JSDoc for src/string/split.ts per style-guide/jsdoc.md? The spec file src/string/split.spec.ts may be useful."
    
    ASSISTANT: Sure, we can add JSDoc comments to the `src/string/split.ts` file. We will need to modify the following files:
    
    1. `src/string/split.ts`
    
    Here are the changes we need to make:
    
    1. Add JSDoc comments to the `_$split` type, `Split_T` interface, and `Split` interface in `src/string/split.ts`. We will use the examples from `src/string/split.spec.ts` to illustrate the usage of these types and interfaces.
    
    Here are the proposed changes:
    
    <source>typescript
    src/string/split.ts
    <<<<<<< HEAD
    export type _$split<
      S extends string,
      Delimiter extends string = '',
      O extends unknown[] = []
    > = String._$isTemplate<Delimiter> extends true
      ? string[]
      : string extends Delimiter
      ? string[]
      : S extends `${infer Head}${Delimiter}${infer Tail}`
      ? _$split<Tail, Delimiter, [...O, Head]>
      : S extends Delimiter
      ? O
      : [...O, S]
    
    interface Split_T<Delimiter extends string> extends Kind.Kind {
      f(x: Type._$cast<this[Kind._], string>): _$split<typeof x, Delimiter>
    }
    
    export interface Split extends Kind.Kind {
      f(x: Type._$cast<this[Kind._], string>): Split_T<typeof x>
    }
    =======
    /**
     * `String.Split` is a type-level function that splits a string into an array of substrings.
     *
     * @template S - The string to split.
     * @template Delimiter - The delimiter to split the string by.
     * @template O - The output array of substrings.
     *
     * @example
     * type T0 = String._$split<'foobar', ''> // ['f', 'o', 'o', 'b', 'a', 'r']
     * type T1 = String._$split<'foo bar', ' '> // ['foo', 'bar']
     */
    export type _$split<
      S extends string,
      Delimiter extends string = '',
      O extends unknown[] = []
    > = String._$isTemplate<Delimiter> extends true
      ? string[]
      : string extends Delimiter
      ? string[]
      : S extends `${infer Head}${Delimiter}${infer Tail}`
      ? _$split<Tail, Delimiter, [...O, Head]>
      : S extends Delimiter
      ? O
      : [...O, S]
    
    /**
     * `String.Split_T` is an intermediate interface for currying.
     *
     * @template Delimiter - The delimiter to split the string by.
     */
    interface Split_T<Delimiter extends string> extends Kind.Kind {
      f(x: Type._$cast<this[Kind._], string>): _$split<typeof x, Delimiter>
    }
    
    /**
     * `String.Split` is a type-level function that splits a string into an array of substrings.
     *
     * @example
     * type T0 = $<$<String.Split, ''>, 'foobar'> // ['f', 'o', 'o', 'b', 'a', 'r']
     * type T1 = $<$<String.Split, ' '>, 'foo bar'> // ['foo', 'bar']
     */
    export interface Split extends Kind.Kind {
      f(x: Type._$cast<this[Kind._], string>): Split_T<typeof x>
    }
    >>>>>>> updated
    </source>

--- src/string/split.ts
+++ src/string/split.ts
@@ -1,5 +1,16 @@
 import { Type, Kind, String } from '..'
 
+/**
+ * `String.Split` is a type-level function that splits a string into an array of substrings.
+ * 
+ * @template S - The string to split.
+ * @template Delimiter - The delimiter to split the string by.
+ * @template O - The output array of substrings.
+ * 
+ * @example
+ * type T0 = String._$split<'foobar', ''> // ['f', 'o', 'o', 'b', 'a', 'r']
+ * type T1 = String._$split<'foo bar', ' '> // ['foo', 'bar']
+ */
 export type _$split<
   S extends string,
   Delimiter extends string = '',
@@ -14,10 +25,22 @@ export type _$split<
   ? O
   : [...O, S]
 
+/**
+ * `String.Split_T` is an intermediate interface for currying.
+ * 
+ * @template Delimiter - The delimiter to split the string by.
+ */
 interface Split_T<Delimiter extends string> extends Kind.Kind {
   f(x: Type._$cast<this[Kind._], string>): _$split<typeof x, Delimiter>
 }
 
+/**
+ * `String.Split` is a type-level function that splits a string into an array of substrings.
+ * 
+ * @example
+ * type T0 = $<$<String.Split, ''>, 'foobar'> // ['f', 'o', 'o', 'b', 'a', 'r']
+ * type T1 = $<$<String.Split, ' '>, 'foo bar'> // ['foo', 'bar']
+ */
 export interface Split extends Kind.Kind {
   f(x: Type._$cast<this[Kind._], string>): Split_T<typeof x>
 }
