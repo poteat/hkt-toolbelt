c4a1b366a3b5981051079926675859d4d40de793
o <poteat@users.noreply.github.com>
2023-08-21 14:13:45 -0700

    aider: Added JSDoc comments to `src/object/keys.ts` following the guidelines in `style-guide/jsdoc.md` and provided examples of usage.
    # Aider chat conversation:
    
    USER: "Can we write JSDoc for src/object/keys.ts per style-guide/jsdoc.md? The spec file src/object/keys.spec.ts may be useful."
    
    ASSISTANT: Sure, we can add JSDoc comments to the `src/object/keys.ts` file. We will follow the guidelines provided in `style-guide/jsdoc.md` and use the spec file `src/object/keys.spec.ts` as a reference.
    
    Here are the steps we need to take:
    
    1. Add a JSDoc comment to the `_$keys` type utility. This comment should explain what the utility does, its template parameter, and provide an example of its usage.
    2. Add a JSDoc comment to the `Keys` interface. This comment should be similar to the one for `_$keys`, but it should also explain the `f` method and provide an example of its usage with the `$` syntax.
    
    Here are the proposed changes:
    
    <source>typescript
    src/object/keys.ts
    <<<<<<< HEAD
    import { Kind, Type, Union } from '..'
    
    export type _$keys<T extends Record<string, unknown>> = Union._$toList<keyof T>
    
    export interface Keys extends Kind.Kind {
      f(x: Type._$cast<this[Kind._], Record<string, unknown>>): _$keys<typeof x>
    }
    =======
    import { Kind, Type, Union } from '..'
    
    /**
     * `Object.Keys` is a type-level function that returns the keys of an object as a tuple.
     *
     * @template T - The object to get the keys of.
     *
     * @example
     * type T0 = Object._$keys<{ a: 1; b: 2; c: 3 }> // ['a', 'b', 'c']
     */
    export type _$keys<T extends Record<string, unknown>> = Union._$toList<keyof T>
    
    /**
     * `Object.Keys` is a type-level function that returns the keys of an object as a tuple.
     *
     * @template T - The object to get the keys of.
     *
     * @example
     * type T0 = $<Object.Keys, { a: 1; b: 2; c: 3 }> // ['a', 'b', 'c']
     */
    export interface Keys extends Kind.Kind {
      f(x: Type._$cast<this[Kind._], Record<string, unknown>>): _$keys<typeof x>
    }
    >>>>>>> updated
    </source>

--- src/object/keys.ts
+++ src/object/keys.ts
@@ -1,7 +1,23 @@
 import { Kind, Type, Union } from '..'
 
+/**
+ * `Object.Keys` is a type-level function that returns the keys of an object as a tuple.
+ * 
+ * @template T - The object to get the keys of.
+ * 
+ * @example
+ * type T0 = Object._$keys<{ a: 1; b: 2; c: 3 }> // ['a', 'b', 'c']
+ */
 export type _$keys<T extends Record<string, unknown>> = Union._$toList<keyof T>
 
+/**
+ * `Object.Keys` is a type-level function that returns the keys of an object as a tuple.
+ * 
+ * @template T - The object to get the keys of.
+ * 
+ * @example
+ * type T0 = $<Object.Keys, { a: 1; b: 2; c: 3 }> // ['a', 'b', 'c']
+ */
 export interface Keys extends Kind.Kind {
   f(x: Type._$cast<this[Kind._], Record<string, unknown>>): _$keys<typeof x>
 }
