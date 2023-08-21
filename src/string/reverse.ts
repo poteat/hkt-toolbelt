69328712234e935f572da3114eca6cae7d4ae539
o <poteat@users.noreply.github.com>
2023-08-20 18:44:34 -0700

    aider: Added JSDoc comments to `src/string/reverse.ts` to document the `String._$reverse` type and the `String.Reverse` interface.
    # Aider chat conversation:
    
    USER: "Can we write JSDoc for src/string/reverse.ts per style-guide/jsdoc.md? The spec file src/string/reverse.spec.ts may be useful."
    
    ASSISTANT: Sure, we can add JSDoc comments to the `src/string/reverse.ts` file. We will need to modify the following files:
    
    1. `src/string/reverse.ts`
    
    Here are the changes we need to make:
    
    1. In `src/string/reverse.ts`, we will add JSDoc comments to the `_$reverse` type and the `Reverse` interface. The comments will explain what these entities do, their template parameters, and provide examples of their usage. The examples will be based on the tests in `src/string/reverse.spec.ts`.
    
    Here are the proposed changes:
    
    <source>typescript
    src/string/reverse.ts
    <<<<<<< HEAD
    import { Type, Kind } from '..'
    
    export type _$reverse<
      S extends string,
      O extends string = ''
    > = S extends `${infer Head}${infer Tail}`
      ? _$reverse<Tail, `${Head}${O}`>
      : `${string extends S ? string : ''}${O}`
    
    export interface Reverse extends Kind.Kind {
      f(x: Type._$cast<this[Kind._], string>): _$reverse<typeof x>
    }
    =======
    import { Type, Kind } from '..'
    
    /**
     * `String._$reverse` is a type-level function that reverses the order of characters in a string.
     *
     * @template S - The string to reverse.
     * @template O - The reversed string (initially empty).
     *
     * @example
     * type T0 = String._$reverse<'foo'> // 'oof'
     * type T1 = String._$reverse<''> // ''
     */
    export type _$reverse<
      S extends string,
      O extends string = ''
    > = S extends `${infer Head}${infer Tail}`
      ? _$reverse<Tail, `${Head}${O}`>
      : `${string extends S ? string : ''}${O}`
    
    /**
     * `String.Reverse` is a type-level function that reverses the order of characters in a string.
     *
     * @example
     * type T0 = $<String.Reverse, 'foo'> // 'oof'
     * type T1 = $<String.Reverse, ''> // ''
     */
    export interface Reverse extends Kind.Kind {
      f(x: Type._$cast<this[Kind._], string>): _$reverse<typeof x>
    }
    >>>>>>> updated
    </source>

--- src/string/reverse.ts
+++ src/string/reverse.ts
@@ -1,5 +1,15 @@
 import { Type, Kind } from '..'
 
+/**
+ * `String._$reverse` is a type-level function that reverses the order of characters in a string.
+ * 
+ * @template S - The string to reverse.
+ * @template O - The reversed string (initially empty).
+ * 
+ * @example
+ * type T0 = String._$reverse<'foo'> // 'oof'
+ * type T1 = String._$reverse<''> // ''
+ */
 export type _$reverse<
   S extends string,
   O extends string = ''
@@ -7,6 +17,13 @@ export type _$reverse<
   ? _$reverse<Tail, `${Head}${O}`>
   : `${string extends S ? string : ''}${O}`
 
+/**
+ * `String.Reverse` is a type-level function that reverses the order of characters in a string.
+ * 
+ * @example
+ * type T0 = $<String.Reverse, 'foo'> // 'oof'
+ * type T1 = $<String.Reverse, ''> // ''
+ */
 export interface Reverse extends Kind.Kind {
   f(x: Type._$cast<this[Kind._], string>): _$reverse<typeof x>
 }
