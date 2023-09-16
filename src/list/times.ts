d8fd28dcc3f217151e27c73f8218800838fd99ff
Jongsun Suh <34228073+MajorLift@users.noreply.github.com>
2023-09-15 18:46:28 -0700

    aider: Added JSDoc comments for `_$times2`, `_$times`, and `Times` in `src/list/times.ts`.
    # Aider chat conversation:
    
    USER: "Can we write JSDoc for src/list/times.ts per style-guide/jsdoc.md? The spec file src/list/times.spec.ts may be useful."
    
    ASSISTANT: Sure, we can add JSDoc comments to the `src/list/times.ts` file. We will need to modify the `src/list/times.ts` file to add the JSDoc comments.
    
    Here are the changes we need to make:
    
    1. Add JSDoc for the `_$times2` type utility.
    2. Add JSDoc for the `_$times` type utility.
    3. Add JSDoc for the `Times` interface.
    
    Here are the proposed changes:
    
    <source>typescript
    src/list/times.ts
    <<<<<<< HEAD
    type _$times2<
      COUNTER extends DigitList.DigitList,
      STATE extends List.List = [],
      DEC extends DigitList.DigitList = DigitList._$decrement<COUNTER>,
      DEC_STR extends string = DigitList._$toString<DEC>,
      DEC_NUM extends Number.Number = Number._$fromString<DEC_STR>
    > = 0 extends 1
      ? never
      : COUNTER extends ['0']
      ? STATE
      : _$times2<DEC, [DEC_NUM, ...STATE]>
    =======
    /**
     * `_$times2` is a type-level function that generates a list of numbers from 0 to N-1.
     *
     * @template COUNTER - The counter for the recursion.
     * @template STATE - The current state of the list.
     * @template DEC - The decremented counter.
     * @template DEC_STR - The string representation of the decremented counter.
     * @template DEC_NUM - The number representation of the decremented counter.
     */
    type _$times2<
      COUNTER extends DigitList.DigitList,
      STATE extends List.List = [],
      DEC extends DigitList.DigitList = DigitList._$decrement<COUNTER>,
      DEC_STR extends string = DigitList._$toString<DEC>,
      DEC_NUM extends Number.Number = Number._$fromString<DEC_STR>
    > = 0 extends 1
      ? never
      : COUNTER extends ['0']
      ? STATE
      : _$times2<DEC, [DEC_NUM, ...STATE]>
    >>>>>>> updated
    </source>
    
    <source>typescript
    src/list/times.ts
    <<<<<<< HEAD
    export type _$times<N extends Number.Number> = _$times2<
      NaturalNumber._$toList<N>
    >
    =======
    /**
     * `_$times` is a type-level function that generates a list of numbers from 0 to N-1.
     *
     * @template N - The number to generate the list to.
     */
    export type _$times<N extends Number.Number> = _$times2<
      NaturalNumber._$toList<N>
    >
    >>>>>>> updated
    </source>
    
    <source>typescript
    src/list/times.ts
    <<<<<<< HEAD
    export interface Times extends Kind.Kind {
      f(
        x: Type._$cast<this[Kind._], Number.Number>
      ): Number._$isNatural<typeof x> extends true ? _$times<typeof x> : never
    }
    =======
    /**
     * `Times` is a type-level function that generates a list of numbers from 0 to N-1.
     *
     * @template x - The number to generate the list to.
     */
    export interface Times extends Kind.Kind {
      f(
        x: Type._$cast<this[Kind._], Number.Number>
      ): Number._$isNatural<typeof x> extends true ? _$times<typeof x> : never
    }
    >>>>>>> updated
    </source>

--- src/list/times.ts
+++ src/list/times.ts
@@ -1,5 +1,14 @@
 import { Kind, Type, DigitList, Number, List, NaturalNumber } from '..'
 
+/**
+ * `_$times2` is a type-level function that generates a list of numbers from 0 to N-1.
+ * 
+ * @template COUNTER - The counter for the recursion.
+ * @template STATE - The current state of the list.
+ * @template DEC - The decremented counter.
+ * @template DEC_STR - The string representation of the decremented counter.
+ * @template DEC_NUM - The number representation of the decremented counter.
+ */
 type _$times2<
   COUNTER extends DigitList.DigitList,
   STATE extends List.List = [],
@@ -12,10 +21,20 @@ type _$times2<
   ? STATE
   : _$times2<DEC, [DEC_NUM, ...STATE]>
 
+/**
+ * `_$times` is a type-level function that generates a list of numbers from 0 to N-1.
+ * 
+ * @template N - The number to generate the list to.
+ */
 export type _$times<N extends Number.Number> = _$times2<
   NaturalNumber._$toList<N>
 >
 
+/**
+ * `Times` is a type-level function that generates a list of numbers from 0 to N-1.
+ * 
+ * @template x - The number to generate the list to.
+ */
 export interface Times extends Kind.Kind {
   f(
     x: Type._$cast<this[Kind._], Number.Number>
