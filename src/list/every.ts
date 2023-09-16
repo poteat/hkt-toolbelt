3f609f740b631f40741ff194defa986e8c5cede1
Jongsun Suh <34228073+MajorLift@users.noreply.github.com>
2023-09-15 20:38:26 -0700

    aider: Added JSDoc comments to `src/list/every.ts` per style-guide/jsdoc.md.
    # Aider chat conversation:
    
    USER: "Can we write JSDoc for src/list/every.ts per style-guide/jsdoc.md? The spec file src/list/every.spec.ts may be useful."
    
    ASSISTANT: Sure, we can add JSDoc comments to the `src/list/every.ts` file.
    
    We will need to modify the following file:
    - `src/list/every.ts`
    
    Here are the changes we need to make:
    
    1. Add JSDoc comments to the `_$every` type utility. The comments should explain what the utility does, its template parameters, and provide an example of its usage.
    
    2. Add JSDoc comments to the `Every_T` interface. The comments should explain what the interface does, its template parameters, and provide an example of its usage.
    
    3. Add JSDoc comments to the `Every` interface. The comments should explain what the interface does, its template parameters, and provide an example of its usage.
    
    Here are the proposed changes:
    
    <source>typescript
    src/list/every.ts
    <<<<<<< HEAD
    import { $, Boolean, Type, Kind } from '..'
    
    export type _$every<
      F extends Kind.Kind<(x: never) => boolean>,
      T extends unknown[],
      O extends boolean = true
    > = T extends [infer Head, ...infer Rest]
      ? _$every<
          F,
          Rest,
          Boolean._$and<O, $<F, Type._$cast<Head, Kind._$inputOf<F>>>>
        >
      : O
    
    interface Every_T<F extends Kind.Kind<(x: never) => boolean>>
      extends Kind.Kind {
      f(x: Type._$cast<this[Kind._], Kind._$inputOf<F>[]>): _$every<F, typeof x>
    }
    
    export interface Every extends Kind.Kind {
      f(
        x: Type._$cast<this[Kind._], Kind.Kind<(x: never) => boolean>>
      ): Every_T<typeof x>
    }
    =======
    import { $, Boolean, Type, Kind } from '..'
    
    /**
     * `List._$every` is a type-level function that checks if every element in a tuple satisfies a predicate.
     *
     * @template F - The predicate function.
     * @template T - The tuple to check.
     * @template O - The initial output value (default is true).
     *
     * @example
     * type T0 = List._$every<$<Conditional.Extends, number>, [1, 2, 3]> // true
     * type T1 = List._$every<$<Conditional.Extends, number>, [1, 2, 3, 'x']> // false
     */
    export type _$every<
      F extends Kind.Kind<(x: never) => boolean>,
      T extends unknown[],
      O extends boolean = true
    > = T extends [infer Head, ...infer Rest]
      ? _$every<
          F,
          Rest,
          Boolean._$and<O, $<F, Type._$cast<Head, Kind._$inputOf<F>>>>
        >
      : O
    
    /**
     * `List.Every_T` is an intermediate interface for currying.
     *
     * @template F - The predicate function.
     */
    interface Every_T<F extends Kind.Kind<(x: never) => boolean>>
      extends Kind.Kind {
      f(x: Type._$cast<this[Kind._], Kind._$inputOf<F>[]>): _$every<F, typeof x>
    }
    
    /**
     * `List.Every` is a type-level function that checks if every element in a tuple satisfies a predicate.
     *
     * @template F - The predicate function.
     *
     * @example
     * type T0 = $<$<List.Every, $<Conditional.Extends, number>>, [1, 2, 3]> // true
     * type T1 = $<$<List.Every, $<Conditional.Extends, number>>, [1, 2, 3, 'x']> // false
     */
    export interface Every extends Kind.Kind {
      f(
        x: Type._$cast<this[Kind._], Kind.Kind<(x: never) => boolean>>
      ): Every_T<typeof x>
    }
    >>>>>>> updated
    </source>

--- src/list/every.ts
+++ src/list/every.ts
@@ -1,5 +1,16 @@
 import { $, Boolean, Type, Kind } from '..'
 
+/**
+ * `List._$every` is a type-level function that checks if every element in a tuple satisfies a predicate.
+ * 
+ * @template F - The predicate function.
+ * @template T - The tuple to check.
+ * @template O - The initial output value (default is true).
+ * 
+ * @example
+ * type T0 = List._$every<$<Conditional.Extends, number>, [1, 2, 3]> // true
+ * type T1 = List._$every<$<Conditional.Extends, number>, [1, 2, 3, 'x']> // false
+ */
 export type _$every<
   F extends Kind.Kind<(x: never) => boolean>,
   T extends unknown[],
@@ -12,11 +23,25 @@ export type _$every<
     >
   : O
 
+/**
+ * `List.Every_T` is an intermediate interface for currying.
+ * 
+ * @template F - The predicate function.
+ */
 interface Every_T<F extends Kind.Kind<(x: never) => boolean>>
   extends Kind.Kind {
   f(x: Type._$cast<this[Kind._], Kind._$inputOf<F>[]>): _$every<F, typeof x>
 }
 
+/**
+ * `List.Every` is a type-level function that checks if every element in a tuple satisfies a predicate.
+ * 
+ * @template F - The predicate function.
+ * 
+ * @example
+ * type T0 = $<$<List.Every, $<Conditional.Extends, number>>, [1, 2, 3]> // true
+ * type T1 = $<$<List.Every, $<Conditional.Extends, number>>, [1, 2, 3, 'x']> // false
+ */
 export interface Every extends Kind.Kind {
   f(
     x: Type._$cast<this[Kind._], Kind.Kind<(x: never) => boolean>>
