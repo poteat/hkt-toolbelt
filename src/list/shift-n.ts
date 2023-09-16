c1bc2da37eee573a68c7b5c50528de16296ad18e
Jongsun Suh <34228073+MajorLift@users.noreply.github.com>
2023-09-15 18:54:47 -0700

    aider: Added JSDoc comments to `src/list/shift-n.ts` per style-guide/jsdoc.md.
    # Aider chat conversation:
    
    USER: "Can we write JSDoc for src/list/shift-n.ts per style-guide/jsdoc.md? The spec file src/list/shift-n.spec.ts may be useful."
    
    ASSISTANT: Sure, we can add JSDoc comments to the `src/list/shift-n.ts` file. We will follow the guidelines provided in `style-guide/jsdoc.md`. The `src/list/shift-n.spec.ts` file will be useful for providing examples in the JSDoc comments.
    
    Here are the changes we need to make:
    
    1. Add JSDoc comments to the `_$shiftN2` type utility.
    2. Add JSDoc comments to the `_$shiftN` type utility.
    3. Add JSDoc comments to the `ShiftN_T` interface.
    4. Add JSDoc comments to the `ShiftN` interface.
    
    Here are the proposed changes:
    
    <source>typescript
    src/list/shift-n.ts
    <<<<<<< HEAD
    type _$shiftN2<
      T extends unknown[],
      N extends DigitList.DigitList,
      RESULT extends List.List = T extends [unknown, ...infer Tail]
        ? N extends [Digit.Zero]
          ? T
          : _$shiftN2<Tail, DigitList._$decrement<N>>
        : []
    > = RESULT
    =======
    /**
     * `_$shiftN2` is a type-level function that shifts N elements from the head of an array.
     *
     * @template T - The array to shift elements from.
     * @template N - The number of elements to shift.
     *
     * @example
     * type T0 = _$shiftN2<['a', 'b', 'c'], 1> // ['b', 'c']
     * type T1 = _$shiftN2<['a', 'b', 'c'], 2> // ['c']
     */
    type _$shiftN2<
      T extends unknown[],
      N extends DigitList.DigitList,
      RESULT extends List.List = T extends [unknown, ...infer Tail]
        ? N extends [Digit.Zero]
          ? T
          : _$shiftN2<Tail, DigitList._$decrement<N>>
        : []
    > = RESULT
    >>>>>>> updated
    </source>
    
    <source>typescript
    src/list/shift-n.ts
    <<<<<<< HEAD
    export type _$shiftN<
      T extends unknown[],
      N extends Number.Number,
      RESULT extends List.List = Number._$isNatural<N> extends true
        ? _$shiftN2<T, NaturalNumber._$toList<N>>
        : never
    > = RESULT
    =======
    /**
     * `_$shiftN` is a type-level function that shifts N elements from the head of an array.
     *
     * @template T - The array to shift elements from.
     * @template N - The number of elements to shift.
     *
     * @example
     * type T0 = _$shiftN<['a', 'b', 'c'], 1> // ['b', 'c']
     * type T1 = _$shiftN<['a', 'b', 'c'], 2> // ['c']
     */
    export type _$shiftN<
      T extends unknown[],
      N extends Number.Number,
      RESULT extends List.List = Number._$isNatural<N> extends true
        ? _$shiftN2<T, NaturalNumber._$toList<N>>
        : never
    > = RESULT
    >>>>>>> updated
    </source>
    
    <source>typescript
    src/list/shift-n.ts
    <<<<<<< HEAD
    interface ShiftN_T<N extends Number.Number> extends Kind.Kind {
      f(x: Type._$cast<this[Kind._], unknown[]>): _$shiftN<typeof x, N>
    }
    =======
    /**
     * `ShiftN_T` is an intermediate interface for currying.
     *
     * @template N - The number of elements to shift.
     */
    interface ShiftN_T<N extends Number.Number> extends Kind.Kind {
      f(x: Type._$cast<this[Kind._], unknown[]>): _$shiftN<typeof x, N>
    }
    >>>>>>> updated
    </source>
    
    <source>typescript
    src/list/shift-n.ts
    <<<<<<< HEAD
    export interface ShiftN extends Kind.Kind {
      f(x: Type._$cast<this[Kind._], Number.Number>): ShiftN_T<typeof x>
    }
    =======
    /**
     * `ShiftN` is a type-level function that shifts N elements from the head of an array.
     *
     * @template N - The number of elements to shift.
     * @template T - The array to shift elements from.
     *
     * @example
     * type T0 = $<ShiftN, 1, ['a', 'b', 'c']> // ['b', 'c']
     * type T1 = $<ShiftN, 2, ['a', 'b', 'c']> // ['c']
     */
    export interface ShiftN extends Kind.Kind {
      f(x: Type._$cast<this[Kind._], Number.Number>): ShiftN_T<typeof x>
    }
    >>>>>>> updated
    </source>

--- src/list/shift-n.ts
+++ src/list/shift-n.ts
@@ -1,5 +1,15 @@
 import { Type, Kind, Number, NaturalNumber, DigitList, Digit, List } from '..'
 
+/**
+ * `_$shiftN2` is a type-level function that shifts N elements from the head of an array.
+ * 
+ * @template T - The array to shift elements from.
+ * @template N - The number of elements to shift.
+ * 
+ * @example
+ * type T0 = _$shiftN2<['a', 'b', 'c'], 1> // ['b', 'c']
+ * type T1 = _$shiftN2<['a', 'b', 'c'], 2> // ['c']
+ */
 type _$shiftN2<
   T extends unknown[],
   N extends DigitList.DigitList,
@@ -10,6 +20,16 @@ type _$shiftN2<
     : []
 > = RESULT
 
+/**
+ * `_$shiftN` is a type-level function that shifts N elements from the head of an array.
+ * 
+ * @template T - The array to shift elements from.
+ * @template N - The number of elements to shift.
+ * 
+ * @example
+ * type T0 = _$shiftN<['a', 'b', 'c'], 1> // ['b', 'c']
+ * type T1 = _$shiftN<['a', 'b', 'c'], 2> // ['c']
+ */
 export type _$shiftN<
   T extends unknown[],
   N extends Number.Number,
@@ -18,10 +38,25 @@ export type _$shiftN<
     : never
 > = RESULT
 
+/**
+ * `ShiftN_T` is an intermediate interface for currying.
+ * 
+ * @template N - The number of elements to shift.
+ */
 interface ShiftN_T<N extends Number.Number> extends Kind.Kind {
   f(x: Type._$cast<this[Kind._], unknown[]>): _$shiftN<typeof x, N>
 }
 
+/**
+ * `ShiftN` is a type-level function that shifts N elements from the head of an array.
+ * 
+ * @template N - The number of elements to shift.
+ * @template T - The array to shift elements from.
+ * 
+ * @example
+ * type T0 = $<ShiftN, 1, ['a', 'b', 'c']> // ['b', 'c']
+ * type T1 = $<ShiftN, 2, ['a', 'b', 'c']> // ['c']
+ */
 export interface ShiftN extends Kind.Kind {
   f(x: Type._$cast<this[Kind._], Number.Number>): ShiftN_T<typeof x>
 }
