49cae826b8b372a1929c0875e7286a8ee9f9945e
Jongsun Suh <34228073+MajorLift@users.noreply.github.com>
2023-09-15 18:45:45 -0700

    aider: Added JSDoc comments to `src/list/unshift.ts` per style-guide/jsdoc.md.
    # Aider chat conversation:
    
    USER: "Can we write JSDoc for src/list/unshift.ts per style-guide/jsdoc.md? The spec file src/list/unshift.spec.ts may be useful."
    
    ASSISTANT: Sure, we can add JSDoc comments to the `src/list/unshift.ts` file. We will follow the guidelines provided in `style-guide/jsdoc.md`.
    
    The JSDoc comments will provide a brief description of the type and interface, the template parameters they use, and examples of their usage. The examples will be based on the tests in `src/list/unshift.spec.ts`.
    
    Here are the changes:
    
    <source>typescript
    src/list/unshift.ts
    <<<<<<< HEAD
    import { Type, Kind } from '..'
    
    export type _$unshift<X, T extends unknown[]> = [X, ...T]
    
    interface Unshift_T<X> extends Kind.Kind {
      f(x: Type._$cast<this[Kind._], unknown[]>): _$unshift<X, typeof x>
    }
    
    export interface Unshift extends Kind.Kind {
      f(x: this[Kind._]): Unshift_T<typeof x>
    }
    =======
    import { Type, Kind } from '..'
    
    /**
     * `List._$unshift` is a type-level function that prepends an item to a list.
     *
     * @template X - The item to prepend.
     * @template T - The list to prepend to.
     *
     * @example
     * type T0 = List._$unshift<1, [2, 3, 4]> // [1, 2, 3, 4]
     */
    export type _$unshift<X, T extends unknown[]> = [X, ...T]
    
    /**
     * `List.Unshift_T` is an intermediate interface for currying.
     *
     * @template X - The item to prepend.
     */
    interface Unshift_T<X> extends Kind.Kind {
      f(x: Type._$cast<this[Kind._], unknown[]>): _$unshift<X, typeof x>
    }
    
    /**
     * `List.Unshift` is a type-level function that prepends an item to a list.
     *
     * @template X - The item to prepend.
     * @template T - The list to prepend to.
     *
     * @example
     * type T0 = $<$<List.Unshift, 1>, [2, 3, 4]> // [1, 2, 3, 4]
     */
    export interface Unshift extends Kind.Kind {
      f(x: this[Kind._]): Unshift_T<typeof x>
    }
    >>>>>>> updated
    </source>

--- src/list/unshift.ts
+++ src/list/unshift.ts
@@ -1,11 +1,34 @@
 import { Type, Kind } from '..'
 
+/**
+ * `List._$unshift` is a type-level function that prepends an item to a list.
+ * 
+ * @template X - The item to prepend.
+ * @template T - The list to prepend to.
+ * 
+ * @example
+ * type T0 = List._$unshift<1, [2, 3, 4]> // [1, 2, 3, 4]
+ */
 export type _$unshift<X, T extends unknown[]> = [X, ...T]
 
+/**
+ * `List.Unshift_T` is an intermediate interface for currying.
+ * 
+ * @template X - The item to prepend.
+ */
 interface Unshift_T<X> extends Kind.Kind {
   f(x: Type._$cast<this[Kind._], unknown[]>): _$unshift<X, typeof x>
 }
 
+/**
+ * `List.Unshift` is a type-level function that prepends an item to a list.
+ * 
+ * @template X - The item to prepend.
+ * @template T - The list to prepend to.
+ * 
+ * @example
+ * type T0 = $<$<List.Unshift, 1>, [2, 3, 4]> // [1, 2, 3, 4]
+ */
 export interface Unshift extends Kind.Kind {
   f(x: this[Kind._]): Unshift_T<typeof x>
 }
