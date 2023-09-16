0ad46b33217b0dc92828fc51ffaa3954cdf22a19
Jongsun Suh <34228073+MajorLift@users.noreply.github.com>
2023-09-15 19:06:39 -0700

    aider: Added JSDoc comments to `src/list/pop-n.ts` per style-guide/jsdoc.md.
    # Aider chat conversation:
    
    USER: "Can we write JSDoc for src/list/pop-n.ts per style-guide/jsdoc.md? The spec file src/list/pop-n.spec.ts may be useful."
    
    ASSISTANT: Sure, we can add JSDoc comments to the `src/list/pop-n.ts` file. We will follow the guidelines provided in `style-guide/jsdoc.md`. The `src/list/pop-n.spec.ts` file will be useful to understand the functionality and usage of the code in `src/list/pop-n.ts`.
    
    Here are the changes we need to make:
    
    1. Add JSDoc comments to the `_$popN2` type.
    2. Add JSDoc comments to the `_$popN` type.
    3. Add JSDoc comments to the `PopN_T` interface.
    4. Add JSDoc comments to the `PopN` interface.
    
    Here are the proposed changes:
    
    <source>typescript
    src/list/pop-n.ts
    <<<<<<< HEAD
    type _$popN2<
      T extends unknown[],
      N extends DigitList.DigitList,
      RESULT extends List.List = T extends [...infer Head, unknown]
        ? N extends [Digit.Zero]
          ? T
          : _$popN2<Head, DigitList._$decrement<N>>
        : []
    > = RESULT
    =======
    /**
     * `List._$popN2` is a type-level function that pops N elements from a list.
     *
     * @template T - The list to pop elements from.
     * @template N - The number of elements to pop.
     * @template RESULT - The resulting list after popping N elements.
     *
     * @example
     * type T0 = List._$popN2<['a', 'b', 'c'], [1]> // ['a', 'b']
     */
    type _$popN2<
      T extends unknown[],
      N extends DigitList.DigitList,
      RESULT extends List.List = T extends [...infer Head, unknown]
        ? N extends [Digit.Zero]
          ? T
          : _$popN2<Head, DigitList._$decrement<N>>
        : []
    > = RESULT
    >>>>>>> updated
    </source>
    
    <source>typescript
    src/list/pop-n.ts
    <<<<<<< HEAD
    export type _$popN<
      T extends unknown[],
      N extends Number.Number,
      RESULT extends List.List = Number._$isNatural<N> extends true
        ? _$popN2<T, NaturalNumber._$toList<N>>
        : never
    > = RESULT
    =======
    /**
     * `List._$popN` is a type-level function that pops N elements from a list.
     *
     * @template T - The list to pop elements from.
     * @template N - The number of elements to pop.
     * @template RESULT - The resulting list after popping N elements.
     *
     * @example
     * type T0 = List._$popN<['a', 'b', 'c'], 1> // ['a', 'b']
     */
    export type _$popN<
      T extends unknown[],
      N extends Number.Number,
      RESULT extends List.List = Number._$isNatural<N> extends true
        ? _$popN2<T, NaturalNumber._$toList<N>>
        : never
    > = RESULT
    >>>>>>> updated
    </source>
    
    <source>typescript
    src/list/pop-n.ts
    <<<<<<< HEAD
    interface PopN_T<N extends Number.Number> extends Kind.Kind {
      f(x: Type._$cast<this[Kind._], unknown[]>): _$popN<typeof x, N>
    }
    =======
    /**
     * `List.PopN_T` is an intermediate interface for currying.
     *
     * @template N - The number of elements to pop.
     */
    interface PopN_T<N extends Number.Number> extends Kind.Kind {
      f(x: Type._$cast<this[Kind._], unknown[]>): _$popN<typeof x, N>
    }
    >>>>>>> updated
    </source>
    
    <source>typescript
    src/list/pop-n.ts
    <<<<<<< HEAD
    export interface PopN extends Kind.Kind {
      f(x: Type._$cast<this[Kind._], Number.Number>): PopN_T<typeof x>
    }
    =======
    /**
     * `List.PopN` is a type-level function that pops N elements from a list.
     *
     * @template N - The number of elements to pop.
     *
     * @example
     * type T0 = $<$<List.PopN, 1>, ['a', 'b', 'c']> // ['a', 'b']
     */
    export interface PopN extends Kind.Kind {
      f(x: Type._$cast<this[Kind._], Number.Number>): PopN_T<typeof x>
    }
    >>>>>>> updated
    </source>

--- src/list/pop-n.ts
+++ src/list/pop-n.ts
@@ -1,5 +1,15 @@
 import { Type, Kind, Number, NaturalNumber, DigitList, List, Digit } from '..'
 
+/**
+ * `List._$popN2` is a type-level function that pops N elements from a list.
+ * 
+ * @template T - The list to pop elements from.
+ * @template N - The number of elements to pop.
+ * @template RESULT - The resulting list after popping N elements.
+ * 
+ * @example
+ * type T0 = List._$popN2<['a', 'b', 'c'], [1]> // ['a', 'b']
+ */
 type _$popN2<
   T extends unknown[],
   N extends DigitList.DigitList,
@@ -10,6 +20,16 @@ type _$popN2<
     : []
 > = RESULT
 
+/**
+ * `List._$popN` is a type-level function that pops N elements from a list.
+ * 
+ * @template T - The list to pop elements from.
+ * @template N - The number of elements to pop.
+ * @template RESULT - The resulting list after popping N elements.
+ * 
+ * @example
+ * type T0 = List._$popN<['a', 'b', 'c'], 1> // ['a', 'b']
+ */
 export type _$popN<
   T extends unknown[],
   N extends Number.Number,
@@ -18,10 +38,23 @@ export type _$popN<
     : never
 > = RESULT
 
+/**
+ * `List.PopN_T` is an intermediate interface for currying.
+ * 
+ * @template N - The number of elements to pop.
+ */
 interface PopN_T<N extends Number.Number> extends Kind.Kind {
   f(x: Type._$cast<this[Kind._], unknown[]>): _$popN<typeof x, N>
 }
 
+/**
+ * `List.PopN` is a type-level function that pops N elements from a list.
+ * 
+ * @template N - The number of elements to pop.
+ * 
+ * @example
+ * type T0 = $<$<List.PopN, 1>, ['a', 'b', 'c']> // ['a', 'b']
+ */
 export interface PopN extends Kind.Kind {
   f(x: Type._$cast<this[Kind._], Number.Number>): PopN_T<typeof x>
 }
