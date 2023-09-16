78a77eaa92dbf8f69517e72d2289f715f0b23f63
Jongsun Suh <34228073+MajorLift@users.noreply.github.com>
2023-09-15 20:39:43 -0700

    aider: Added JSDoc comments to `src/list/first.ts` per style-guide/jsdoc.md.
    # Aider chat conversation:
    
    USER: "Can we write JSDoc for src/list/first.ts per style-guide/jsdoc.md? The spec file src/list/first.spec.ts may be useful."
    
    ASSISTANT: Sure, we can add JSDoc comments to the `src/list/first.ts` file. We will need to modify the following file:
    
    - `src/list/first.ts`
    
    Here are the steps we will follow:
    
    1. Add a JSDoc comment to the `_$first` type utility. This comment will explain what the utility does, its template parameter, and provide an example of its usage.
    2. Add a JSDoc comment to the `First` interface. This comment will be similar to the one for `_$first`, but will demonstrate usage with the `$` syntax.
    
    Here are the proposed changes:
    
    <source>typescript
    src/list/first.ts
    <<<<<<< HEAD
    import { Type, Kind } from '..'
    
    export type _$first<T extends unknown[]> = T extends [] ? never : T[0]
    
    export interface First extends Kind.Kind {
      f(x: Type._$cast<this[Kind._], unknown[]>): _$first<typeof x>
    }
    =======
    import { Type, Kind } from '..'
    
    /**
     * `List.First` is a type-level function that returns the first element of a tuple.
     *
     * @template T - The tuple to get the first element of.
     *
     * @example
     * type T0 = List._$first<[1, 2, 3]> // 1
     * type T1 = List._$first<[]> // never
     */
    export type _$first<T extends unknown[]> = T extends [] ? never : T[0]
    
    /**
     * `List.First` is a type-level function that returns the first element of a tuple.
     *
     * @template T - The tuple to get the first element of.
     *
     * @example
     * type T0 = $<List.First, [1, 2, 3]> // 1
     * type T1 = $<List.First, []> // never
     */
    export interface First extends Kind.Kind {
      f(x: Type._$cast<this[Kind._], unknown[]>): _$first<typeof x>
    }
    >>>>>>> updated
    </source>

--- src/list/first.ts
+++ src/list/first.ts
@@ -1,7 +1,25 @@
 import { Type, Kind } from '..'
 
+/**
+ * `List.First` is a type-level function that returns the first element of a tuple.
+ * 
+ * @template T - The tuple to get the first element of.
+ * 
+ * @example
+ * type T0 = List._$first<[1, 2, 3]> // 1
+ * type T1 = List._$first<[]> // never
+ */
 export type _$first<T extends unknown[]> = T extends [] ? never : T[0]
 
+/**
+ * `List.First` is a type-level function that returns the first element of a tuple.
+ * 
+ * @template T - The tuple to get the first element of.
+ * 
+ * @example
+ * type T0 = $<List.First, [1, 2, 3]> // 1
+ * type T1 = $<List.First, []> // never
+ */
 export interface First extends Kind.Kind {
   f(x: Type._$cast<this[Kind._], unknown[]>): _$first<typeof x>
 }
