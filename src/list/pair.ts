2391442396a2fb941cb9a18d70998d5aad183f93
Jongsun Suh <34228073+MajorLift@users.noreply.github.com>
2023-09-15 19:07:23 -0700

    aider: Added JSDoc comments to `src/list/pair.ts` per style-guide/jsdoc.md.
    # Aider chat conversation:
    
    USER: "Can we write JSDoc for src/list/pair.ts per style-guide/jsdoc.md? The spec file src/list/pair.spec.ts may be useful."
    
    ASSISTANT: Sure, we can add JSDoc comments to the `src/list/pair.ts` file. We will follow the guidelines provided in `style-guide/jsdoc.md`. The `src/list/pair.spec.ts` file will be useful to understand the functionality of the code and to provide relevant examples.
    
    Here are the changes we need to make:
    
    1. Add JSDoc comments to the `_$pair` type utility.
    2. Add JSDoc comments to the `Pair` interface.
    
    Here are the proposed changes:
    
    <source>typescript
    src/list/pair.ts
    <<<<<<< HEAD
    import { Type, Kind } from '..'
    
    export type _$pair<
      T extends unknown[],
      O extends unknown[][] = []
    > = T extends [infer X1, infer X2, ...infer Rest]
      ? _$pair<[X2, ...Rest], [...O, [X1, X2]]>
      : number extends T['length']
      ? [T[number], T[number]][]
      : O
    
    export interface Pair extends Kind.Kind {
      f(x: Type._$cast<this[Kind._], unknown[]>): _$pair<typeof x>
    }
    =======
    import { Type, Kind } from '..'
    
    /**
     * `List.Pair` is a type-level function that generates a tuple of pairs from a tuple,
     * where each element is paired with the next element.
     *
     * @template T - The tuple to generate pairs from.
     * @template O - The output tuple of pairs.
     *
     * @example
     * type T0 = List._$pair<[1, 2, 3, 4]> // [[1, 2], [2, 3], [3, 4]]
     * type T1 = List._$pair<[]> // []
     * type T2 = List._$pair<[1]> // []
     * type T3 = List._$pair<[1, 2]> // [[1, 2]]
     */
    export type _$pair<
      T extends unknown[],
      O extends unknown[][] = []
    > = T extends [infer X1, infer X2, ...infer Rest]
      ? _$pair<[X2, ...Rest], [...O, [X1, X2]]>
      : number extends T['length']
      ? [T[number], T[number]][]
      : O
    
    /**
     * `List.Pair` is a type-level function that generates a tuple of pairs from a tuple,
     * where each element is paired with the next element.
     *
     * @template T - The tuple to generate pairs from.
     *
     * @example
     * type T0 = $<List.Pair, [1, 2, 3, 4]> // [[1, 2], [2, 3], [3, 4]]
     * type T1 = $<List.Pair, []> // []
     * type T2 = $<List.Pair, [1]> // []
     * type T3 = $<List.Pair, [1, 2]> // [[1, 2]]
     */
    export interface Pair extends Kind.Kind {
      f(x: Type._$cast<this[Kind._], unknown[]>): _$pair<typeof x>
    }
    >>>>>>> updated
    </source>

--- src/list/pair.ts
+++ src/list/pair.ts
@@ -1,5 +1,18 @@
 import { Type, Kind } from '..'
 
+/**
+ * `List.Pair` is a type-level function that generates a tuple of pairs from a tuple, 
+ * where each element is paired with the next element.
+ * 
+ * @template T - The tuple to generate pairs from.
+ * @template O - The output tuple of pairs.
+ * 
+ * @example
+ * type T0 = List._$pair<[1, 2, 3, 4]> // [[1, 2], [2, 3], [3, 4]]
+ * type T1 = List._$pair<[]> // []
+ * type T2 = List._$pair<[1]> // []
+ * type T3 = List._$pair<[1, 2]> // [[1, 2]]
+ */
 export type _$pair<
   T extends unknown[],
   O extends unknown[][] = []
@@ -9,6 +22,18 @@ export type _$pair<
   ? [T[number], T[number]][]
   : O
 
+/**
+ * `List.Pair` is a type-level function that generates a tuple of pairs from a tuple, 
+ * where each element is paired with the next element.
+ * 
+ * @template T - The tuple to generate pairs from.
+ * 
+ * @example
+ * type T0 = $<List.Pair, [1, 2, 3, 4]> // [[1, 2], [2, 3], [3, 4]]
+ * type T1 = $<List.Pair, []> // []
+ * type T2 = $<List.Pair, [1]> // []
+ * type T3 = $<List.Pair, [1, 2]> // [[1, 2]]
+ */
 export interface Pair extends Kind.Kind {
   f(x: Type._$cast<this[Kind._], unknown[]>): _$pair<typeof x>
 }
