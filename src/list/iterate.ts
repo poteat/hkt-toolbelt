23c8faa0f137326675936c9fbf169bb7a7237e95
Jongsun Suh <34228073+MajorLift@users.noreply.github.com>
2023-09-15 20:42:10 -0700

    aider: Added JSDoc comments to `src/list/iterate.ts` per style-guide/jsdoc.md.
    # Aider chat conversation:
    
    USER: "Can we write JSDoc for src/list/iterate.ts per style-guide/jsdoc.md? The spec file src/list/iterate.spec.ts may be useful."
    
    ASSISTANT: Sure, we can add JSDoc comments to the `src/list/iterate.ts` file.
    
    We will need to modify the following file:
    - `src/list/iterate.ts`
    
    Here are the steps we will follow:
    
    1. Add a JSDoc comment to the `_$iterate` type. This comment will explain what the type does, its template parameters, and provide an example of its usage. We will use the examples from the `src/list/iterate.spec.ts` file for this.
    
    2. Add a JSDoc comment to the `Iterate_T2` interface. This comment will explain what the interface does and its template parameters.
    
    3. Add a JSDoc comment to the `Iterate_T` interface. This comment will explain what the interface does and its template parameters.
    
    4. Add a JSDoc comment to the `Iterate` interface. This comment will explain what the interface does and its template parameters.
    
    Here are the proposed changes:
    
    <source>typescript
    src/list/iterate.ts
    <<<<<<< HEAD
    export type _$iterate<
      F extends Kind.Kind,
      O extends Kind._$inputOf<F>,
      N extends Number.Number,
      COUNT extends Number.Number = NaturalNumber._$decrement<N>,
      M extends Kind._$inputOf<F>[] = [O],
      CURR extends Kind._$inputOf<F> = O,
      ACC = $<F, CURR>,
      RESULT extends Kind._$inputOf<F>[] = COUNT extends 0
        ? M
        : ACC extends Kind._$inputOf<F>
        ? _$iterate<
            F,
            O,
            N,
            NaturalNumber._$decrement<COUNT>,
            List._$push<ACC, M>,
            ACC
          >
        : never
    > = 0 extends 1 ? never : RESULT
    
    interface Iterate_T2<F extends Kind.Kind, N extends Number.Number>
      extends Kind.Kind {
      f(x: Type._$cast<this[Kind._], Kind._$inputOf<F>>): _$iterate<F, typeof x, N>
    }
    
    interface Iterate_T<F extends Kind.Kind> extends Kind.Kind {
      f(x: Type._$cast<this[Kind._], Number.Number>): Iterate_T2<F, typeof x>
    }
    
    export interface Iterate extends Kind.Kind {
      f(x: Type._$cast<this[Kind._], Kind.Kind>): Iterate_T<typeof x>
    }
    =======
    /**
     * `List._$iterate` is a type-level function that iterates over a list.
     *
     * @template F - The function to iterate with.
     * @template O - The initial input to the function.
     * @template N - The number of times to iterate.
     *
     * @example
     * type T0 = List._$iterate<$<Boolean.Xor, true>, true, 5> // [true, false, true, false, true]
     */
    export type _$iterate<
      F extends Kind.Kind,
      O extends Kind._$inputOf<F>,
      N extends Number.Number,
      COUNT extends Number.Number = NaturalNumber._$decrement<N>,
      M extends Kind._$inputOf<F>[] = [O],
      CURR extends Kind._$inputOf<F> = O,
      ACC = $<F, CURR>,
      RESULT extends Kind._$inputOf<F>[] = COUNT extends 0
        ? M
        : ACC extends Kind._$inputOf<F>
        ? _$iterate<
            F,
            O,
            N,
            NaturalNumber._$decrement<COUNT>,
            List._$push<ACC, M>,
            ACC
          >
        : never
    > = 0 extends 1 ? never : RESULT
    
    /**
     * `List.Iterate_T2` is an intermediate interface for currying.
     *
     * @template F - The function to iterate with.
     * @template N - The number of times to iterate.
     */
    interface Iterate_T2<F extends Kind.Kind, N extends Number.Number>
      extends Kind.Kind {
      f(x: Type._$cast<this[Kind._], Kind._$inputOf<F>>): _$iterate<F, typeof x, N>
    }
    
    /**
     * `List.Iterate_T` is an intermediate interface for currying.
     *
     * @template F - The function to iterate with.
     */
    interface Iterate_T<F extends Kind.Kind> extends Kind.Kind {
      f(x: Type._$cast<this[Kind._], Number.Number>): Iterate_T2<F, typeof x>
    }
    
    /**
     * `List.Iterate` is a type-level function that iterates over a list.
     *
     * @example
     * type T0 = $<$<$<List.Iterate, $<Boolean.Xor, true>>, 5>, true> // [true, false, true, false, true]
     */
    export interface Iterate extends Kind.Kind {
      f(x: Type._$cast<this[Kind._], Kind.Kind>): Iterate_T<typeof x>
    }
    >>>>>>> updated
    </source>

--- src/list/iterate.ts
+++ src/list/iterate.ts
@@ -1,5 +1,15 @@
 import { $, Type, Kind, Number, List, NaturalNumber } from '..'
 
+/**
+ * `List._$iterate` is a type-level function that iterates over a list.
+ * 
+ * @template F - The function to iterate with.
+ * @template O - The initial input to the function.
+ * @template N - The number of times to iterate.
+ * 
+ * @example
+ * type T0 = List._$iterate<$<Boolean.Xor, true>, true, 5> // [true, false, true, false, true]
+ */
 export type _$iterate<
   F extends Kind.Kind,
   O extends Kind._$inputOf<F>,
@@ -22,15 +32,32 @@ export type _$iterate<
     : never
 > = 0 extends 1 ? never : RESULT
 
+/**
+ * `List.Iterate_T2` is an intermediate interface for currying.
+ * 
+ * @template F - The function to iterate with.
+ * @template N - The number of times to iterate.
+ */
 interface Iterate_T2<F extends Kind.Kind, N extends Number.Number>
   extends Kind.Kind {
   f(x: Type._$cast<this[Kind._], Kind._$inputOf<F>>): _$iterate<F, typeof x, N>
 }
 
+/**
+ * `List.Iterate_T` is an intermediate interface for currying.
+ * 
+ * @template F - The function to iterate with.
+ */
 interface Iterate_T<F extends Kind.Kind> extends Kind.Kind {
   f(x: Type._$cast<this[Kind._], Number.Number>): Iterate_T2<F, typeof x>
 }
 
+/**
+ * `List.Iterate` is a type-level function that iterates over a list.
+ * 
+ * @example
+ * type T0 = $<$<$<List.Iterate, $<Boolean.Xor, true>>, 5>, true> // [true, false, true, false, true]
+ */
 export interface Iterate extends Kind.Kind {
   f(x: Type._$cast<this[Kind._], Kind.Kind>): Iterate_T<typeof x>
 }
