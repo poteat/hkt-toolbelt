5c8ac537f894099ebac2094079488d83a9dadf1a
Jongsun Suh <34228073+MajorLift@users.noreply.github.com>
2023-09-15 20:43:23 -0700

    aider: Added JSDoc comments to `src/list/length.ts` per style-guide/jsdoc.md.
    # Aider chat conversation:
    
    USER: "Can we write JSDoc for src/list/length.ts per style-guide/jsdoc.md? The spec file src/list/length.spec.ts may be useful."
    
    ASSISTANT: Sure, we can add JSDoc comments to the `src/list/length.ts` file. We will follow the guidelines provided in `style-guide/jsdoc.md`.
    
    Here are the steps we will follow:
    
    1. Add a JSDoc comment to the `_$length` type. This comment will explain what the type does, provide a template parameter description, and include an example of usage.
    2. Add a JSDoc comment to the `Length` interface. This comment will also explain what the interface does, provide a template parameter description, and include an example of usage.
    
    Here are the proposed changes:
    
    <source>typescript
    src/list/length.ts
    <<<<<<< HEAD
    import { Type, Kind } from '..'
    
    export type _$length<T extends unknown[]> = T['length']
    
    export interface Length extends Kind.Kind {
      f(x: Type._$cast<this[Kind._], unknown[]>): _$length<typeof x>
    }
    =======
    import { Type, Kind } from '..'
    
    /**
     * `List.Length` is a type-level function that returns the length of a list.
     *
     * @template T - The list to get the length of.
     *
     * @example
     * type T0 = List._$length<[1, 2, 3]> // 3
     * type T1 = List._$length<[]> // 0
     */
    export type _$length<T extends unknown[]> = T['length']
    
    /**
     * `List.Length` is a type-level function that returns the length of a list.
     *
     * @template T - The list to get the length of.
     *
     * @example
     * type T0 = $<List.Length, [1, 2, 3]> // 3
     * type T1 = $<List.Length, []> // 0
     */
    export interface Length extends Kind.Kind {
      f(x: Type._$cast<this[Kind._], unknown[]>): _$length<typeof x>
    }
    >>>>>>> updated
    </source>

--- src/list/length.ts
+++ src/list/length.ts
@@ -1,7 +1,25 @@
 import { Type, Kind } from '..'
 
+/**
+ * `List.Length` is a type-level function that returns the length of a list.
+ * 
+ * @template T - The list to get the length of.
+ * 
+ * @example
+ * type T0 = List._$length<[1, 2, 3]> // 3
+ * type T1 = List._$length<[]> // 0
+ */
 export type _$length<T extends unknown[]> = T['length']
 
+/**
+ * `List.Length` is a type-level function that returns the length of a list.
+ * 
+ * @template T - The list to get the length of.
+ * 
+ * @example
+ * type T0 = $<List.Length, [1, 2, 3]> // 3
+ * type T1 = $<List.Length, []> // 0
+ */
 export interface Length extends Kind.Kind {
   f(x: Type._$cast<this[Kind._], unknown[]>): _$length<typeof x>
 }
