c2582c3c98955be6650c7b4c07db52030fee6d0a
Jongsun Suh <34228073+MajorLift@users.noreply.github.com>
2023-09-15 18:57:13 -0700

    aider: Add JSDoc comments to `src/list/reverse.ts` per style-guide/jsdoc.md.
    # Aider chat conversation:
    
    USER: "Can we write JSDoc for src/list/reverse.ts per style-guide/jsdoc.md? The spec file src/list/reverse.spec.ts may be useful."
    
    ASSISTANT: Sure, we can add JSDoc comments to the `src/list/reverse.ts` file.
    
    We will need to modify the following file:
    - `src/list/reverse.ts`
    
    Here are the changes we need to make:
    
    1. Add JSDoc comments to the `_$reverse2` type utility. This utility is not exported, but it's a key part of the implementation of the exported `_$reverse` utility, so it's worth documenting. The comment should explain what the utility does and what its template parameters represent.
    
    2. Add JSDoc comments to the `_$reverse` type utility. This utility is exported, so it's important to document it thoroughly. The comment should explain what the utility does, what its template parameters represent, and provide an example of its usage.
    
    3. Add JSDoc comments to the `Reverse` interface. This interface is also exported, so it should be documented. The comment should explain what the interface does, what its template parameters represent, and provide an example of its usage.
    
    Here are the proposed changes:
    
    <source>typescript
    src/list/reverse.ts
    <<<<<<< HEAD
    import { Type, Kind } from '..'
    
    type _$reverse2<T extends unknown[], O extends unknown[] = []> = T extends [
      ...infer Init,
      infer Last
    ]
      ? Init extends []
        ? [...O, Last]
        : _$reverse2<Init, [...O, Last]>
      : T extends [infer Head, ...unknown[]]
      ? Head
      : [...O, ...T]
    
    export type _$reverse<
      T extends unknown[],
      O extends unknown[] = []
    > = T extends [infer Head, ...infer Tail]
      ? _$reverse<Tail, [Head, ...O]>
      : T extends []
      ? O
      : [..._$reverse2<T>, ...O]
    
    export interface Reverse extends Kind.Kind {
      f(x: Type._$cast<this[Kind._], unknown[]>): _$reverse<typeof x>
    }
    =======
    import { Type, Kind } from '..'
    
    /**
     * `_$reverse2` is an internal utility that reverses a tuple.
     *
     * @template T - The tuple to reverse.
     * @template O - The reversed tuple.
     */
    type _$reverse2<T extends unknown[], O extends unknown[] = []> = T extends [
      ...infer Init,
      infer Last
    ]
      ? Init extends []
        ? [...O, Last]
        : _$reverse2<Init, [...O, Last]>
      : T extends [infer Head, ...unknown[]]
      ? Head
      : [...O, ...T]
    
    /**
     * `_$reverse` is a type-level function that reverses a tuple.
     *
     * @template T - The tuple to reverse.
     * @template O - The reversed tuple.
     *
     * @example
     * type T0 = _$reverse<[1, 2, 3]> // [3, 2, 1]
     */
    export type _$reverse<
      T extends unknown[],
      O extends unknown[] = []
    > = T extends [infer Head, ...infer Tail]
      ? _$reverse<Tail, [Head, ...O]>
      : T extends []
      ? O
      : [..._$reverse2<T>, ...O]
    
    /**
     * `Reverse` is a type-level function that reverses a tuple.
     *
     * @template T - The tuple to reverse.
     *
     * @example
     * type T0 = $<Reverse, [1, 2, 3]> // [3, 2, 1]
     */
    export interface Reverse extends Kind.Kind {
      f(x: Type._$cast<this[Kind._], unknown[]>): _$reverse<typeof x>
    }
    >>>>>>> updated
    </source>

--- src/list/reverse.ts
+++ src/list/reverse.ts
@@ -1,5 +1,11 @@
 import { Type, Kind } from '..'
 
+/**
+ * `_$reverse2` is an internal utility that reverses a tuple.
+ * 
+ * @template T - The tuple to reverse.
+ * @template O - The reversed tuple.
+ */
 type _$reverse2<T extends unknown[], O extends unknown[] = []> = T extends [
   ...infer Init,
   infer Last
@@ -11,6 +17,15 @@ type _$reverse2<T extends unknown[], O extends unknown[] = []> = T extends [
   ? Head
   : [...O, ...T]
 
+/**
+ * `_$reverse` is a type-level function that reverses a tuple.
+ * 
+ * @template T - The tuple to reverse.
+ * @template O - The reversed tuple.
+ * 
+ * @example
+ * type T0 = _$reverse<[1, 2, 3]> // [3, 2, 1]
+ */
 export type _$reverse<
   T extends unknown[],
   O extends unknown[] = []
@@ -20,6 +35,14 @@ export type _$reverse<
   ? O
   : [..._$reverse2<T>, ...O]
 
+/**
+ * `Reverse` is a type-level function that reverses a tuple.
+ * 
+ * @template T - The tuple to reverse.
+ * 
+ * @example
+ * type T0 = $<Reverse, [1, 2, 3]> // [3, 2, 1]
+ */
 export interface Reverse extends Kind.Kind {
   f(x: Type._$cast<this[Kind._], unknown[]>): _$reverse<typeof x>
 }
