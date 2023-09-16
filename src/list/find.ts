92ca118b2282a782d8c1f96b4562ebc67402a186
Jongsun Suh <34228073+MajorLift@users.noreply.github.com>
2023-09-15 20:39:14 -0700

    aider: Added JSDoc comments to `src/list/find.ts` per style-guide/jsdoc.md.
    # Aider chat conversation:
    
    USER: "Can we write JSDoc for src/list/find.ts per style-guide/jsdoc.md? The spec file src/list/find.spec.ts may be useful."
    
    ASSISTANT: Sure, we can add JSDoc comments to the `src/list/find.ts` file.
    
    We will need to modify the following file:
    
    - `src/list/find.ts`
    
    Here are the changes we need to make:
    
    1. Add JSDoc comments to the `_$find` type utility. The comments should explain what the utility does, its template parameters, and provide an example of its usage. We can use the examples from the `src/list/find.spec.ts` file for this.
    
    2. Add JSDoc comments to the `Find_T` interface. The comments should explain what the interface does and its template parameters.
    
    3. Add JSDoc comments to the `Find` interface. The comments should explain what the interface does and provide an example of its usage. We can use the examples from the `src/list/find.spec.ts` file for this.
    
    Here is the *edit block* for these changes:
    
    <source>typescript
    src/list/find.ts
    <<<<<<< HEAD
    import { $, Type, Kind } from '..'
    
    export type _$find<F extends Kind.Kind, X extends unknown[]> = X extends [
      infer Head,
      ...infer Tail
    ]
      ? $<F, Type._$cast<Head, Kind._$inputOf<F>>> extends true
        ? Head
        : _$find<F, Tail>
      : never
    
    interface Find_T<F extends Kind.Kind<(x: never) => boolean>> extends Kind.Kind {
      f(x: Type._$cast<this[Kind._], Kind._$inputOf<F>[]>): _$find<F, typeof x>
    }
    
    export interface Find extends Kind.Kind {
      f(
        x: Type._$cast<this[Kind._], Kind.Kind<(x: never) => boolean>>
      ): Find_T<typeof x>
    }
    =======
    import { $, Type, Kind } from '..'
    
    /**
     * `List._$find` is a type-level function that finds the first element in a list that satisfies a given condition.
     *
     * @template F - The condition function.
     * @template X - The list to search in.
     *
     * @example
     * type T0 = List._$find<$<Conditional.Equals, 3>, [1, 2, 3]> // 3
     * type T1 = List._$find<$<Conditional.Equals, 4>, [1, 2, 3]> // never
     */
    export type _$find<F extends Kind.Kind, X extends unknown[]> = X extends [
      infer Head,
      ...infer Tail
    ]
      ? $<F, Type._$cast<Head, Kind._$inputOf<F>>> extends true
        ? Head
        : _$find<F, Tail>
      : never
    
    /**
     * `List.Find_T` is an intermediate interface for currying.
     *
     * @template F - The condition function.
     */
    interface Find_T<F extends Kind.Kind<(x: never) => boolean>> extends Kind.Kind {
      f(x: Type._$cast<this[Kind._], Kind._$inputOf<F>[]>): _$find<F, typeof x>
    }
    
    /**
     * `List.Find` is a type-level function that finds the first element in a list that satisfies a given condition.
     *
     * @template F - The condition function.
     *
     * @example
     * type T0 = $<$<List.Find, $<Conditional.Equals, 3>>, [1, 2, 3]> // 3
     * type T1 = $<$<List.Find, $<Conditional.Equals, 4>>, [1, 2, 3]> // never
     */
    export interface Find extends Kind.Kind {
      f(
        x: Type._$cast<this[Kind._], Kind.Kind<(x: never) => boolean>>
      ): Find_T<typeof x>
    }
    >>>>>>> updated
    </source>

--- src/list/find.ts
+++ src/list/find.ts
@@ -1,5 +1,15 @@
 import { $, Type, Kind } from '..'
 
+/**
+ * `List._$find` is a type-level function that finds the first element in a list that satisfies a given condition.
+ * 
+ * @template F - The condition function.
+ * @template X - The list to search in.
+ * 
+ * @example
+ * type T0 = List._$find<$<Conditional.Equals, 3>, [1, 2, 3]> // 3
+ * type T1 = List._$find<$<Conditional.Equals, 4>, [1, 2, 3]> // never
+ */
 export type _$find<F extends Kind.Kind, X extends unknown[]> = X extends [
   infer Head,
   ...infer Tail
@@ -9,10 +19,24 @@ export type _$find<F extends Kind.Kind, X extends unknown[]> = X extends [
     : _$find<F, Tail>
   : never
 
+/**
+ * `List.Find_T` is an intermediate interface for currying.
+ * 
+ * @template F - The condition function.
+ */
 interface Find_T<F extends Kind.Kind<(x: never) => boolean>> extends Kind.Kind {
   f(x: Type._$cast<this[Kind._], Kind._$inputOf<F>[]>): _$find<F, typeof x>
 }
 
+/**
+ * `List.Find` is a type-level function that finds the first element in a list that satisfies a given condition.
+ * 
+ * @template F - The condition function.
+ * 
+ * @example
+ * type T0 = $<$<List.Find, $<Conditional.Equals, 3>>, [1, 2, 3]> // 3
+ * type T1 = $<$<List.Find, $<Conditional.Equals, 4>>, [1, 2, 3]> // never
+ */
 export interface Find extends Kind.Kind {
   f(
     x: Type._$cast<this[Kind._], Kind.Kind<(x: never) => boolean>>
