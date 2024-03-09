516f9be4c3e3ee6b1d088edcc1d8c0bfa2d6db13
yahyha benzha <yahya.benzha1@gmail.com>
2024-03-09 17:12:08 +0100

    Boolean Utilities

---
+++ src/stress/ten-boolean.ts
@@ -0,0 +1,38 @@
+export type TenTrueList = [
+  true,
+  true,
+  true,
+  true,
+  true,
+  true,
+  true,
+  true,
+  true,
+  true
+]
+
+export type TenFalseList = [
+  false,
+  false,
+  false,
+  false,
+  false,
+  false,
+  false,
+  false,
+  false,
+  false
+]
+
+export type TenBooleanList = [
+  true,
+  true,
+  false,
+  false,
+  true,
+  true,
+  false,
+  false,
+  true,
+  true
+]
