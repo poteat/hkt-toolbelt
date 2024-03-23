ef4eb66597db641de7fd83cf1a6b7b93f0cec1b8
yahyha benzha <yahya.benzha1@gmail.com>
2024-03-23 18:21:23 +0000

    object utilities

---
+++ src/object/at-path-n.ts
@@ -0,0 +1,46 @@
+import { Kind, Type } from '..'
+import { _$atPath } from './at-path'
+
+export type KeyOrPath = PropertyKey | PropertyKey[]
+
+/**
+ * `_atPathN` is a type-level function that get the nested values in object O at the paths and keys specified in P.
+ *
+ * @template P - The path to the property or properties.
+ * @template O - The type of the object to retrieve values from.
+ * @template Acc - Accumulator type to store intermediate results.
+ * @template Output - The resulting type after retrieving values.
+ */
+export type _$atPathN<
+  P extends KeyOrPath[],
+  O extends Record<PropertyKey, unknown>,
+  Acc extends any[] = [],
+  Output = P extends [
+    infer Head extends KeyOrPath,
+    ...infer Tail extends KeyOrPath[]
+  ]
+    ? _$atPathN<
+        Tail,
+        O,
+        [
+          ...Acc,
+          Head extends PropertyKey[]
+            ? _$atPath<Head, O>
+            : O[Type._$cast<Head, keyof O>]
+        ]
+      >
+    : Acc[number]
+> = Output
+
+interface AtPathN_T<Path extends KeyOrPath[]> extends Kind.Kind {
+  f(
+    x: Type._$cast<this[Kind._], Record<string, unknown>>
+  ): _$atPathN<Path, typeof x>
+}
+
+/**
+ * `AtPathN` is a type-level function that retrieves values from nested properties of an object based on a path.
+ */
+export interface AtPathN extends Kind.Kind {
+  f(x: Type._$cast<this[Kind._], KeyOrPath[]>): AtPathN_T<typeof x>
+}
---
+++ src/object/update-n.ts
@@ -0,0 +1,60 @@
+import { Kind, Type } from '..'
+import { KeyOrPath } from './at-path-n'
+import { _$update } from './update'
+
+/**
+ * `_updateN` is a type-level function that Update multiple nested values in object O
+ *  at paths and keys specified in P, using values specified in V.
+ *
+ * @template P - The paths to the properties.
+ * @template V - The new values for the properties.
+ * @template O - The object to update.
+ */
+export type _$updateN<
+  P extends KeyOrPath[],
+  V extends unknown[],
+  O extends Record<string, unknown>
+> = [[P], [V]] extends [
+  [[infer Head, ...infer Tail extends KeyOrPath[]]],
+  [[infer VHead, ...infer VTail]]
+]
+  ? _$updateN<
+      Tail,
+      VTail,
+      Type._$cast<
+        _$update<
+          Type._$cast<
+            Head extends PropertyKey[] ? Head : [Head],
+            PropertyKey[]
+          >,
+          VHead,
+          O
+        >,
+        Record<PropertyKey, unknown>
+      >
+    >
+  : O
+
+/**
+ * Intermediate interface for currying `_updateN`.
+ */
+interface UpdateN_T2<PATHS extends KeyOrPath[], VALUE extends unknown[]>
+  extends Kind.Kind {
+  f(
+    x: Type._$cast<this[Kind._], Record<PropertyKey, unknown>>
+  ): _$updateN<PATHS, VALUE, typeof x>
+}
+
+/**
+ * Intermediate interface for currying `_updateN`.
+ */
+interface UpdateN_T<PATHS extends KeyOrPath[]> extends Kind.Kind {
+  f(x: Type._$cast<this[Kind._], unknown[]>): UpdateN_T2<PATHS, typeof x>
+}
+
+/**
+ * `UpdateN` is a type-level function that updates nested properties of an object based on multiple paths.
+ */
+export interface UpdateN extends Kind.Kind {
+  f(x: Type._$cast<this[Kind._], KeyOrPath[]>): UpdateN_T<typeof x>
+}
