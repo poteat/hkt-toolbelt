a3c07f5213899706d16d4bf6b85ebe4844e0bd39
Jongsun Suh <jongsun.suh@icloud.com>
2024-03-27 00:01:50 -0400

    Expand jsdoc for `Type.Cast`

--- src/type/cast.ts
+++ src/type/cast.ts
@@ -1,7 +1,8 @@
 import { Kind } from '..'
 
 /**
- * `_$cast` is a type-level function that casts a type `T` to a type `U`.
+ * `_$cast` is a generic type that coercively downcasts a type `T` to a type `U`.
+ * Returns the narrower out of the two types. If the two types are unrelated, returns `U`.
  *
  * @template T - The type to cast.
  * @template U - The type to cast to.
@@ -10,6 +11,7 @@ import { Kind } from '..'
  * type T0 = _$cast<true, true> // true
  * type T1 = _$cast<boolean, true> // true
  * type T2 = _$cast<true, boolean> // true
+ * type T3 = _$cast<boolean, 0> // 0
  */
 export type _$cast<T, U> = [T] extends [U] ? T : U
 
@@ -18,7 +20,8 @@ interface Cast_T<T> extends Kind.Kind {
 }
 
 /**
- * `Cast` is a type-level function that casts a type `T` to a type `U`.
+ * `Cast` is a type-level function that coercively downcasts a type `T` to a type `U`.
+ * Returns the narrower out of the two types. If the two types are unrelated, returns `U`.
  *
  * @template T - The type to cast.
  * @template U - The type to cast to.
@@ -27,6 +30,7 @@ interface Cast_T<T> extends Kind.Kind {
  * type T0 = $<$<Cast, true>, true> // true
  * type T1 = $<$<Cast, boolean>, true> // true
  * type T2 = $<$<Cast, true>, boolean> // true
+ * type T3 = $<$<Cast, boolean>, 0> // 0
  */
 export interface Cast extends Kind.Kind {
   f(x: this[Kind._]): Cast_T<typeof x>
