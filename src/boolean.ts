d9723d74c8c391412585bdb3853d32e38a59b7ee
poteat <poteat@users.noreply.github.com>
2024-08-23 12:33:10 -0700

    docs: provide module-level documentation

--- src/boolean.ts
+++ src/boolean.ts
@@ -1,1 +1,14 @@
 export * from './boolean/'
+
+/**
+ * The `Boolean` module contains various boolean type utilities. These utilities
+ * are used to perform logical operations on boolean types.
+ *
+ * @example
+ * ```ts
+ * import { $, Boolean } from 'hkt-toolbelt'
+ *
+ * type Result = $<Boolean.And<true>, false> // false
+ * ```
+ */
+declare module './boolean' {}
--- src/object.ts
+++ src/object.ts
@@ -1,1 +1,14 @@
 export * from './object/'
+
+/**
+ * The `Object` module contains various utilities for working with
+ * objects, including getting and setting values, merging, etc.
+ *
+ * @example
+ * ```ts
+ * import { $, Object } from 'hkt-toolbelt'
+ *
+ * type Result = $<Object.AtPath, ['name', 'first'], { name: { first: 'foo' } }> // 'foo'
+ * ```
+ */
+declare module './object' {}
