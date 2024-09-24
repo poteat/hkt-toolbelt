79ad92446be9c82880d81df7eb51c7221b419899
poteat <poteat@users.noreply.github.com>
2024-09-24 13:47:24 -0700

    feat: add various sudoku type-level functions

--- src/index.ts
+++ src/index.ts
@@ -1,62 +1,48 @@
-import * as $ from './$'
 export { $, $$, $N } from './$'
-
-import * as Boolean from './boolean'
 export * as Boolean from './boolean'
-
-import * as Combinator from './combinator'
 export * as Combinator from './combinator'
-
-import * as Conditional from './conditional'
 export * as Conditional from './conditional'
-
-import * as DigitList from './digit-list'
-export * as DigitList from './digit-list'
-
-import * as Digit from './digit'
 export * as Digit from './digit'
-
-import * as Function from './function'
+export * as DigitList from './digit-list'
 export * as Function from './function'
-
-import * as Integer from './integer'
 export * as Integer from './integer'
-
-import * as Kind from './kind'
 export * as Kind from './kind'
-
-import * as List from './list'
 export * as List from './list'
-
-import * as NaturalNumberTheory from './natural-number-theory'
-export * as NaturalNumberTheory from './natural-number-theory'
-
-import * as NaturalNumber from './natural-number'
+export * as Loop from './loop'
+export * as Matrix from './matrix'
 export * as NaturalNumber from './natural-number'
-
-import * as Number from './number'
+export * as NaturalNumberTheory from './natural-number-theory'
 export * as Number from './number'
-
-import * as Object from './object'
 export * as Object from './object'
-
-import * as Parser from './parser'
 export * as Parser from './parser'
-
-import * as Stress from './stress'
 export * as Stress from './stress'
-
-import * as String from './string'
 export * as String from './string'
-
-import * as Test from './test'
 export * as Test from './test'
-
-import * as Type from './type'
 export * as Type from './type'
-
-import * as Union from './union'
 export * as Union from './union'
+
+import * as $ from './$'
+import * as Boolean from './boolean'
+import * as Combinator from './combinator'
+import * as Conditional from './conditional'
+import * as Digit from './digit'
+import * as DigitList from './digit-list'
+import * as Function from './function'
+import * as Integer from './integer'
+import * as Kind from './kind'
+import * as List from './list'
+import * as Loop from './loop'
+import * as Matrix from './matrix'
+import * as NaturalNumber from './natural-number'
+import * as NaturalNumberTheory from './natural-number-theory'
+import * as Number from './number'
+import * as Object_ from './object'
+import * as Parser from './parser'
+import * as Stress from './stress'
+import * as String from './string'
+import * as Test from './test'
+import * as Type from './type'
+import * as Union from './union'
 
 const _ = {
   ...$,
@@ -66,12 +52,15 @@ const _ = {
   DigitList,
   Digit,
   Function,
+  Integer,
   Kind,
   List,
+  Loop,
+  Matrix,
   NaturalNumberTheory,
   NaturalNumber,
   Number,
-  Object,
+  Object: Object_,
   Parser,
   Stress,
   String,
