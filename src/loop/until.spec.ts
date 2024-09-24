8c672e57fd9b42e389f1cf327102e85a748a17fb
poteat <poteat@users.noreply.github.com>
2024-09-24 13:35:19 -0700

    feat: add new 'loop' module

---
+++ src/loop/until.spec.ts
@@ -0,0 +1,31 @@
+import { $, Conditional, Kind, List, Loop, NaturalNumber, Test } from '..'
+
+type Until_Spec = [
+  /**
+   * Can loop until a number is greater than 10.
+   */
+  Test.Expect<
+    $<
+      $<
+        $<Loop.Until, $<NaturalNumber.IsGreaterThan, 10>>,
+        NaturalNumber.Increment
+      >,
+      0
+    >,
+    11
+  >,
+
+  /**
+   * Can handle growing tuples.
+   */
+  Test.Expect<
+    $<
+      $<
+        $<Loop.Until, $<Kind.Pipe, [List.Length, $<Conditional.Equals, 10>]>>,
+        $<List.Push, 1>
+      >,
+      [1]
+    >,
+    [1, 1, 1, 1, 1, 1, 1, 1, 1, 1]
+  >
+]
