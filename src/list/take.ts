7134b15157d40a7e5069669afd1d04add6124a6e
poteat <poteat@users.noreply.github.com>
2024-09-29 16:34:48 -0700

    feat: add 'Haskellian' take operator for lists

---
+++ src/list/take.ts
@@ -0,0 +1,45 @@
+import { Kind, Type, Number } from '..'
+
+/**
+ * `_$take` is a type-level function that takes in a number `N` and a list `T`,
+ * and returns a list of the first `N` elements of `T`.
+ *
+ * @template {number} N - The number of elements to take.
+ * @template {unknown[]} T - The list to take elements from.
+ *
+ * @example
+ * ```ts
+ * type T0 = _$take<2, [1, 2, 3]> // [1, 2]
+ * ```
+ */
+export type _$take<
+  N extends Number.Number,
+  T extends unknown[],
+  O extends unknown[] = []
+> = 0 extends 1
+  ? never
+  : O['length'] extends N
+    ? O
+    : T extends [infer Head, ...infer Tail]
+      ? _$take<N, Tail, [...O, Head]>
+      : never
+
+interface Take_T<N extends Number.Number> extends Kind.Kind {
+  f(x: Type._$cast<this[Kind._], unknown[]>): _$take<N, typeof x>
+}
+
+/**
+ * `Take` is a type-level function that takes in a number `N` and a list `T`,
+ * and returns a list of the first `N` elements of `T`.
+ *
+ * @template {number} N - The number of elements to take.
+ * @template {unknown[]} T - The list to take elements from.
+ *
+ * @example
+ * ```ts
+ * type T0 = $<$<List.Take, 2>, [1, 2, 3]> // [1, 2]
+ * ```
+ */
+export interface Take extends Kind.Kind {
+  f(x: Type._$cast<this[Kind._], Number.Number>): Take_T<typeof x>
+}
