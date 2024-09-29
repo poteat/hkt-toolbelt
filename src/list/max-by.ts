0138c73b309917f65e91b581b43bfe3dd424514e
poteat <poteat@users.noreply.github.com>
2024-09-28 21:04:02 -0700

    feat: implement various value-level utils

--- src/list/max-by.ts
+++ src/list/max-by.ts
@@ -1,4 +1,4 @@
-import { $, Kind, NaturalNumber, Type } from '..'
+import { $, Kind, NaturalNumber, Type, Function } from '..'
 
 /**
  * `_$maxBy` is a type-level function that takes in a kind `F` that returns a number,
@@ -28,7 +28,7 @@ export type _$maxBy<
     : never
   : MaxValue
 
-interface MaxBy_T<F extends Kind.Kind> extends Kind.Kind {
+export interface MaxBy_T<F extends Kind.Kind> extends Kind.Kind {
   f(x: Type._$cast<this[Kind._], unknown[]>): _$maxBy<F, typeof x>
 }
 
@@ -48,3 +48,34 @@ interface MaxBy_T<F extends Kind.Kind> extends Kind.Kind {
 export interface MaxBy extends Kind.Kind {
   f(x: Type._$cast<this[Kind._], Kind.Kind>): MaxBy_T<typeof x>
 }
+
+/**
+ * Given a list, return the element in the list that has the highest score
+ * when applying `F` to each element.
+ *
+ * @param {Kind.Kind} f - The kind that returns a number.
+ * @param {unknown[]} values - The list to find the maximum element of.
+ *
+ * @example
+ * ```ts
+ * import { List, Function } from "hkt-toolbelt";
+ *
+ * const result = List.maxBy(Function.identity)([1, 2, 3])
+ * //    ^? 3
+ * ```
+ */
+export const maxBy = ((f: Function.Function) => (values: unknown[]) => {
+  let maxValue = undefined
+  let maxScore = -Infinity
+
+  for (const value of values) {
+    const score = f(value as never) as number
+
+    if (score > maxScore) {
+      maxValue = value
+      maxScore = score
+    }
+  }
+
+  return maxValue
+}) as Kind._$reify<MaxBy>
