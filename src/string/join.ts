df41752c2677435130740495ff0abdc9ea13b06b
poteat <poteat@users.noreply.github.com>
2024-10-03 19:02:23 -0700

    feat: reify string join method

--- src/string/join.ts
+++ src/string/join.ts
@@ -52,3 +52,20 @@ interface Join_T<D extends string> extends Kind.Kind {
 export interface Join extends Kind.Kind {
   f(x: Type._$cast<this[Kind._], string>): Join_T<typeof x>
 }
+
+/**
+ * Given a list of strings, join them together with a delimiter.
+ *
+ * @param {string} d - The delimiter to use when joining the strings.
+ * @param {string[]} x - The list of strings to join.
+ *
+ * @example
+ * ```ts
+ * import { String } from "hkt-toolbelt";
+ *
+ * const result = String.join(' ', ['foo', 'bar', 'qux'])
+ * //    ^? 'foo bar qux'
+ * ```
+ */
+export const join = ((d: string) => (x: string[]) =>
+  x.join(d)) as Kind._$reify<Join>
