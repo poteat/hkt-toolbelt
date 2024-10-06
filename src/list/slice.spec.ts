34348f0c6b06fa80535191bae0ab9d3726b005a4
poteat <poteat@users.noreply.github.com>
2024-10-06 14:34:41 -0700

    feat: reify slice utility

--- src/list/slice.spec.ts
+++ src/list/slice.spec.ts
@@ -72,3 +72,19 @@ type Slice_Spec = [
   // @ts-expect-error
   $<$<$<$<List.Slice, 1>, 2>, 3>, [1, 2, 3]>
 ]
+
+it('should return a slice of a list', () => {
+  expect(List.slice([1, 2, 3, 4, 5])(1)(3)).toEqual([2, 3])
+})
+
+it('should return a slice of a list with negative indices', () => {
+  expect(List.slice([1, 2, 3, 4, 5])(-2)(-1)).toEqual([3, 4])
+})
+
+it('can reverse a list', () => {
+  expect(List.slice([1, 2, 3, 4, 5])(-1)(0)).toEqual([5, 4, 3, 2, 1])
+})
+
+it('can handle empty lists', () => {
+  expect(List.slice([])(0)(0)).toEqual([])
+})
