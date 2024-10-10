c32e1b04bb077ce96491ca7e8c9c99f417a899c0
poteat <poteat@users.noreply.github.com>
2024-10-09 19:51:30 -0700

    feat: generalize from-string number type

--- src/number/from-string.ts
+++ src/number/from-string.ts
@@ -25,5 +25,5 @@ export type _$fromString<T extends Number.Number> = T extends number
  * type T1 = $<Number.FromString, 'abc'> // never
  */
 export interface FromString extends Kind.Kind {
-  f(x: Type._$cast<this[Kind._], string>): _$fromString<typeof x>
+  f(x: Type._$cast<this[Kind._], Number.Number>): _$fromString<typeof x>
 }
