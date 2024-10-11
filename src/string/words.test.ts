04ce59698b1e7f5d1bca2651f23f7494195cf07f
poteat <poteat@users.noreply.github.com>
2024-10-11 16:15:50 -0700

    feat: implement word-splitting for strings

---
+++ src/string/words.test.ts
@@ -0,0 +1,44 @@
+import { $, String, Test } from '..'
+
+type Words_Spec = [
+  /**
+   * Can split a string into words.
+   */
+  Test.Expect<$<String.Words, 'hello world'>, ['hello', 'world']>,
+
+  /**
+   * Can split a string with digits
+   */
+  Test.Expect<$<String.Words, 'hello42 world'>, ['hello', '42', 'world']>,
+
+  /**
+   * Can split a string into words with multiple spaces.
+   */
+  Test.Expect<$<String.Words, 'foo   bar'>, ['foo', 'bar']>,
+
+  /**
+   * Can properly handle acronyms.
+   */
+  Test.Expect<$<String.Words, 'XMLHttpRequest'>, ['XML', 'Http', 'Request']>,
+
+  /**
+   * Can handle alphanumeric input with no delimiters.
+   */
+  Test.Expect<$<String.Words, 'hello42world'>, ['hello', '42', 'world']>
+]
+
+it('should split a string into words', () => {
+  expect(String.words('hello world')).toEqual(['hello', 'world'])
+})
+
+it('should split a string with digits', () => {
+  expect(String.words('hello42world')).toEqual(['hello', '42', 'world'])
+})
+
+it('should split a string into words with multiple spaces', () => {
+  expect(String.words('foo   bar')).toEqual(['foo', 'bar'])
+})
+
+it('should properly handle acronyms', () => {
+  expect(String.words('XMLHttpRequest')).toEqual(['XML', 'Http', 'Request'])
+})
