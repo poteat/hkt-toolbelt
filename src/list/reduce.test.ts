e7881871e1ed85803f85b082af51ef65a3bb2da6
poteat <poteat@users.noreply.github.com>
2024-10-12 22:34:17 -0700

    feat: reify list reduce method

--- src/list/reduce.test.ts
+++ src/list/reduce.test.ts
@@ -6,3 +6,7 @@ type Reduce_Spec = [
    */
   Test.Expect<$<$<$<List.Reduce, NaturalNumber.Add>, 0>, [2, 2]>, 4>
 ]
+
+it('should reduce a list of numbers to their sum', () => {
+  expect(List.reduce(NaturalNumber.add)(0)([2, 2])).toBe(4)
+})
