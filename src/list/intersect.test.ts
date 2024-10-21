15db36b3c5b5f6bb3844b21d58d825cd781f3c3c
poteat <poteat@users.noreply.github.com>
2024-10-20 19:30:01 -0700

    feat: reify list intersect method

--- src/list/intersect.test.ts
+++ src/list/intersect.test.ts
@@ -21,3 +21,15 @@ type Intersect_Spec = [
    */
   Test.Expect<$<$<List.Intersect, [1, 2, 3]>, [4, 5, 6]>, []>
 ]
+
+it('should return the common elements of two lists', () => {
+  expect(List.intersect([1, 2, 3])([1, 2, 3, 4, 5])).toEqual([1, 2, 3])
+})
+
+it('should return an empty list if the lists are disjoint', () => {
+  expect(List.intersect([1, 2, 3])([4, 5, 6])).toEqual([])
+})
+
+it('can handle deep equality', () => {
+  expect(List.intersect([{ a: 1 }])([{ a: 1 }])).toEqual([{ a: 1 }])
+})
