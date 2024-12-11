e08726364491f8aa2cdd1400c04d9e52caceead7
poteat <poteat@users.noreply.github.com>
2024-12-11 01:02:44 -0800

    feat: reify number is-integer method

--- src/number/is-integer.ts
+++ src/number/is-integer.ts
@@ -1,4 +1,4 @@
-import { Type, Kind, Number } from '..'
+import { Type, Kind, Number as Number_ } from '..'
 
 /**
  * `Number.IsInteger` is a type-level function that checks if a number is an integer.
@@ -9,7 +9,7 @@ import { Type, Kind, Number } from '..'
  * type T0 = Number._$isInteger<1> // true
  * type T1 = Number._$isInteger<1.5> // false
  */
-export type _$isInteger<T extends Number.Number> = `${T}` extends `${bigint}`
+export type _$isInteger<T extends Number_.Number> = `${T}` extends `${bigint}`
   ? T extends `0x${string}`
     ? false
     : true
@@ -25,5 +25,13 @@ export type _$isInteger<T extends Number.Number> = `${T}` extends `${bigint}`
  * type T1 = $<Number.IsInteger, 1.5> // false
  */
 export interface IsInteger extends Kind.Kind {
-  f(x: Type._$cast<this[Kind._], Number.Number>): _$isInteger<typeof x>
+  f(x: Type._$cast<this[Kind._], Number_.Number>): _$isInteger<typeof x>
 }
+
+/**
+ * Given a number, determines if it is an integer.
+ */
+export const isInteger = ((value: Number_.Number) =>
+  typeof value === 'bigint'
+    ? true
+    : Number.isInteger(Number(value))) as Kind._$reify<IsInteger>
